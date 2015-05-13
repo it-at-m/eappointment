4d892fa397cf6c8c0b49fecf13b00a0d3d3672dd
Mathias Fischer <mathias.fischer@berlinonline.de>
2015-05-13 14:09:40 +0200

    refs #25230 - 115 - LocationList kann nun nach Appointments filtern und ID-Listen und CSV zurückgeben

--- src/Dldb/Collection/Locations.php
+++ src/Dldb/Collection/Locations.php
@@ -8,4 +8,32 @@ namespace BO\Dldb\Collection;
 
 class Locations extends Base
 {
+    /**
+     * find locations with appointments
+     *
+     */
+    public function getLocationsWithAppointmentsFor($service_id = null, $external = false)
+    {
+        $list = new self();
+        foreach ($this as $location) {
+            if ($location->hasAppointments($service_id, $external)) {
+                $list[] = $location;
+            }
+        }
+        return $list;
+    }
+
+    public function getIds()
+    {
+        $idList = array();
+        foreach ($this as $location) {
+            $idList[] = $location['id'];
+        }
+        return $idList;
+    }
+
+    public function getCSV()
+    {
+        return implode(',', $this->getIds());
+    }
 }
