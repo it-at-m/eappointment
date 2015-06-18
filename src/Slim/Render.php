9644289074f7d0eb6a57bed54cf68e88cc661512
Mathias Fischer <mathias.fischer@berlinonline.de>
2015-06-18 15:14:33 +0200

    Added Render-Helper

---
+++ src/Slim/Render.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package 115Mandant
+ * @copyright BerlinOnline Stadtportal GmbH & Co. KG
+ **/
+
+namespace BO\Slim\Helper;
+
+class Render
+{
+
+    /**
+     * @return self
+     */
+    public static function error404()
+    {
+        \App::$slim->notFound();
+    }
+
+    /**
+     * @return NULL
+     */
+    public static function html($template, $parameters = array())
+    {
+        \App::$slim->response->setStatus(200);
+        \App::$slim->response->headers->set('Content-Type', 'text/html; charset=utf-8');
+        \App::$slim->render($template, $parameters);
+    }
+
+    /**
+     * @param String $date strtotime interpreted
+     * @param String $expires strtotime interpreted
+     *
+     * @return NULL
+     */
+    public static function lastModified($date, $expires = '+5 minutes')
+    {
+        if (!is_int($date)) {
+            $date = strtotime($date);
+        }
+        \App::$slim->lastModified($date);
+        \App::$slim->expires($expires);
+    }
+
+    /**
+     * @param String $route_name
+     * @param Array $arguments parameters in the route path
+     * @param Array $parameter parameters to append with "?"
+     * @param Int $statuscode see an HTTP reference
+     *
+     * @return NULL
+     */
+    public static function redirect($route_name, $arguments, $parameter, $statuscode = 302)
+    {
+        $url = \App::$slim->urlFor($route_name, $arguments);
+        $url = preg_replace('#^.*?(https?://)#', '\1', $url); // allow http:// routes
+        if ($parameter) {
+            $url .= '?' . http_build_query($parameter);
+        }
+        \App::$slim->redirect($url, $statuscode);
+        return $url;
+    }
+}
