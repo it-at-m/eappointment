d8d1fa76e30189fc172bdb00b70533884ff27239
Torsten Kunst <torsten.kunst@berlinonline.de>
2015-07-08 14:33:39 +0200

    dldbfrontend - renamed Themen to Topics

--- src/Dldb/Collection/Services.php
+++ src/Dldb/Collection/Services.php
@@ -22,23 +22,4 @@ class Services extends Base
 	{
 		return implode(',', $this->getIds($serviceList));
 	}
-	
-	public function getSidebarTopics()
-	{
-		foreach($this as $key => $topic){
-			/*
-			if($topic['relation']['navi'] == 1){
-				$list[] = array(
-						'id' => $topic['id'],
-						'name' => $topic['name'],
-						'path' => $topic['path'],
-						'rank' => $topic['relation']['rank'],
-						'root' => $topic['relation']['root']
-				);
-			}
-			*/
-		}
-		
-		//return $list;
-	}
 }
