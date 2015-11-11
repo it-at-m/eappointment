a16494e1782669896e95081512965d531ec9fd32
Mathias Fischer <mathias.fischer@berlinonline.de>
2015-11-11 19:03:10 +0100

    refs #27034 - Validate swagger defintion; Added Calender/Day Schema

--- gulp/tasks/yaml-swagger.js
+++ gulp/tasks/yaml-swagger.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var yaml = require('js-yaml');
 var fs   = require('fs');
+var SwaggerParser = require('swagger-parser');
+
 
 gulp.task('yaml-swagger', [], function () {
     fs.readFile('public/doc/swagger.yaml', function(error, data) {
@@ -8,11 +10,20 @@ gulp.task('yaml-swagger', [], function () {
             console.log(error);
         } else {
             var doc = yaml.safeLoad(data);
-            fs.writeFile('public/doc/swagger.json', JSON.stringify(doc), function (error) {
-                if (error) {
-                    console.log(error);
-                }
+            var savedoc = yaml.safeLoad(data);
+            SwaggerParser.validate(doc)
+            .then(function(api) {
+                console.log("Validated API %s, Version: %s", api.info.title, api.info.version);
+                fs.writeFile('public/doc/swagger.json', JSON.stringify(savedoc), function (error) {
+                    if (error) {
+                        console.log(error);
+                    }
+                });
+            })
+            .catch(function(err) {
+                console.error(err);
             });
+
         }
     });
 });
