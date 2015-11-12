d708932089c4a957ec606a09b02e7edb2367d886
Mathias Fischer <mathias.fischer@berlinonline.de>
2015-11-12 18:24:42 +0100

    refs #27034 - Weitere routen zu notifications

--- routing.php
+++ routing.php
@@ -16,6 +16,106 @@
 /* ---------------------------------------------------------------------------
  * json
  * -------------------------------------------------------------------------*/
+
+
+/**
+*  @swagger
+*  "/calendar/":
+*      get:
+*          description: Get a list of available days for appointments
+*          parameters:
+*              -   name: calendar
+*                  description: data for finding available days
+*                  in: body
+*                  schema:
+*                      $ref: "schema/calendar.json"
+*          responses:
+*              200:
+*                  description: get an updated calendar objects with updated days list
+*                  schema:
+*                      type: object
+*                      properties:
+*                          meta:
+*                              $ref: "schema/metaresult.json"
+*                          data:
+*                              $ref: "schema/calendar.json"
+*              404:
+*                  description: "Could not find any available days"
+*                  schema:
+*                      $ref: "schema/calendar.json"
+*/
+\App::$slim->get('/calendar/',
+    '\BO\Zmsapi\CalendarGet:render')
+    ->name("pagesindex");
+
+
+/**
+ *  @swagger
+ *  "/notifications/":
+ *      get:
+ *          description: get a list of notifications in the send queue
+ *          responses:
+ *              200:
+ *                  description: returns a list, might be empty
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: "schema/notification.json"
+ */
+\App::$slim->post('/notifications/',
+    '\BO\Zmsapi\ProcessFree:render')
+    ->name("pagesindex");
+
+
+
+/**
+ *  @swagger
+ *  "/notifications/":
+ *      post:
+ *          description: Add a notification to the send queue
+ *          parameters:
+ *              -   name: notification
+ *                  description: notification data to send
+ *                  in: body
+ *                  schema:
+ *                      $ref: "schema/notification.json"
+ *          responses:
+ *              200:
+ *                  description: notification accepted
+ *              400:
+ *                  description: "Missing required properties in the notification"
+ */
+\App::$slim->post('/notifications/',
+    '\BO\Zmsapi\ProcessFree:render')
+    ->name("pagesindex");
+
+/**
+ *  @swagger
+ *  "/notifications/{id}":
+ *      delete:
+ *          description: delete a notification in the send queue
+ *          parameters:
+ *              -   name: id
+ *                  description: notification number
+ *                  in: path
+ *                  required: true
+ *                  type: integer
+ *          responses:
+ *              200:
+ *                  description: succesfully deleted
+ *              404:
+ *                  description: "could not find notification or notification already sent"
+ */
+\App::$slim->post('/notifications/',
+    '\BO\Zmsapi\ProcessFree:render')
+    ->name("pagesindex");
+
+
 /**
  *  @swagger
  *  "/process/{id}/{authKey}/":
@@ -36,7 +136,12 @@
  *              200:
  *                  description: "success"
  *                  schema:
- *                      $ref: "schema/process.json"
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              $ref: "schema/process.json"
  *              403:
  *                  description: "authkey does not match"
  *              404:
@@ -44,6 +149,9 @@
  */
 \App::$slim->get('/process/:id/:authKey/',
     '\BO\Zmsapi\AppointmentGet:render')
+    ->conditions([
+        'id' => '\d{4,11}',
+     ])
     ->name("pagesindex");
 
 
@@ -72,44 +180,27 @@
  *              200:
  *                  description: "success, there might be changes on the object or added information. Use the response for further action with the process"
  *                  schema:
- *                      $ref: "schema/process.json"
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              $ref: "schema/process.json"
  *              403:
  *                  description: "authkey does not match"
  *              404:
  *                  description: "process id does not exists"
  */
- \App::$slim->get('/process/:id/:authKey/',
-     '\BO\Zmsapi\AppointmentPost:render')
-     ->name("pagesindex");
+\App::$slim->get('/process/:id/:authKey/',
+    '\BO\Zmsapi\AppointmentPost:render')
+    ->conditions([
+        'id' => '\d{4,11}',
+    ])
+    ->name("pagesindex");
 
 /**
  *  @swagger
- *  "/calendar/":
- *      get:
- *          description: Get a list of available days for appointments
- *          parameters:
- *              -   name: calendar
- *                  description: data for finding available days
- *                  in: body
- *                  schema:
- *                      $ref: "schema/calendar.json"
- *          responses:
- *              200:
- *                  description: get an updated calendar objects with updated days list
- *                  schema:
- *                      $ref: "schema/calendar.json"
- *              404:
- *                  description: "Could not find any available days"
- *                  schema:
- *                      $ref: "schema/calendar.json"
- */
- \App::$slim->get('/calendar/',
-     '\BO\Zmsapi\CalendarGet:render')
-     ->name("pagesindex");
-
-/**
- *  @swagger
- *  "/process/free/":
+ *  "/process/status/free/":
  *      get:
  *          description: Get a list of free processes for a given day
  *          parameters:
@@ -120,11 +211,16 @@
  *                      $ref: "schema/calendar.json"
  *          responses:
  *              200:
- *                  description: get an updated calendar objects with updated days list
+ *                  description: get a list of available processes
  *                  schema:
- *                      type: array
- *                      items:
- *                          $ref: "schema/process.json"
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: "schema/process.json"
  *              404:
  *                  description: "Could not find any available processes, returns empty list"
  *                  schema:
@@ -132,17 +228,81 @@
  *                      items:
  *                          $ref: "schema/process.json"
  */
- \App::$slim->get('/calendar/',
-     '\BO\Zmsapi\CalendarGet:render')
-     ->name("pagesindex");
+\App::$slim->get('/process/status/free/',
+    '\BO\Zmsapi\ProcessFree:render')
+    ->name("pagesindex");
 
+/**
+ *  @swagger
+ *  "/process/status/reserved/":
+ *      get:
+ *          description: Get a list of reserved processes
+ *          responses:
+ *              200:
+ *                  description: get a list of processes
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: "schema/process.json"
+ *              404:
+ *                  description: "Could not find any processes, returns empty list"
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: "schema/process.json"
+ */
+\App::$slim->get('/process/status/reserved/',
+    '\BO\Zmsapi\ProcessFree:render')
+    ->name("pagesindex");
 
-//\App::$slim->get('/dienstleistung/:service_id',
-//    '\BO\Zmsapi\ServiceDetail:render')
-//    ->conditions([
-//        'service_id' => '\d{3,10}',
-//        ])
-//    ->name("servicedetail");
+/**
+ *  @swagger
+ *  "/process/status/reserved/":
+ *      post:
+ *          description: Try to reserve the appointments in a process
+ *          parameters:
+ *              -   name: process
+ *                  description: process data to update
+ *                  in: body
+ *                  schema:
+ *                      $ref: "schema/process.json"
+ *          responses:
+ *              200:
+ *                  description: get a list of processes
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: "schema/process.json"
+ *              404:
+ *                  description: "Could not find any processes, returns empty list"
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          meta:
+ *                              $ref: "schema/metaresult.json"
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: "schema/process.json"
+ */
+\App::$slim->get('/process/status/reserved/',
+    '\BO\Zmsapi\ProcessFree:render')
+    ->name("pagesindex");
 
 /* ---------------------------------------------------------------------------
  * maintenance
