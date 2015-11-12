9131e525e4e101f71bdc9722bb1ee1d8d0d1a37f
Mathias Fischer <mathias.fischer@berlinonline.de>
2015-11-12 18:24:42 +0100

    refs #27034 - better logging

--- gulp/tasks/yaml-swagger.js
+++ gulp/tasks/yaml-swagger.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var yaml = require('js-yaml');
 var fs   = require('fs');
 var SwaggerParser = require('swagger-parser');
-
+var gutil = require('gulp-util');
 
 gulp.task('yaml-swagger', [], function () {
     fs.readFile('public/doc/swagger.yaml', function(error, data) {
@@ -13,15 +13,15 @@ gulp.task('yaml-swagger', [], function () {
             var savedoc = yaml.safeLoad(data);
             SwaggerParser.validate(doc)
             .then(function(api) {
-                console.log("Validated API %s, Version: %s", api.info.title, api.info.version);
+                gutil.log("Validated API %s, Version: %s", gutil.colors.magenta(api.info.title), api.info.version);
                 fs.writeFile('public/doc/swagger.json', JSON.stringify(savedoc), function (error) {
                     if (error) {
-                        console.log(error);
+                        gutil.log(gutil.colors.red(error));
                     }
                 });
             })
             .catch(function(err) {
-                console.error(err);
+                gutil.log(gutil.colors.red.bold(err));
             });
 
         }
