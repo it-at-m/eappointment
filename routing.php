9e623c53b1643137606fbc3c596ecfb26bd83211
Mathias Fischer <mathias.fischer@berlinonline.de>
2015-11-12 18:24:42 +0100

    refs #27034 - neue Routen für process und calendar

--- routing.php
+++ routing.php
@@ -16,42 +16,42 @@
 /* ---------------------------------------------------------------------------
  * json
  * -------------------------------------------------------------------------*/
- /**
-  *  @swagger
-  *  "/process/{id}/{authKey}/":
-  *      get:
-  *          description: Get a process
-  *          parameters:
-  *              -   name: id
-  *                  description: process number
-  *                  in: path
-  *                  required: true
-  *                  type: integer
-  *              -   name: authKey
-  *                  description: authentication key
-  *                  in: path
-  *                  required: true
-  *                  type: string
-  *          responses:
-  *              200:
-  *                  description: "success"
-  *                  schema:
-  *                      $ref: "schema/process.json"
-  *              403:
-  *                  description: "authkey does not match"
-  *              404:
-  *                  description: "process id does not exists"
-  */
-  \App::$slim->get('/appointment/:id/',
-      '\BO\Zmsapi\AppointmentGet:render')
-      ->name("pagesindex");
+/**
+ *  @swagger
+ *  "/process/{id}/{authKey}/":
+ *      get:
+ *          description: Get a process
+ *          parameters:
+ *              -   name: id
+ *                  description: process number
+ *                  in: path
+ *                  required: true
+ *                  type: integer
+ *              -   name: authKey
+ *                  description: authentication key
+ *                  in: path
+ *                  required: true
+ *                  type: string
+ *          responses:
+ *              200:
+ *                  description: "success"
+ *                  schema:
+ *                      $ref: "schema/process.json"
+ *              403:
+ *                  description: "authkey does not match"
+ *              404:
+ *                  description: "process id does not exists"
+ */
+\App::$slim->get('/process/:id/:authKey/',
+    '\BO\Zmsapi\AppointmentGet:render')
+    ->name("pagesindex");
 
 
 /**
  *  @swagger
  *  "/process/{id}/{authKey}/":
  *      post:
- *          description: Get a process
+ *          description: Update a process
  *          parameters:
  *              -   name: id
  *                  description: process number
@@ -70,7 +70,7 @@
  *                      $ref: "schema/process.json"
  *          responses:
  *              200:
- *                  description: "success"
+ *                  description: "success, there might be changes on the object or added information. Use the response for further action with the process"
  *                  schema:
  *                      $ref: "schema/process.json"
  *              403:
@@ -78,8 +78,62 @@
  *              404:
  *                  description: "process id does not exists"
  */
- \App::$slim->get('/appointment/:id/',
+ \App::$slim->get('/process/:id/:authKey/',
      '\BO\Zmsapi\AppointmentPost:render')
+     ->name("pagesindex");
+
+/**
+ *  @swagger
+ *  "/calendar/":
+ *      get:
+ *          description: Get a list of available days for appointments
+ *          parameters:
+ *              -   name: calendar
+ *                  description: data for finding available days
+ *                  in: body
+ *                  schema:
+ *                      $ref: "schema/calendar.json"
+ *          responses:
+ *              200:
+ *                  description: get an updated calendar objects with updated days list
+ *                  schema:
+ *                      $ref: "schema/calendar.json"
+ *              404:
+ *                  description: "Could not find any available days"
+ *                  schema:
+ *                      $ref: "schema/calendar.json"
+ */
+ \App::$slim->get('/calendar/',
+     '\BO\Zmsapi\CalendarGet:render')
+     ->name("pagesindex");
+
+/**
+ *  @swagger
+ *  "/process/free/":
+ *      get:
+ *          description: Get a list of free processes for a given day
+ *          parameters:
+ *              -   name: calendar
+ *                  description: data for finding available processes, try to restrict data to one day, if possible
+ *                  in: body
+ *                  schema:
+ *                      $ref: "schema/calendar.json"
+ *          responses:
+ *              200:
+ *                  description: get an updated calendar objects with updated days list
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: "schema/process.json"
+ *              404:
+ *                  description: "Could not find any available processes, returns empty list"
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: "schema/process.json"
+ */
+ \App::$slim->get('/calendar/',
+     '\BO\Zmsapi\CalendarGet:render')
      ->name("pagesindex");
 
 
