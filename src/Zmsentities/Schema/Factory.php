0b04a81b406b373894c5644f0058f9e4fc13e3e1
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-01-14 16:30:30 +0100

    refs #27521 - Factory class to generate entities from array/json

---
+++ src/Zmsentities/Schema/Factory.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace BO\Zmsentities\Schema;
+
+/**
+ * @SuppressWarnings(NumberOfChildren)
+ */
+class Factory
+{
+    /**
+     * @var Array $data unserialized entity
+     */
+    protected $data = null;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public static function create($data)
+    {
+        return new self($data);
+    }
+
+    /**
+     * Detect type of entity and return initialized object
+     *
+     * @return Entity
+     */
+    public function getEntity()
+    {
+        $entityName = $this->getEntityName();
+        $class = "\\BO\\Zmsentities\\$entityName";
+        return new $class($this->data);
+    }
+
+    /**
+     * Parse schema and return Entity name
+     *
+     * @return String
+     */
+    public function getEntityName()
+    {
+        if (!array_key_exists('$schema', $this->data)) {
+            throw new Exception('Missing $schema-key on given data.');
+        }
+        $schema = $this->data['$schema'];
+        $entityName = preg_replace('#^.*/([^/]+)\.json#', '$1', $schema);
+        return ucfirst($entityName);
+    }
+}
