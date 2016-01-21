b000044b84c33fcbd3f9fedb42c043b2e4d821c0
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-01-21 13:02:50 +0100

    ref #27410 - add methods to add some properties to entities

--- src/Zmsentities/Calendar.php
+++ src/Zmsentities/Calendar.php
@@ -1,8 +1,30 @@
 <?php
-
 namespace BO\Zmsentities;
 
 class Calendar extends Schema\Entity
 {
+
     public static $schema = "calendar.json";
+
+    public function addProvider($source, $idList)
+    {
+        foreach (explode(',', $idList) as $id) {
+            $provider = new Provider();
+            $provider->source = $source;
+            $provider->id = $id;
+            $this->providers[] = $provider;
+        }
+        return $this;
+    }
+
+    public function addRequest($source, $requestList)
+    {
+        foreach (explode(',', $requestList) as $id) {
+            $request = new Request();
+            $request->source = $source;
+            $request->id = $id;
+            $this->requests[] = $request;
+        }
+        return $this;
+    }
 }
--- src/Zmsentities/Process.php
+++ src/Zmsentities/Process.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace BO\Zmsentities;
 
 class Process extends Schema\Entity
 {
+
     public static $schema = "process.json";
 
     public function getRequestIds()
@@ -18,5 +18,22 @@ class Process extends Schema\Entity
     public function getRequestCSV()
     {
         return implode(',', $this->getRequestIds());
+    }
+
+    public function addDate($date)
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    public function addRequest($source, $requestList)
+    {
+        foreach (explode(',', $requestList) as $id) {
+            $request = new Request();
+            $request->source = $source;
+            $request->id = $id;
+            $this->requests[] = $request;
+        }
+        return $this;
     }
 }
