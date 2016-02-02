f7b02f7ab5c6bea209b305aa0cfc5a31ced2f62e
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-02-02 16:15:46 +0100

    refs #27450 - Fill free days in calendar

--- src/Zmsdb/Calendar.php
+++ src/Zmsdb/Calendar.php
@@ -3,6 +3,7 @@
 namespace BO\Zmsdb;
 
 use \BO\Zmsentities\Calendar as Entity;
+use \BO\Zmsdb\Query\SlotList;
 
 class Calendar extends Base
 {
@@ -10,6 +11,7 @@ class Calendar extends Base
     public function readResolvedEntity(\BO\Zmsentities\Calendar $calendar)
     {
         $calendar['processing'] = [];
+        $calendar['processing']['slotlist'] = new SlotList();
         $calendar = $this->readResolvedProviders($calendar);
         $calendar = $this->readResolvedClusters($calendar);
         $calendar = $this->readResolvedRequests($calendar);
@@ -65,61 +67,16 @@ class Calendar extends Base
 
     protected function readResolvedDays(\BO\Zmsentities\Calendar $calendar)
     {
-        $query = 'SELECT
-                UNIX_TIMESTAMP(CONCAT(b.Datum, " ", b.Uhrzeit)) AS appointment__date,
-                s.StandortID AS appointment__scope__id,
-                s.mehrfachtermine AS appointment__scope__preferences__appointment__multipleSlotsEnabled,
-                DAYOFMONTH(b.Datum) AS `day`,
-                MONTH(b.Datum) AS `month`,
-                YEAR(b.Datum) AS `year`,
-                GREATEST(0, o.Anzahlterminarbeitsplaetze - o.reduktionTermineImInternet - COUNT(b.Datum))
-                    AS `freeAppointments__public`,
-                GREATEST(0, o.Anzahlterminarbeitsplaetze - o.reduktionTermineCallcenter - COUNT(b.Datum))
-                    AS `freeAppointments__callcenter`,
-                o.Anzahlterminarbeitsplaetze - COUNT(b.Datum)
-                    AS `freeAppointments__intern`,
-                FLOOR(((TIME_TO_SEC(b.Uhrzeit) - TIME_TO_SEC(o.Anfangszeit)) / TIME_TO_SEC(o.Timeslot))) AS `slotnr`,
-                o.erlaubemehrfachslots AS availability__multipleSlotsAllowed,
-                o.allexWochen AS availability__repeat__afterWeeks,
-                o.jedexteWoche AS availability__repeat__weekOfMonth,
-                FLOOR(TIME_TO_SEC(o.Timeslot) / 60) AS availability__slotTimeInMinutes,
-                UNIX_TIMESTAMP(o.Startdatum) AS availability__startDate,
-                o.Terminanfangszeit	 AS availability__startTime
-            FROM
-                standort s
-                LEFT JOIN oeffnungszeit o USING(StandortID)
-                LEFT JOIN buerger b ON
-                    b.StandortID = o.StandortID
-                    AND o.Wochentag & POW(2, DAYOFWEEK(b.Datum) - 1)
-                    AND b.Uhrzeit >= o.Anfangszeit
-                    AND b.Uhrzeit <= o.Endzeit
-                    AND b.Datum >= o.Startdatum
-                    AND b.Datum <= o.Endedatum
-            WHERE
-                o.StandortID = :scope_id
-                AND b.Datum IS NOT NULL
-                AND b.Datum BETWEEN :start_process AND :end_process
-                AND o.Endedatum >= :start_availability
-                AND o.Startdatum <= :end_availability
-                AND o.Anzahlterminarbeitsplaetze != 0
-            GROUP BY o.OeffnungszeitID, b.Datum, `slotnr`
-            -- HAVING
-                -- reduce results cause processing them costs time
-                -- freeAppointments__intern != 0
-        ';
+        $query = SlotList::getQuery();
         $monthList = $calendar->getMonthList();
         $statement = $this->getReader()->prepare($query);
         foreach ($monthList as $monthDateTime) {
+            $month =  new \DateTimeImmutable($monthDateTime->format('c'));
             foreach ($calendar->scopes as $scope) {
-                $statement->execute([
-                    'scope_id' => $scope->id,
-                    'start_process' => $monthDateTime->format('Y-m-1'),
-                    'end_process' => $monthDateTime->format('Y-m-t'),
-                    'start_availability' => $monthDateTime->format('Y-m-1'),
-                    'end_availability' => $monthDateTime->format('Y-m-t'),
-                ]);
-                while ($dayInfo = $statement->fetch(\PDO::FETCH_ASSOC)) {
-                    $calendar = $this->addDayInfoToCalendar($calendar, $dayInfo);
+                $statement->execute(SlotList::getParameters($scope['id'], $monthDateTime));
+                $slotsRequired = $calendar['processing']['slotinfo'][$scope->getProviderId()];
+                while ($slotData = $statement->fetch(\PDO::FETCH_ASSOC)) {
+                    $calendar = $this->addDayInfoToCalendar($calendar, $slotData, $month, $slotsRequired);
                 }
             }
         }
@@ -130,15 +87,24 @@ class Calendar extends Base
      *
      * ATTENTION: performance critical function, keep highly optimized!
      */
-    public function addDayInfoToCalendar(\BO\Zmsentities\Calendar $calendar, array $dayInfo)
-    {
-        $day = $calendar->getDay($dayInfo['year'], $dayInfo['month'], $dayInfo['day']);
-        /* TODO check slots (calendar[processing]) and
-          if availability is true on this day (week series, Termin_ab, Termine_bis)
-        */
-        $day['freeAppointments']['public'] += $dayInfo['freeAppointments__public'];
-        $day['freeAppointments']['intern'] += $dayInfo['freeAppointments__intern'];
-        $day['freeAppointments']['callcenter'] += $dayInfo['freeAppointments__callcenter'];
+    protected function addDayInfoToCalendar(
+        \BO\Zmsentities\Calendar $calendar,
+        array $slotData,
+        \DateTimeImmutable $month,
+        $slotsRequired
+    ) {
+        $slotlist =& $calendar['processing']['slotlist'];
+        if (!$slotlist->isSlotData($slotData)) {
+            $slotlist->toReducedBySlots($slotsRequired);
+            $calendar = $slotlist->addToCalendar($calendar);
+            $calendar['processing']['slotlist'] = new SlotList(
+                $slotData,
+                $month->modify('first day of'),
+                $month->modify('last day of')
+            );
+        } else {
+            $slotlist->addSlotData($slotData);
+        }
         return $calendar;
     }
 }
---
+++ src/Zmsdb/Query/SlotList.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace BO\Zmsdb\Query;
+
+/**
+ * Calculate Slots for available booking times
+ */
+class SlotList
+{
+    const QUERY = 'SELECT
+            UNIX_TIMESTAMP(CONCAT(b.Datum, " ", b.Uhrzeit)) AS appointment__date,
+            s.StandortID AS appointment__scope__id,
+            s.mehrfachtermine AS appointment__scope__preferences__appointment__multipleSlotsEnabled,
+            DAYOFMONTH(b.Datum) AS `day`,
+            MONTH(b.Datum) AS `month`,
+            YEAR(b.Datum) AS `year`,
+            b.Uhrzeit AS slottime,
+            b.Datum AS slotdate,
+            GREATEST(0, o.Anzahlterminarbeitsplaetze - o.reduktionTermineImInternet - COUNT(b.Datum))
+                AS `freeAppointments__public`,
+            GREATEST(0, o.Anzahlterminarbeitsplaetze - o.reduktionTermineCallcenter - COUNT(b.Datum))
+                AS `freeAppointments__callcenter`,
+            o.Anzahlterminarbeitsplaetze - COUNT(b.Datum)
+                AS `freeAppointments__intern`,
+            FLOOR(((TIME_TO_SEC(b.Uhrzeit) - TIME_TO_SEC(o.Terminanfangszeit)) / TIME_TO_SEC(o.Timeslot))) AS `slotnr`,
+            o.OeffnungszeitID AS availability__id,
+            o.erlaubemehrfachslots AS availability__multipleSlotsAllowed,
+            o.allexWochen AS availability__repeat__afterWeeks,
+            o.jedexteWoche AS availability__repeat__weekOfMonth,
+            FLOOR(TIME_TO_SEC(o.Timeslot) / 60) AS availability__slotTimeInMinutes,
+            UNIX_TIMESTAMP(o.Startdatum) AS availability__startDate,
+            UNIX_TIMESTAMP(o.Endedatum) AS availability__endDate,
+            o.Terminanfangszeit	 AS availability__startTime,
+            o.Terminendzeit	 AS availability__endTime,
+            o.Wochentag & 2 AS availability__weekday__monday,
+            o.Wochentag & 4 AS availability__weekday__tuesday,
+            o.Wochentag & 8 AS availability__weekday__wednesday,
+            o.Wochentag & 16 AS availability__weekday__thursday,
+            o.Wochentag & 32 AS availability__weekday__friday,
+            o.Wochentag & 64 AS availability__weekday__saturday,
+            o.Wochentag & 1 AS availability__weekday__sunday,
+            o.Anzahlterminarbeitsplaetze - o.reduktionTermineImInternet AS availability__workstationCount__public,
+            o.Anzahlterminarbeitsplaetze - o.reduktionTermineCallcenter AS availability__workstationCount__callcenter,
+            o.Anzahlterminarbeitsplaetze AS availability__workstationCount__intern,
+            IF(o.Offen_ab, o.Offen_ab, s.Termine_ab) AS availability__bookable__startInDays,
+            IF(o.Offen_bis, o.Offen_bis, s.Termine_bis) AS availability__bookable__endInDays
+        FROM
+            standort s
+            LEFT JOIN oeffnungszeit o USING(StandortID)
+            LEFT JOIN buerger b ON
+                b.StandortID = o.StandortID
+                AND o.Wochentag & POW(2, DAYOFWEEK(b.Datum) - 1)
+                AND b.Uhrzeit >= o.Terminanfangszeit
+                AND b.Uhrzeit <= o.Terminendzeit
+                AND b.Datum >= o.Startdatum
+                AND b.Datum <= o.Endedatum
+        WHERE
+            o.StandortID = :scope_id
+            AND o.OeffnungszeitID IS NOT NULL
+            AND (b.Datum IS  NULL OR b.Datum BETWEEN :start_process AND :end_process)
+            AND o.Endedatum >= :start_availability
+            AND o.Startdatum <= :end_availability
+            AND o.Anzahlterminarbeitsplaetze != 0
+        GROUP BY o.OeffnungszeitID, b.Datum, `slotnr`
+        HAVING
+            -- reduce results cause processing them costs time and here we have a query cache
+            FROM_UNIXTIME(appointment__date) BETWEEN
+                DATE_ADD(:nowStart, INTERVAL availability__bookable__startInDays DAY)
+                AND DATE_ADD(:nowEnd, INTERVAL availability__bookable__endInDays DAY)
+        ORDER BY o.OeffnungszeitID, b.Datum, `slotnr`
+        ';
+
+    /**
+     * @var array $slotData Single result row from the query
+     */
+    protected $slotData = null;
+
+    /**
+     * @var \BO\Zmsentities\Availability $availability
+     */
+    protected $availability = null;
+
+    /**
+     * @var array $slots
+     */
+    protected $slots = [];
+
+    public function __construct(
+        array $slotData = ['availability__id' => null],
+        \DateTimeImmutable $start = null,
+        \DateTimeImmutable $stop = null
+    ) {
+        $this->setSlotData($slotData);
+        if (isset($this->availability['id'])) {
+            $this->createSlots($start, $stop);
+            $this->addSlotData($slotData);
+        }
+    }
+
+    public static function getQuery()
+    {
+        return self::QUERY;
+    }
+
+    public static function getParameters($scopeId, \DateTime $monthDateTime)
+    {
+        $now = new \DateTimeImmutable();
+        $parameters = [
+            'scope_id' => $scopeId,
+            'start_process' => $monthDateTime->format('Y-m-1'),
+            'end_process' => $monthDateTime->format('Y-m-t'),
+            'start_availability' => $monthDateTime->format('Y-m-1'),
+            'end_availability' => $monthDateTime->format('Y-m-t'),
+            'nowStart' => $now->format('Y-m-d'),
+            'nowEnd' => $now->format('Y-m-d'),
+        ];
+        return $parameters;
+    }
+
+    public function setSlotData(array $slotData)
+    {
+        $this->slotData = $slotData;
+        $availability = [];
+        foreach ($slotData as $key => $value) {
+            if (0 === strpos($key, 'availability__')) {
+                $newkey = str_replace('availability__', '', $key);
+                $availability[$newkey] = $value;
+            }
+        }
+        $this->availability = new \BO\Zmsentities\Availability($availability);
+        return $this;
+    }
+
+    public function isSlotData(array $slotData)
+    {
+        return $this->slotData['availability__id'] == $slotData['availability__id']
+            //&& $this->slotData['day'] == $slotData['day']
+            //&& $this->slotData['month'] == $slotData['month']
+            //&& $this->slotData['year'] == $slotData['year']
+        ;
+    }
+
+    public function addSlotData(array $slotData)
+    {
+        if (isset($slotData['slotnr'])) {
+            $slotnumber = $slotData['slotnr'];
+            $slotdate = $slotData['slotdate'];
+            $slot =& $this->slots[$slotdate][$slotnumber];
+            if (isset($slot['slottime'])) {
+                throw new \Exception(
+                    "Found two database entries for the same slot $slotdate #$slotnumber @" . $slotData['slottime']
+                );
+            }
+            if (!isset($slot['public'])) {
+                var_dump($this->slots);
+                var_dump($slotData);
+            }
+            $slot['slottime'] = $slotData['slottime'];
+            $slot['public'] -= $slotData['availability__workstationCount__public'];
+            $slot['public'] += $slotData['freeAppointments__public'];
+            $slot['callcenter'] -= $slotData['availability__workstationCount__callcenter'];
+            $slot['callcenter'] += $slotData['freeAppointments__callcenter'];
+            $slot['intern'] -= $slotData['availability__workstationCount__intern'];
+            $slot['intern'] += $slotData['freeAppointments__intern'];
+        }
+        return $this;
+    }
+
+    public function addToCalendar(\BO\Zmsentities\Calendar $calendar)
+    {
+        foreach ($this->slots as $date => $slotList) {
+            $datetime = new \DateTimeImmutable($date);
+            $day = $calendar->getDayByDateTime($datetime);
+            foreach ($slotList as $slotInfo) {
+                $day['freeAppointments']['public'] += $slotInfo['public'];
+                $day['freeAppointments']['intern'] += $slotInfo['intern'];
+                $day['freeAppointments']['callcenter'] += $slotInfo['callcenter'];
+            }
+        }
+        //var_dump($this->slots);
+        return $calendar;
+    }
+
+    public function toReducedBySlots($slotsRequired)
+    {
+        //TODO: implement
+        $slotsRequired = $slotsRequired;
+        return $this;
+    }
+
+    /**
+     * Create slots based on availability
+     */
+    public function createSlots(
+        \DateTimeImmutable $startDate,
+        \DateTimeImmutable $stopDate
+    ) {
+        $startDate = new \DateTime($startDate->format('c'));
+        do {
+            $startTime = new \DateTime($this->availability['startTime']);
+            $stopTime = new \DateTime($this->availability['endTime']);
+            $date = $startDate->format('Y-m-d');
+            if ($this->availability->hasDate($startDate)) {
+                $slotnr = 0;
+                do {
+                    $this->slots[$date][$slotnr] = [
+                        'time' => $startTime->format('H:i'),
+                        'public' => $this->availability['workstationCount']['public'],
+                        'callcenter' => $this->availability['workstationCount']['callcenter'],
+                        'intern' => $this->availability['workstationCount']['intern'],
+                    ];
+                    $startTime->modify('+' . $this->availability['slotTimeInMinutes'] . 'minute');
+                    $slotnr++;
+                } while ($startTime->getTimestamp() <= $stopTime->getTimestamp());
+            }
+            $startDate->modify('+1day');
+            $date = $startDate->format('Y-m-d');
+        } while ($startDate->getTimestamp() <= $stopDate->getTimestamp());
+    }
+}
