bf45d6d3646afb3577c256dc3c1d2e7cd53be82e
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-02-26 08:33:15 +0100

    clientzms package to zmsclient and cf in SessionGet

--- src/Zmsapi/SessionGet.php
+++ src/Zmsapi/SessionGet.php
@@ -22,22 +22,21 @@ class SessionGet extends BaseController
      * @return String
      */
     public static function render()
-    {        
+    {
         $message = Response\Message::create();
         $input = Validator::input()->isJson()->getValue();
-     
+
         $handler = new SessionHandler();
         session_set_save_handler($handler, true);
-        if($input && array_key_exists('id', $input)){
+        if ($input && array_key_exists('id', $input)) {
             session_id($input['id']);
             session_name($input['name']);
-        }
-        else {
+        } else {
             session_name(\App::SESSION_NAME);
-        }       
-       
+        }
+
         session_start();
-        $session = (new Query())->readEntity();        
+        $session = (new Query())->readEntity();
         $message->data = $session;
         // $message->data = \BO\Zmsentities\Session::createExample();
         Render::lastModified(time(), '0');
