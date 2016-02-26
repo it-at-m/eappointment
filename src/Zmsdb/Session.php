1a23c17c96bf4deace860807b786e78fab8b0a60
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-02-26 08:44:48 +0100

    fix - remove double session start

--- src/Zmsdb/Session.php
+++ src/Zmsdb/Session.php
@@ -3,7 +3,6 @@
 namespace BO\Zmsdb;
 
 use \BO\Zmsentities\Session as Entity;
-use \BO\Zmsclient\SessionHandler;
 
 class Session extends Base
 {
@@ -14,11 +13,7 @@ class Session extends Base
     public function readEntity()
     {
         $entity = new Entity();
-        $handler = new SessionHandler();        
-        session_set_save_handler($handler, true);        
-        session_name($handler->sessionName);
-        session_start();
-        $entity['name'] = $handler->sessionName;
+        $entity['name'] = session_name();
         $entity['id'] = session_id();
         $entity['content'] = $_SESSION;
         return $entity;
