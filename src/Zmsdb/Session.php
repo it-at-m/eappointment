327e7ea7e88c2f4e38b4ce00edde772e2d862001
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-02-26 17:38:26 +0100

    ref #27886 - read, write and delete functions on entity

--- src/Zmsdb/Session.php
+++ src/Zmsdb/Session.php
@@ -8,14 +8,78 @@ class Session extends Base
 {
     /**
      * Fetch status from db
-     * @return \BO\Zmsentities\Status
+     * @return \BO\Zmsentities\Session
      */
-    public function readEntity()
+    public function readEntity($sessionName, $sessionId)
     {
         $entity = new Entity();
-        $entity['name'] = session_name();
-        $entity['id'] = session_id();
-        $entity['content'] = $_SESSION;
+        $sessionContent = $this->readSessionData($sessionName, $sessionId);
+        $entity['name'] = $sessionName;
+        $entity['id'] = $sessionId;
+        $entity['content'] = $sessionContent;
         return $entity;
+    }
+
+    public function updateEntity($session)
+    {
+        $this->writeSessionData($session['name'], $session['id'], $session['content']);
+        $entity = $this->readEntity($session['name'], $session['id']);
+        return $entity;
+    }
+
+    public function deleteEntity($sessionName, $sessionId)
+    {
+        $this->deleteSessionData($sessionName, $sessionId);
+    }
+
+    protected function readSessionData($sessionName, $sessionId)
+    {
+        $result = $this->getReader()->fetchOne('
+            SELECT
+                sessioncontent
+            FROM
+                sessiondata
+            WHERE
+                sessionid = ? AND
+                sessionname = ?
+            ', array(
+                    $sessionId,
+                    $sessionName
+                ));
+        return $result['sessioncontent'];
+    }
+
+    protected function writeSessionData($sessionName, $sessionId, $sessionContent)
+    {
+        $query = '
+            REPLACE INTO
+                sessiondata
+            SET
+                sessionid=?,
+                sessionname=?,
+                sessioncontent=?
+        ';
+        $statement = $this->getWriter()->prepare($query);
+        $statement->execute(array(
+            $sessionId,
+            $sessionName,
+            $sessionContent
+        ));
+    }
+
+    protected function deleteSessionData($sessionName, $sessionId)
+    {
+        $query = '
+            DELETE FROM
+                sessiondata
+            WHERE
+                sessionid=? AND
+                sessionname=?
+        ';
+        $statement = $this->getWriter()->prepare($query);
+        $statement->execute(array(
+            $sessionId,
+            $sessionName
+        ));
     }
 }
