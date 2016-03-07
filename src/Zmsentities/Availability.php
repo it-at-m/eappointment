c518083b5ac33666427f7db7ac018a943988153c
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-03-07 18:11:41 +0100

    hasDay() with date range check

--- src/Zmsentities/Availability.php
+++ src/Zmsentities/Availability.php
@@ -19,13 +19,47 @@ class Availability extends Schema\Entity
         'saturday'
     ];
 
+    /**
+     * Set Default values
+     */
+    public function getDefaults()
+    {
+        return [
+            'weekday' => array_fill_keys(self::$weekdayNameList, 0),
+        ];
+    }
+
     public function hasDate(\DateTimeInterface $dateTime)
     {
         $weekDayName = self::$weekdayNameList[$dateTime->format('w')];
+        $start = $this->getStartDateTime();
+        $end = $this->getEndDateTime();
         if (!$this['weekday'][$weekDayName]) {
+            // Wrong weekday
+            return false;
+        }
+        if ($dateTime->getTimestamp() < $start->getTimestamp() || $dateTime->getTimestamp() > $end->getTimestamp()) {
+            // Out of date range
             return false;
         }
         // TODO: implement afterWeeks and weekOfMonth
+
         return true;
+    }
+
+    public function getStartDateTime()
+    {
+        $time = new \DateTime();
+        $time->setTimestamp($this['startDate']);
+        $time->modify('today ' .  $this['startTime']);
+        return $time;
+    }
+
+    public function getEndDateTime()
+    {
+        $time = new \DateTime();
+        $time->setTimestamp($this['endDate']);
+        $time->modify('today ' .  $this['endTime']);
+        return $time;
     }
 }
--- tests/Zmsentities/AvailabilityTest.php
+++ tests/Zmsentities/AvailabilityTest.php
@@ -5,4 +5,17 @@ namespace BO\Zmsentities\Tests;
 class AvailabilityTest extends EntityCommonTests
 {
     public $entityclass = '\BO\Zmsentities\Availability';
+
+    public function testHasDay()
+    {
+        $entity = new $this->entityclass();
+        $time = new \DateTimeImmutable('2016-01-01 12:34:56');
+        $entity['startDate'] = $time->getTimestamp();
+        $entity['startTime'] = $time->format('H:i');
+        $entity['endDate'] = $time->modify("+2month")->getTimestamp();
+        $entity['endTime'] = $time->modify("+2month 17:10:00")->format('H:i');
+        $entity['weekday']['friday'] = 1;
+        //var_dump($entity);
+        $this->assertTrue($entity->hasDate($time));
+    }
 }
