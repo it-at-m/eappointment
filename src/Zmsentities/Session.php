f1f111d7c23d165e4051025c99bd6c8189a58652
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-03-14 18:21:41 +0100

    ref #27472, #27477, #27886 - session schema

--- src/Zmsentities/Session.php
+++ src/Zmsentities/Session.php
@@ -5,4 +5,32 @@ namespace BO\Zmsentities;
 class Session extends Schema\Entity
 {
     public static $schema = "session.json";
+
+    public function getDefaults()
+    {
+        return [
+            'basket' =>[
+                'requests' => '',
+                'providers' => '',
+                'scope' => '',
+                'process' => '',
+                'date' => '',
+                'familyName' => '',
+                'email' => '',
+                'telehone' => '',
+                'amendment' => '',
+                'authKey' => ''
+            ],
+            'human' => [
+                'captcha_text' => '',
+                'Client' => 0,
+                'TS' => 0,
+                'Origin' => '',
+                'RemoteAddress' => '',
+                'referrer' => '',
+                'Step' => array()
+            ]
+        ];
+    }
+
 }
