41220182f8ac3b6a94137d82b2eb5284a5dd3c35
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-04-25 13:35:21 +0200

    ref #28423 - getContent and isEncoding check on Ics Entity

--- src/Zmsentities/Ics.php
+++ src/Zmsentities/Ics.php
@@ -5,4 +5,15 @@ namespace BO\Zmsentities;
 class Ics extends Schema\Entity
 {
     public static $schema = "ics.json";
+
+    public function getContent()
+    {
+        $content = $this->content;
+        return ($this->isEncoding()) ? \base64_decode($content) : $content;
+    }
+
+    public function isEncoding()
+    {
+        return (\base64_decode($this->content, true)) ? true : false;
+    }
 }
