80d42a348b33e110ffb44fa2fd333dfea4f999f3
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-04-25 16:35:33 +0200

    refs #28293 - Kompatibilität PHP 5.4 für Präsentation

--- src/Zmsadmin/Availability.php
+++ src/Zmsadmin/Availability.php
@@ -15,8 +15,9 @@ class Availability extends ScopeAvailabilityDay
     /**
      * @return String
      */
-    public static function render()
+    public static function render($scope_id = '00')
     {
-        return parent::render('00');
+        $scope_id = '00';
+        return parent::render($scope_id);
     }
 }
