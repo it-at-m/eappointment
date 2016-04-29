41f89aa7f0057c3651cc19abfd2c85d4363f7130
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-04-29 14:18:37 +0200

    refs #28299 - Globaler Basis-Klasse für Controller

---
+++ src/Slim/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Slimproject
+ * @copyright BerlinOnline Stadtportal GmbH & Co. KG
+ **/
+
+namespace BO\Slim;
+
+abstract class Controller
+{
+
+    public function __construct()
+    {
+    }
+}
