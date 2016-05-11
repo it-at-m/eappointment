ac33899e3e49e9e09f9e6c30d9b7e59eb047e795
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-05-11 16:42:48 +0200

    BROKEN - ref #28552, #28305 - test read, write and delete mail and notification in unit test

---
+++ tests/Zmsdb/MailTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace BO\Zmsdb\Tests;
+
+use \BO\Zmsdb\Mail as Query;
+use \BO\Zmsentities\Mail as Entity;
+
+class MailTest extends Base
+{
+    public function testBasic()
+    {
+        $input = $this->getTestEntity();
+        $query = new Query();
+        $queueId = $query->writeInQueue($input);
+        $entity = $query->readEntity($queueId);
+
+        $this->assertEntity("\\BO\\Zmsentities\\Mail", $entity);
+        $this->assertEquals("Das ist ein Plaintext Test", $entity->getPlainPart());
+        $this->assertEquals("max@service.berlin.de", $entity->getFirstClient()['email']);
+
+        $deleteTest = $query->deleteEntity($queueId);
+        $this->assertTrue($deleteTest, "Failed to delete Mail from Database.");
+
+        $entity = $query->readEntity($queueId);
+        $this->assertFalse($entity->hasId($queueId), "Deleted Mail still exists in Database.");
+    }
+
+    public function testCollection()
+    {
+        $input = $this->getTestEntity();
+        $query = new Query();
+
+        $mailList = $query->readList(1);
+        $mailList->addMail($input);
+        $this->assertEntityList("\\BO\\Zmsentities\\Mail", $mailList);
+        $this->assertTrue($mailList->hasMail(1234), "Missing Test Notification with ID 1234 in collection");
+    }
+    protected function getTestEntity()
+    {
+        $input = new Entity(array(
+            "id" => 1234,
+            "createIP" => "145.15.3.10",
+            "createTimestamp" => 1447931596000,
+            "department" => [
+                "id" => 72,
+                "preferences" => [
+                    "notifications" => [
+                        "enabled" => true,
+                        "identification" => "terminvereinbarung@mitte.berlin.de",
+                        "sendConfirmationEnabled" => true,
+                        "sendReminderEnabled" => true
+                    ]
+                ]
+            ],
+            "multipart" => array(
+                [
+                    "mime" => "text/html",
+                    "content" =>  "<h1>Title</h1><p>Message</p>",
+                    "base64" => false
+                ],
+                [
+                    "mime" => "text/plain",
+                    "content" =>  "Das ist ein Plaintext Test",
+                    "base64" => false
+                ],
+                [
+                    "mime" => "text/calendar",
+                    "content" =>  "Hier steht ein IcsString",
+                    "base64" => false
+                ]
+            ),
+            "process" => [
+                "clients" => array(
+                    [
+                        "familyName" => "Max Mustermann",
+                        "email" => "max@service.berlin.de",
+                        "telephone" => "030 115"
+                    ]
+                ),
+                "id" => 80410,
+                "authKey" => "f22c",
+                "reminderTimestamp" => 1447931730000,
+                "scope" => [
+                    "id" => 151,
+                    "preferences" => [
+                        "appointment" => [
+                            "deallocationDuration" => "5",
+                            "endInDaysDefault" => "60",
+                            "multipleSlotsEnabled" => "1",
+                            "reservationDuration" => "5",
+                            "startInDaysDefault" => "0"
+                        ],
+                        "client" => [
+                            "alternateAppointmentUrl" => "",
+                            "amendmentActivated" => "0",
+                            "amendmentLabel" => "",
+                            "emailRequired" => "1",
+                            "telephoneActivated" => "1",
+                            "telephoneRequired" => "1"
+                        ],
+                        "notifications" => [
+                            "confirmationContent" => "",
+                            "confirmationEnabled" => "0",
+                            "enabled" => "1",
+                            "headsUpContent" => "",
+                            "headsUpTime" => "0"
+                        ],
+                        "pickup" => [
+                            "alternateName" => "Ausgabe",
+                            "isDefault" => "0"
+                        ],
+                        "queue" => [
+                            "callCountMax" => "0",
+                            "callDisplayText" => "Herzlich Willkommen \r\nin Berlin Reinickendorf\r\n=====================\r\nTIP => Termin statt Wartezeit!\r\n=====================\r\nNutzen Sie die Online Terminvergabe unter =>\r\nhttp =>//www.berlin.de/ba-reinickendorf/org/buergeramt/",
+                            "firstNumber" => "1000",
+                            "lastNumber" => "1999",
+                            "processingTimeAverage" => "00 =>15 =>00",
+                            "publishWaitingTimeEnabled" => "1",
+                            "statisticsEnabled" => "1"
+                        ],
+                        "survey" => [
+                            "emailContent" => "",
+                            "enabled" => "0",
+                            "label" => ""
+                        ],
+                        "ticketprinter" => [
+                            "deactivatedText" => "",
+                            "notificationsAmendmentEnabled" => "0",
+                            "notificationsDelay" => "0"
+                        ],
+                        "workstation" => [
+                            "emergencyEnabled" => "0"
+                        ]
+                    ],
+                ],
+                "status" => "confirmed"
+            ],
+            "subject" => "Example Mail"
+        ));
+        return $input;
+    }
+}
---
+++ tests/Zmsdb/NotificationTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace BO\Zmsdb\Tests;
+
+use \BO\Zmsdb\Notification as Query;
+use \BO\Zmsentities\Notification as Entity;
+
+class NotificationTest extends Base
+{
+    public function testBasic()
+    {
+        $input = $this->getTestEntity();
+        $query = new Query();
+        $queueId = $query->writeInQueue($input);
+        $entity = $query->readEntity($queueId, 1);
+
+        $this->assertEntity("\\BO\\Zmsentities\\Notification", $entity);
+        $this->assertEquals("80410", $entity->getProcessId());
+        $this->assertEquals("f22c", $entity->getProcessAuthKey());
+
+        $deleteTest = $query->deleteEntity($queueId);
+        $this->assertTrue($deleteTest, "Failed to delete Notification from Database.");
+    }
+
+    public function testCollection()
+    {
+        $input = $this->getTestEntity();
+        $query = new Query();
+
+        $notificationList = $query->readList(1);
+        $notificationList->addNotification($input);
+        $this->assertEntityList("\\BO\\Zmsentities\\Notification", $notificationList);
+        $this->assertTrue($notificationList->hasNotification(1234), "Missing Test Notification with ID 1234 in collection");
+    }
+
+    protected function getTestEntity()
+    {
+        $input = new Entity(array(
+            "id" => 1234,
+            "createIP" => "145.15.3.10",
+            "createTimestamp" => 1447931596000,
+            "department" => [
+                "id" => 72,
+                "preferences" => [
+                    "notifications" => [
+                        "enabled" => true,
+                        "identification" => "terminvereinbarung@mitte.berlin.de",
+                        "sendConfirmationEnabled" => true,
+                        "sendReminderEnabled" => true
+                    ]
+                ]
+            ],
+            "message" => "Denken Sie an ihren Termin mit der Nummer 80410",
+            "process" => [
+                "clients" => [
+                    [
+                        "familyName" => "Max Mustermann",
+                        "email" => "max@service.berlin.de",
+                        "telephone" => "030 115"
+                    ]
+                ],
+                "id" => 80410,
+                "authKey" => "f22c",
+                "reminderTimestamp" => 1447931730000,
+                "scope" => [
+                    "id" => 141
+                ],
+                "status" => "confirmed"
+            ]
+        ));
+        return $input;
+    }
+}
