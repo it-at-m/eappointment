762a06b662c168e0cdc0c308be98145d5ace1c8f
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-05-12 14:51:34 +0200

    create Ics from Entity, not from Api

--- src/Zmsapi/ProcessIcs.php
+++ src/Zmsapi/ProcessIcs.php
@@ -8,6 +8,7 @@ namespace BO\Zmsapi;
 
 use \BO\Slim\Render;
 use \BO\Zmsdb\Process as Query;
+use \BO\Zmsdb\Config as Config;
 
 /**
   * Handle requests concerning services
@@ -21,7 +22,8 @@ class ProcessIcs extends BaseController
     {
         $message = Response\Message::create();
         $process = (new Query())->readEntity($itemId, $authKey, 2);
-        $ics = (new Messaging\IcsAppointment())->createIcsString($process);
+        $config = (new Config())->readEntity();
+        $ics = \BO\Zmsentities\Helper\Messaging::createIcs($process, $config);
         $message->data = $ics;
         Render::lastModified(time(), '0');
         Render::json($message);
