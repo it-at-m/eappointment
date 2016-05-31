8e11bcb16adbf5725961774baf2397bd78cf9267
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-05-31 10:39:28 +0200

    refs #28737 - Added timezone in config, cause bootstrapping is later

--- config.example.php
+++ config.example.php
@@ -9,4 +9,4 @@ class App extends \BO\Zmsapi\Application
 }
 
 // Uncomment the following line for production data, this is testing only
-App::$now = new DateTimeImmutable('2016-04-01 11:55:00');
+App::$now = new DateTimeImmutable('2016-04-01 11:55:00', new DateTimeZone('Europe/Berlin'));
