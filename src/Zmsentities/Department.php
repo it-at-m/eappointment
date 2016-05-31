71e74ff358c133e30ac811d6adc25c0004213bc0
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-05-31 15:30:01 +0200

    ref #28627, 28627# some funtions for department entity to get and create or transform some properties, add composer binary, add contact to department scheme

--- src/Zmsentities/Department.php
+++ src/Zmsentities/Department.php
@@ -15,4 +15,30 @@ class Department extends Schema\Entity
     {
         return ($this->preferences['notifications']);
     }
+    
+    public function toContact($address = array(), $contactPerson = null)
+    {
+        $address = explode(' ', str_replace(',', '', $address));
+        $this->contact['street'] = trim($address[0]);
+        $this->contact['streetNumber'] = trim($address[1]);
+        $this->contact['postalCode'] = trim($address[2]);
+        $this->contact['region'] = trim($address[3]);
+        $this->contact['name'] = $contactPerson;
+        return $this;
+    }
+    
+    public function toAddress()
+    {
+        $address =
+            $this->contact['street'] .' '.
+            $this->contact['streetNumber'] .', '.
+            $this->contact['postalCode'] .' '.
+            $this->contact['region'];
+        return $address;
+    }
+    
+    public function getContactPerson()
+    {
+        return $this->contact['name'];
+    }
 }
