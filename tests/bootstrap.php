acbe12531d8698ea7aa4f60a05b860d03a2cbcd5
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-06-03 11:23:44 +0200

    Fixed locale and timezone for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,6 @@ require(__DIR__ . '/../config.php');
 
 \BO\Zmsdb\Helper\DldbData::$dataPath = __DIR__ . '/Zmsdb/fixtures';
 \BO\Zmsdb\Helper\DldbData::getDataRepository('de');
+
+setlocale(LC_ALL, 'de_DE.utf8', 'de_DE', 'de');
+date_default_timezone_set('Europe/Berlin');
