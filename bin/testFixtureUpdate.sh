f95712c215a021793f39a97088737c2eebb3f204
Mathias Fischer <mathias.fischer@berlinonline.de>
2016-06-06 10:25:58 +0200

    Fixed inconsistent filename shema for authority_de.json

--- bin/testFixtureUpdate.sh
+++ bin/testFixtureUpdate.sh
@@ -16,5 +16,5 @@ cp $CACHE/amtshelfer_dienstleistungen.json $DIR/../tests/Dldb/fixtures/services_
 cp $CACHE/amtshelfer_dienstleistungen_en.json $DIR/../tests/Dldb/fixtures/services_en.json
 cp $CACHE/amtshelfer_settings.json $DIR/../tests/Dldb/fixtures/settings.json
 cp $CACHE/amtshelfer_themen.json $DIR/../tests/Dldb/fixtures/topic_de.json
-cp $CACHE/amtshelfer_behoerden.json $DIR/../tests/Dldb/fixtures/authorities_de.json
+cp $CACHE/amtshelfer_behoerden.json $DIR/../tests/Dldb/fixtures/authoriy_de.json
 
