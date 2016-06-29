77d681d018e2f380ae15fce53f369566992d66e8
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-06-29 10:55:57 +0200

    ref #28673, #28674 - add check if entity exists in collection

--- src/Zmsentities/Collection/OwnerList.php
+++ src/Zmsentities/Collection/OwnerList.php
@@ -8,4 +8,14 @@ class OwnerList extends Base
         $this[] = clone $entity;
         return $this;
     }
+
+    public function hasEntity($entityId)
+    {
+        foreach ($this as $entity) {
+            if ($entityId == $entity->id) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
