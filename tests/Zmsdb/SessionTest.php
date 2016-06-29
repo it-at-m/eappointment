46dfdb5e91470a5da8e0f01292ee775af7898e6a
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-06-29 18:28:04 +0200

    remove var_dump

--- tests/Zmsdb/SessionTest.php
+++ tests/Zmsdb/SessionTest.php
@@ -11,7 +11,6 @@ class SessionTest extends Base
     {
         $query = new Query();
         $input = $this->getTestEntity();
-        var_dump($input);
         $entity = $query->updateEntity($input);
         $entity = $query->readEntity($entity->name, $entity->id);
         $this->assertEquals('7b89b1c1fd6c7a52fa748ff663babd0c', $entity->id);
