0f69bb6db159645105d7e3f7d8f61274a9fadf8c
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-07-01 16:59:41 +0200

    ref #28638, 28679 - admin login workstation and useraccount entity methodes

--- src/Zmsentities/Useraccount.php
+++ src/Zmsentities/Useraccount.php
@@ -5,4 +5,24 @@ namespace BO\Zmsentities;
 class Useraccount extends Schema\Entity
 {
     public static $schema = "useraccount.json";
+
+    private $allRights = array(
+        90 => 'superuser',
+        70 => 'organisation',
+        50 => 'department',
+        40 => 'cluster',
+        30 => 'useraccount',
+        20 => 'scope',
+        15 => 'availability',
+        10 => 'ticketprinter',
+        0 => 'sms'
+    );
+
+    public function getRights()
+    {
+        if (array_key_exists($this-right, $this->allRights)) {
+            return array_slice($this->allRights, $this->right);
+        }
+        return null;
+    }
 }
--- src/Zmsentities/Workstation.php
+++ src/Zmsentities/Workstation.php
@@ -5,4 +5,25 @@ namespace BO\Zmsentities;
 class Workstation extends Schema\Entity
 {
     public static $schema = "workstation.json";
+
+    public function hasId()
+    {
+        return (array_key_exists('id', $this)) ? true : false;
+    }
+
+    public function getQueuePreference($key, $isBoolean = false)
+    {
+        if (array_key_exists($key, $this)) {
+            if ($isBoolean) {
+                return ($this[$key]) ? 1 : 0;
+            } else {
+                return $this[$key];
+            }
+        }
+    }
+
+    public function getAuthKey()
+    {
+        return bin2hex(openssl_random_pseudo_bytes(16));
+    }
 }
