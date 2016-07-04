f42867f96fe91718785d8314a6e23503c75ca800
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-07-04 16:51:39 +0200

    easier SessionHandling without session entity

--- src/Zmsclient/Auth.php
+++ src/Zmsclient/Auth.php
@@ -6,14 +6,16 @@ namespace BO\Zmsclient;
  */
 class Auth
 {
-    public static function setKey($authKey, $sessionName)
+    /**
+     *
+     * @SuppressWarnings(Superglobals)
+     *
+     */
+    public static function setKey($authKey)
     {
-        self::startSession($sessionName);
-        self::writeContentToSession(array(
-            'content' => array(
-                'X-Authkey' => $authKey
-            )
-        ));
+        $_SESSION = array(
+            'X-Authkey' => $authKey
+        );
     }
 
     /**
@@ -23,45 +25,6 @@ class Auth
      */
     public static function getKey()
     {
-        self::startSession();
-        $session = new \BO\Zmsentities\Session($_SESSION);
-        return ('' != $session->content['X-Authkey']) ? $session->content['X-Authkey'] : null;
-    }
-
-    /**
-     *
-     * @SuppressWarnings(Superglobals)
-     *
-     */
-    public static function deleteKey()
-    {
-        self::startSession();
-        session_destroy();
-    }
-
-    private static function startSession($sessionName = '')
-    {
-        if (headers_sent() === false && session_status() !== PHP_SESSION_ACTIVE) {
-            $handler = new SessionHandler();
-            session_set_save_handler($handler, true);
-            if ('' != $sessionName) {
-                session_name($sessionName);
-            }
-            session_start();
-        }
-    }
-
-    /**
-     *
-     * @SuppressWarnings(Superglobals)
-     *
-     * @return self
-     */
-    private static function writeContentToSession($sessionData = null)
-    {
-        $_SESSION = $sessionData;
-        if (session_status() === PHP_SESSION_ACTIVE) {
-            session_register_shutdown();
-        }
+        return (isset($_SESSION['X-Authkey'])) ? $_SESSION['X-Authkey'] : null;
     }
 }
