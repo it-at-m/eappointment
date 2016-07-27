13aecbc96d381c6776b9edc5aeec376fa427f3bc
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-07-27 15:18:34 +0200

    Bug: freeAppointments could be an array, should be slot entity

--- src/Zmsentities/Day.php
+++ src/Zmsentities/Day.php
@@ -16,8 +16,9 @@ class Day extends Schema\Entity
         ];
     }
 
+    //@todo freeAppointments could be an array, should be slot entity
     public function __toString()
     {
-        return "Day @{$this->year}-{$this->month}-{$this->day} with " . $this->freeAppointments;
+        return "Day @{$this->year}-{$this->month}-{$this->day} with "; //. $this->freeAppointments;
     }
 }
