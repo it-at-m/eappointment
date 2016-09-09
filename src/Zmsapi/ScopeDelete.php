bbd01952682f16a0cb34ca2acdf77e8b919e28dd
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-09-09 16:06:16 +0200

    ref #28892 - throw exception on scope delete call if processes are assigned to scope

--- src/Zmsapi/ScopeDelete.php
+++ src/Zmsapi/ScopeDelete.php
@@ -1,29 +1,39 @@
 <?php
-/**
- * @package 115Mandant
- * @copyright BerlinOnline Stadtportal GmbH & Co. KG
- **/
 
+/**
+ *
+ * @package Zmsadmin
+ * @copyright BerlinOnline Stadtportal GmbH & Co. KG
+ *
+ */
 namespace BO\Zmsapi;
 
 use \BO\Slim\Render;
 use \BO\Zmsdb\Scope as Query;
 
 /**
-  * Handle requests concerning services
-  */
+ * Handle requests concerning services
+ */
 class ScopeDelete extends BaseController
 {
+
     /**
+     *
      * @return String
      */
     public static function render($itemId)
     {
         $query = new Query();
         $message = Response\Message::create(Render::$request);
-        $department = $query->readEntity($itemId);
-        $query->deleteEntity($itemId);
-        $message->data = $department;
+        $entity = $query->readEntity($itemId);
+        $processList = (new \BO\Zmsdb\Process())->readProcessListByScopeId($itemId);
+        error_log(count($processList));
+        if (count($processList)) {
+            throw new Exception\Scope\ScopeHasProcesses();
+        } else {
+            //$query->deleteEntity($itemId);
+        }
+        $message->data = $entity;
         Render::lastModified(time(), '0');
         Render::json($message->setUpdatedMetaData(), $message->getStatuscode());
     }
