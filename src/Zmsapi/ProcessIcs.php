a7d3cd9bf5bcefe1f9058153d602d6112497eb4e
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-09-21 09:06:40 +0200

    repare ics process api call, update swagger docs, composer update

--- src/Zmsapi/ProcessIcs.php
+++ src/Zmsapi/ProcessIcs.php
@@ -22,9 +22,18 @@ class ProcessIcs extends BaseController
     {
         $message = Response\Message::create(Render::$request);
         $process = (new Query())->readEntity($itemId, $authKey, 2);
-        $config = (new Config())->readEntity();
-        $ics = \BO\Zmsentities\Helper\Messaging::createIcs($process, $config);
-        $message->data = $ics;
+        $authKeyByProcessId = (new Query())->readAuthKeyByProcessId($process->id);
+
+        if (null === $authKeyByProcessId) {
+            throw new Exception\Process\ProcessNotFound();
+        } elseif ($authKeyByProcessId != $process->authKey) {
+            throw new Exception\Process\AuthKeyMatchFailed();
+        } else {
+            $config = (new Config())->readEntity();
+            $ics = \BO\Zmsentities\Helper\Messaging::getMailIcs($process, $config);
+            $message->data = $ics;
+        }
+
         Render::lastModified(time(), '0');
         Render::json($message->setUpdatedMetaData(), $message->getStatuscode());
     }
