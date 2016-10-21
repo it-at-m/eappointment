89db3f7f07450c2de0b801da59c6c766688a09b2
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-10-21 16:58:27 +0200

    ref #29875 - add function to get alternate appointment url from only scope in list

--- src/Zmsentities/Collection/ScopeList.php
+++ src/Zmsentities/Collection/ScopeList.php
@@ -3,4 +3,14 @@ namespace BO\Zmsentities\Collection;
 
 class ScopeList extends Base
 {
+    public function getAlternateRedirectUrl()
+    {
+        $result = null;
+        $scope = reset($this);
+        $alternateUrl = $scope->toProperty()->preferences->client->alternateAppointmentUrl->get();
+        if (1 == count($this) && $alternateUrl) {
+            $result = $alternateUrl;
+        }
+        return $result;
+    }
 }
