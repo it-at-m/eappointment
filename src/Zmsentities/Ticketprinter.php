64cd7208880f8fd667e826572ceee557af050789
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-10-25 15:44:34 +0200

    ref #29844, #29848 - create random hash with leading organisationId

--- src/Zmsentities/Ticketprinter.php
+++ src/Zmsentities/Ticketprinter.php
@@ -7,4 +7,9 @@ class Ticketprinter extends Schema\Entity
     const PRIMARY = 'id';
 
     public static $schema = "ticketprinter.json";
+
+    public function getHashWith($organisiationId)
+    {
+        return $organisiationId . bin2hex(openssl_random_pseudo_bytes(16));
+    }
 }
