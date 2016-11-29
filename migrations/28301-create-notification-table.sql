e57387d5527f53eecd397069ac1e83b89d1ebb73
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-11-29 16:39:17 +0100

    length of notification message field up to 350

--- migrations/28301-create-notification-table.sql
+++ migrations/28301-create-notification-table.sql
@@ -5,7 +5,7 @@ CREATE TABLE `notificationqueue` (
 	`departmentID` INT(5) UNSIGNED NOT NULL DEFAULT '0' , 	
 	`createIP` VARCHAR(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL , 
 	`createTimestamp` BIGINT(20) NOT NULL DEFAULT '0' , 		
-	`message` VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+	`message` VARCHAR(350) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
 	`clientFamilyName` VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
 	`clientTelephone` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,   		
 PRIMARY KEY (`id`)) 
