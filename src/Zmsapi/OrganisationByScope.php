e69c4eff2311e72d110b2f65689795e812742252
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-12-01 14:03:29 +0100

    throw exception if scope not exists, no need to search for organisation

--- src/Zmsapi/OrganisationByScope.php
+++ src/Zmsapi/OrganisationByScope.php
@@ -9,6 +9,7 @@ namespace BO\Zmsapi;
 use \BO\Slim\Render;
 use \BO\Mellon\Validator;
 use \BO\Zmsdb\Organisation as Query;
+use \BO\Zmsdb\Scope;
 
 /**
   * Handle requests concerning services
@@ -21,8 +22,13 @@ class OrganisationByScope extends BaseController
     public static function render($itemId)
     {
         $resolveReferences = Validator::param('resolveReferences')->isNumber()->setDefault(1)->getValue();
+        $scope = (new Scope())->readEntity($itemId, 0);
+        if (! $scope->hasId()) {
+            error_log(print_r($scope, 1));
+            throw new Exception\Scope\ScopeNotFound();
+        }
         $organisation = (new Query())->readByScopeId($itemId, $resolveReferences);
-        if (! $organisation) {
+        if (! $organisation->hasId()) {
             throw new Exception\Organisation\OrganisationNotFound();
         }
         $message = Response\Message::create(Render::$request);
