43599b97a95a9fca126eac30c8b064ebfdc91cd1
Torsten Kunst <torsten.kunst@berlinonline.de>
2016-12-13 17:50:02 +0100

    refs #29827, #30256 - add organisation to call display schema, resolve cluster and scope csv to lists

--- src/Zmsentities/Calldisplay.php
+++ src/Zmsentities/Calldisplay.php
@@ -4,14 +4,103 @@ namespace BO\Zmsentities;
 
 class Calldisplay extends Schema\Entity
 {
-    const PRIMARY = 'id';
+    const PRIMARY = 'serverTime';
 
     public static $schema = "calldisplay.json";
 
     public function getDefaults()
     {
         return [
-            'serverTime' => time(),
+            'serverTime' => (new \DateTime())->getTimestamp(),
         ];
+    }
+
+    public function withResolvedCollections($input)
+    {
+        if (array_key_exists('scopelist', $input)) {
+            $this->scopes = $this->getScopeListFromCsv($input['scopelist']);
+        }
+        if (array_key_exists('clusterlist', $input)) {
+            $this->clusters = $this->getClusterListFromCsv($input['clusterlist']);
+        }
+        return $this;
+    }
+
+    public function hasScopeList()
+    {
+        return $this->toProperty()->scopes->isAvailable();
+    }
+
+    public function hasClusterList()
+    {
+        return $this->toProperty()->clusters->isAvailable();
+    }
+
+    public function setServerTime($timestamp)
+    {
+        $this->serverTime = $timestamp;
+        return $this;
+    }
+
+    public function getScopeList()
+    {
+        $scopeList = new Collection\ScopeList();
+        if ($this->hasScopeList()) {
+            if ($this->scopes instanceof Collection\ScopeList) {
+                $scopeList = $this->scopes;
+            } else {
+                $scopeList = new Collection\ScopeList($this->scopes);
+            }
+        }
+        return $scopeList;
+    }
+
+    public function getClusterList()
+    {
+        $clusterList = new Collection\ClusterList();
+        if ($this->hasClusterList()) {
+            if ($this->clusters instanceof Collection\ClusterList) {
+                $clusterList = $this->clusters;
+            } else {
+                $clusterList = new Collection\ClusterList($this->clusters);
+            }
+        }
+        return $clusterList;
+    }
+
+    public function getImageName()
+    {
+        if (1 == $this->getScopeList()->count()) {
+            $name = "s_" . $this->getScopeList()->getFirst()->id . "_bild.jpg";
+        } elseif (1 == $this->getClusterList()->count()) {
+            $name = "c_" . $this->getClusterList()->getFirst()->id . "_bild.jpg";
+        }
+        return $name;
+    }
+
+    protected function getScopeListFromCsv($scopeIds = '')
+    {
+        $scopeList = new Collection\ScopeList();
+        $scopeIds = explode(',', $scopeIds);
+        if ($scopeIds) {
+            foreach ($scopeIds as $scopeId) {
+                $scope = new Scope(array('id' => $scopeId));
+                $scopeList->addEntity($scope);
+            }
+        }
+        return $scopeList;
+    }
+
+    protected function getClusterListFromCsv($clusterIds = '')
+    {
+        $clusterList = new Collection\ClusterList();
+        $clusterIds = explode(',', $clusterIds);
+        if ($clusterIds) {
+            foreach ($clusterIds as $clusterId) {
+                $cluster = new Cluster(array('id' => $clusterId));
+                $clusterList->addEntity($cluster);
+            }
+        }
+        return $clusterList;
     }
 }
--- src/Zmsentities/Cluster.php
+++ src/Zmsentities/Cluster.php
@@ -11,7 +11,7 @@ class Cluster extends Schema\Entity
     public function getDefaults()
     {
         return [
-            'name' => '',
+            //'name' => '',
             'scopes' => new Collection\ScopeList(),
         ];
     }
