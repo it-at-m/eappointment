90d51e4767b8dfae904aabda7e6b7824b24ad6c3
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-01-27 09:57:18 +0100

    29826 - new scopelist without dublicates from given list

--- src/Zmsentities/Collection/ScopeList.php
+++ src/Zmsentities/Collection/ScopeList.php
@@ -9,6 +9,17 @@ class ScopeList extends Base
         return (1 == count($this) && $scope->getAlternateRedirectUrl()) ? $scope->getAlternateRedirectUrl() : null;
     }
 
+    public function withoutDublicates($scopeList)
+    {
+        $collection = new self();
+        foreach ($this as $scope) {
+            if (! $scopeList->hasEntity($scope->id)) {
+                $collection->addEntity(clone $scope);
+            }
+        }
+        return $collection;
+    }
+
     public function withUniqueScopes()
     {
         $scopeList = new self();
