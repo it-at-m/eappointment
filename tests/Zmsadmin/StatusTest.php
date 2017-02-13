4cec4bec711743b61014e363798cc09a52b099a9
Leon Weidauer <leon@lnwdr.de>
2017-02-13 16:03:25 +0000

    fix _self template issue. refs #30725

--- tests/Zmsadmin/StatusTest.php
+++ tests/Zmsadmin/StatusTest.php
@@ -10,21 +10,19 @@ class StatusTest extends Base
     protected function getApiCalls()
     {
         return [
-        /*
             [
                 'function' => 'readGetResult',
                 'url' => '/status/',
                 'response' => $this->readFixture("GET_status.json")
             ]
-         */
         ];
     }
 
     public function testRendering()
     {
-        //$response = parent::testRendering();
-        //$this->assertContains('API Version', (string)$response->getBody());
-        // check processes.confirmed:
-        //$this->assertContains('86860', (string)$response->getBody());
+        $response = parent::testRendering();
+        $this->assertContains('API Version', (string)$response->getBody());
+        //check processes.confirmed:
+        $this->assertContains('86860', (string)$response->getBody());
     }
 }
