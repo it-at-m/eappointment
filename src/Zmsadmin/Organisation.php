e7d93c7f8b65205aab36c2e3d14e40e9964c1cda
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-02-13 13:25:46 +0100

    ref 29849 - ticketprinter form fields in organisation, refactored organisation controller

--- src/Zmsadmin/Organisation.php
+++ src/Zmsadmin/Organisation.php
@@ -28,22 +28,11 @@ class Organisation extends BaseController
         $entityId = Validator::value($args['id'])->isNumber()->getValue();
         $entity = \App::$http->readGetResult('/organisation/'. $entityId .'/')->getEntity();
 
-        if (null === $entity || !$entity->hasId()) {
-            return \BO\Slim\Render::withHtml($response, 'page/404.twig', array());
-        }
-
         $input = $request->getParsedBody();
         if (array_key_exists('save', (array) $input)) {
-            try {
-                $entity = new Entity($input);
-                $entity->id = $entityId;
-                $entity = \App::$http->readPostResult(
-                    '/organisation/'. $entity->id .'/',
-                    $entity
-                )->getEntity();
-            } catch (\Exception $exception) {
-                return Helper\Render::error($exception);
-            }
+            $entity = (new Entity($input))->withCleanedUpFormData();
+            $entity->id = $entityId;
+            $entity = \App::$http->readPostResult('/organisation/'. $entity->id .'/', $entity)->getEntity();
         }
 
         return \BO\Slim\Render::withHtml(
