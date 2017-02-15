80abd6b9fa7e90320f42ad6931fd5b6202fde69d
Mathias Fischer <mathias.fischer@berlinonline.de>
2017-02-15 15:02:25 +0100

    refs #28889 - Add workstation data to exception templates

---
+++ src/Zmsadmin/Helper/TwigExceptionHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BO\Zmsadmin\Helper;
+
+use \Psr\Http\Message\RequestInterface;
+use \Psr\Http\Message\ResponseInterface;
+
+class TwigExceptionHandler extends \BO\Slim\TwigExceptionHandler
+{
+    public static function withHtml(
+        RequestInterface $request,
+        ResponseInterface $response,
+        \Exception $exception,
+        $status = 500
+    ) {
+        $exception->templatedata = [
+            'workstation' => \App::$http->readGetResult('/workstation/')->getEntity(),
+            ];
+        return parent::withHtml($request, $response, $exception, $status);
+    }
+}
