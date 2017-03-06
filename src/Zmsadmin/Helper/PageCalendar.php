7d99e93305fcac82b67988e3335b5084aa360cac
Leon Weidauer <leon@lnwdr.de>
2017-03-06 17:27:33 +0000

    fill months in calendar pages. refs #30907

--- src/Zmsadmin/Helper/PageCalendar.php
+++ src/Zmsadmin/Helper/PageCalendar.php
@@ -27,7 +27,7 @@ class PageCalendar
     {
         $this->calendar->scopes[] = $scope;
         try {
-            $calendar = \App::$http->readPostResult('/calendar/', $this->calendar)->getEntity();
+            $calendar = \App::$http->readPostResult('/calendar/', $this->calendar, ['fillWithEmptyDays' => 1])->getEntity();
             return $calendar->getMonthList();
         } catch (\BO\Zmsclient\Exception $exception) {
             if ($exception->template != 'BO\Zmsapi\Exception\Calendar\AppointmentsMissed') {
