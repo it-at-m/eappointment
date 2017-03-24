5a3d2a424470e2ecf4f9c6233a4d7d1b67b3a93d
Leon Weidauer <leon@lnwdr.de>
2017-03-24 09:28:52 +0000

    fix exception detection. refs #31111

--- js/lib/baseview.js
+++ js/lib/baseview.js
@@ -38,7 +38,7 @@ class BaseView extends ErrorHandler {
                 this.$main.html(responseData);
                 resolve(this.$main);
             }).fail(err => {
-                let isException = $(err.responseText).filter('.exception');
+                let isException = $(err.responseText).filter('.exception').length > 0;
                 if (err.status >= 400 && err.status < 500 && isException) {
                     const { lightboxContentElement, destroyLightbox } = lightbox(null, () => {
                         reject({'source': 'lightbox'})
