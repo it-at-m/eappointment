1be7ab03945699d1bc422aeab7fdc02b999cc988
Mathias Fischer <mathias.fischer@berlinonline.de>
2017-03-24 16:07:14 +0100

    refs #30009 - Mail and Notification to status schema

--- src/Zmsentities/Status.php
+++ src/Zmsentities/Status.php
@@ -6,4 +6,33 @@ class Status extends Schema\Entity
 {
     const PRIMARY = 'version';
     public static $schema = "status.json";
+
+    public function getDefaults()
+    {
+        return [
+            'database' => array (
+                'nodeConnections' => 0.0,
+                'clusterStatus' => 'OFF',
+                'logbin' => 'OFF',
+            ),
+            'processes' => array (
+                'blocked' => 0,
+                'confirmed' => 0,
+                'deleted' => 0,
+                'missed' => 0,
+                'reserved' => 0,
+                'lastInsert' => 0,
+            ),
+            'mail' => array (
+                'queueCount' => 0,
+                'oldestSeconds' => 0,
+                'newestSeconds' => 0,
+            ),
+            'notification' => array (
+                'queueCount' => 0,
+                'oldestSeconds' => 0,
+                'newestSeconds' => 0,
+            ),
+        ];
+    }
 }
