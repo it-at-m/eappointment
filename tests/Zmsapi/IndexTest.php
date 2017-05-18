6a6df484eedbe1dd9ceb910bf045b3a9696584b6
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-05-18 11:55:18 +0200

    ref #29759 - getNow tests from Application

--- tests/Zmsapi/IndexTest.php
+++ tests/Zmsapi/IndexTest.php
@@ -11,4 +11,16 @@ class IndexTest extends Base
         $response = $this->render([ ], [ ], [ ]);
         $this->assertContains('swagger.json', (string) $response->getBody());
     }
+
+    public function testNow()
+    {
+        $now = \App::getNow();
+        $this->assertEquals('2016-04-01', $now->format('Y-m-d'));
+
+        \App::$now = null;
+        $dateTime = \App::getNow();
+        $this->assertTrue('2016-04-01' !== $dateTime->format('Y-m-d'));
+
+        \App::$now = $now;
+    }
 }
