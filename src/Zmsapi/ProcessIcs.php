a61f7aab8f2a9acdc4f8a4f76b3e4990f5d39830
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-05-22 12:39:46 +0200

    ref #29759 - process get ics psr7 with unit tests

--- src/Zmsapi/ProcessIcs.php
+++ src/Zmsapi/ProcessIcs.php
@@ -1,40 +1,47 @@
 <?php
 /**
- * @package Zmsappointment
+ * @package ZMS API
  * @copyright BerlinOnline Stadtportal GmbH & Co. KG
  **/
 
 namespace BO\Zmsapi;
 
 use \BO\Slim\Render;
-use \BO\Zmsdb\Process as Query;
-use \BO\Zmsdb\Config as Config;
+use \BO\Zmsdb\Process;
+use \BO\Zmsdb\Config;
 
-/**
-  * Handle requests concerning services
-  */
 class ProcessIcs extends BaseController
 {
     /**
+     * @SuppressWarnings(Param)
      * @return String
      */
-    public static function render($itemId, $authKey)
+    public function readResponse(
+        \Psr\Http\Message\RequestInterface $request,
+        \Psr\Http\Message\ResponseInterface $response,
+        array $args
+    ) {
+        $this->testProcessData($args['id'], $args['authKey']);
+        $process = (new Process)->readEntity($args['id'], $args['authKey'], 1);
+
+        $config = (new Config())->readEntity();
+        $ics = \BO\Zmsentities\Helper\Messaging::getMailIcs($process, $config);
+
+        $message = Response\Message::create($request);
+        $message->data = $ics;
+
+        $response = Render::withLastModified($response, time(), '0');
+        $response = Render::withJson($response, $message->setUpdatedMetaData(), $message->getStatuscode());
+        return $response;
+    }
+
+    protected function testProcessData($processId, $authKey)
     {
-        $query = new Query();
-        $message = Response\Message::create(Render::$request);
-        $process = $query->readEntity($itemId, $authKey, 2);
-        $authCheck = $query->readAuthKeyByProcessId($itemId);
+        $authCheck = (new Process())->readAuthKeyByProcessId($processId);
         if (! $authCheck) {
             throw new Exception\Process\ProcessNotFound();
         } elseif ($authCheck['authKey'] != $authKey && $authCheck['authName'] != $authKey) {
             throw new Exception\Process\AuthKeyMatchFailed();
-        } else {
-            $config = (new Config())->readEntity();
-            $ics = \BO\Zmsentities\Helper\Messaging::getMailIcs($process, $config);
-            $message->data = $ics;
         }
-
-        Render::lastModified(time(), '0');
-        Render::json($message->setUpdatedMetaData(), $message->getStatuscode());
     }
 }
