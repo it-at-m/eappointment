95f93002d2f9c51a66e9ba4b7540acf43181503d
Mathias Fischer <mathias.fischer@berlinonline.de>
2017-05-22 15:30:13 +0200

    refs #31772 - some MySQL indice added

---
+++ migrations/31772-optimization.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `sms` ADD INDEX `BehoerdenID` (`BehoerdenID`);
+ALTER TABLE `email` ADD INDEX `BehoerdenID` (`BehoerdenID`);
+ALTER TABLE `provider` ADD INDEX `id` (`id`);
+ALTER TABLE `request` ADD INDEX `id` (`id`);
+ALTER TABLE `nutzer` ADD INDEX `Name` (`Name`);
+ALTER TABLE `buerger` ADD INDEX `NutzerID` (`NutzerID`);
+ALTER TABLE `kundenlinks` ADD INDEX `behoerdenid` (`behoerdenid`);
+ALTER TABLE `imagedata` ADD INDEX `behoerdenid` (`behoerdenid`);
