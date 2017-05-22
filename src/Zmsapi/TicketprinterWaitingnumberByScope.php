632f81d6c523657dcfdce27bd157426102bce841
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-05-22 09:48:18 +0200

    ref #29759 - ticketprinter waitingnumber bei scope psr7 with unit tests

--- src/Zmsapi/TicketprinterWaitingnumberByScope.php
+++ src/Zmsapi/TicketprinterWaitingnumberByScope.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package 115Mandant
+ * @package ZMS API
  * @copyright BerlinOnline Stadtportal GmbH & Co. KG
  **/
 
@@ -12,38 +12,38 @@ use \BO\Zmsdb\Ticketprinter as Query;
 use \BO\Zmsdb\Scope;
 use \BO\Zmsdb\ProcessStatusQueued;
 
-/**
-  * Handle requests concerning services
-  */
 class TicketprinterWaitingnumberByScope extends BaseController
 {
     /**
+     * @SuppressWarnings(Param)
      * @return String
      */
-    public static function render($scopeId, $hash)
-    {
-        $message = Response\Message::create(Render::$request);
-        $ticketprinter = (new Query())->readByHash($hash);
+    public function readResponse(
+        \Psr\Http\Message\RequestInterface $request,
+        \Psr\Http\Message\ResponseInterface $response,
+        array $args
+    ) {
+        $ticketprinter = (new Query())->readByHash($args['hash']);
 
         if (! $ticketprinter->hasId()) {
-            throw new Exception\Ticketprinter\TicketprinterNotFound();
+            throw new Exception\Ticketprinter\TicketprinterHashNotValid();
         }
         if (! $ticketprinter->isEnabled()) {
             throw new Exception\Ticketprinter\TicketprinterNotEnabled();
         }
 
-        $scope = (new Scope())->readEntity($scopeId, 0);
-        if (! $scope->hasId()) {
+        $scope = (new Scope())->readEntity($args['id'], 0);
+        if (! $scope) {
             throw new Exception\Scope\ScopeNotFound();
         }
 
         $process = ProcessStatusQueued::init()->writeNewFromTicketprinter($scope, \App::$now);
-        if (! $process->hasId()) {
-            throw new Exception\Process\ProcessReserveFailed();
-        }
 
+        $message = Response\Message::create($request);
         $message->data = $process;
-        Render::lastModified(time(), '0');
-        Render::json($message->setUpdatedMetaData(), $message->getStatuscode());
+
+        $response = Render::withLastModified($response, time(), '0');
+        $response = Render::withJson($response, $message->setUpdatedMetaData(), $message->getStatuscode());
+        return $response;
     }
 }
---
+++ tests/Zmsapi/TicketprinterWaitingnumberByScopeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BO\Zmsapi\Tests;
+
+class TicketprinterWaitingnumberByScopeTest extends Base
+{
+    protected $classname = "TicketprinterWaitingnumberByScope";
+
+    public function testRendering()
+    {
+        //Schöneberg with test scope ghostWorkstationCount of 3
+        $response = $this->render(['id' => 146, 'hash' => 'ac9df1f2983c3f94aebc1a9bd121bfecf5b374f2'], [], []);
+        $this->assertContains('process.json', (string)$response->getBody());
+        $this->assertContains('"id":"146"', (string)$response->getBody());
+        $this->assertTrue(200 == $response->getStatusCode());
+    }
+
+    public function testScopeFailed()
+    {
+        $this->expectException('BO\Zmsapi\Exception\Scope\ScopeNotFound');
+        $this->expectExceptionCode(404);
+        $this->render(['id' => 999, 'hash' => 'ac9df1f2983c3f94aebc1a9bd121bfecf5b374f2'], [], []);
+    }
+
+    public function testUnvalidHash()
+    {
+        $this->expectException('\BO\Zmsapi\Exception\Ticketprinter\TicketprinterHashNotValid');
+        $this->expectExceptionCode(403);
+        $this->render(['id' => 141, 'hash' => '123456687890'], [], []);
+    }
+}
