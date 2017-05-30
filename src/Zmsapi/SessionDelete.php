9c79989f0287f6d29eda55aeb6c947c82b84c577
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-05-30 15:57:04 +0200

    ref #29759 - session psr7 with unit tests

--- src/Zmsapi/SessionDelete.php
+++ src/Zmsapi/SessionDelete.php
@@ -1,36 +1,35 @@
 <?php
-
 /**
  * @package ZMS API
  * @copyright BerlinOnline Stadtportal GmbH & Co. KG
  **/
+
 namespace BO\Zmsapi;
 
 use \BO\Slim\Render;
-use \BO\Zmsdb\Session as Query;
+use \BO\Zmsdb\Session;
 
-/**
- * Handle requests concerning services
- */
 class SessionDelete extends BaseController
 {
-
     /**
-     *
+     * @SuppressWarnings(Param)
      * @return String
      */
-    public static function render($sessionName, $sessionId)
-    {
-        $message = Response\Message::create(Render::$request);
-        $query = new Query();
-        $session = $query->readEntity($sessionName, $sessionId);
-        if (! $session || ! $query->deleteEntity($sessionName, $sessionId)) {
+    public function readResponse(
+        \Psr\Http\Message\RequestInterface $request,
+        \Psr\Http\Message\ResponseInterface $response,
+        array $args
+    ) {
+        $session = (new Session)->readEntity($args['name'], $args['id']);
+        if (! $session || ! (new Session)->deleteEntity($args['name'], $args['id'])) {
             throw new Exception\Session\SessionDeleteFailed();
         }
 
+        $message = Response\Message::create($request);
         $message->data = $session;
 
-        Render::lastModified(time(), '0');
-        Render::json($message->setUpdatedMetaData(), $message->getStatuscode());
+        $response = Render::withLastModified($response, time(), '0');
+        $response = Render::withJson($response, $message->setUpdatedMetaData(), $message->getStatuscode());
+        return $response;
     }
 }
--- src/Zmsapi/SessionUpdate.php
+++ src/Zmsapi/SessionUpdate.php
@@ -1,40 +1,47 @@
 <?php
-
 /**
  * @package ZMS API
  * @copyright BerlinOnline Stadtportal GmbH & Co. KG
  **/
+
 namespace BO\Zmsapi;
 
 use \BO\Slim\Render;
 use \BO\Mellon\Validator;
-use \BO\Zmsdb\Session as Query;
+use \BO\Zmsdb\Session;
 
 class SessionUpdate extends BaseController
 {
     /**
-     *
+     * @SuppressWarnings(Param)
      * @return String
      */
-    public static function render()
-    {
-        $message = Response\Message::create(Render::$request);
+    public function readResponse(
+        \Psr\Http\Message\RequestInterface $request,
+        \Psr\Http\Message\ResponseInterface $response,
+        array $args
+    ) {
         $input = Validator::input()->isJson()->assertValid()->getValue();
         $session = new \BO\Zmsentities\Session($input);
         $session->testValid();
-        $message->data = null;
+        $this->testMatching($session);
 
+        $message = Response\Message::create($request);
+        $message->data = (new Session())->updateEntity($session);
+
+        $response = Render::withLastModified($response, time(), '0');
+        $response = Render::withJson($response, $message->setUpdatedMetaData(), $message->getStatuscode());
+        return $response;
+    }
+
+    protected function testMatching($session)
+    {
         if (false === Helper\Matching::isProviderExisting($session)) {
             throw new Exception\Matching\ProviderNotFound();
         } elseif (false === Helper\Matching::isRequestExisting($session)) {
             throw new Exception\Matching\RequestNotFound();
         } elseif (false === Helper\Matching::hasProviderRequest($session)) {
             throw new Exception\Matching\MatchingNotFound();
-        } else {
-            $message->data = (new Query())->updateEntity($session);
         }
-
-        Render::lastModified(time(), '0');
-        Render::json($message->setUpdatedMetaData(), $message->getStatuscode());
     }
 }
