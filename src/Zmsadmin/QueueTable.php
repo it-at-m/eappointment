51df563c293859db054a1c12f07be037e6dc5c03
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-06-15 10:05:17 +0200

    ref #31963 - ignore cluster not found exception while reading queuetable, refactoring controller

--- src/Zmsadmin/QueueTable.php
+++ src/Zmsadmin/QueueTable.php
@@ -22,32 +22,10 @@ class QueueTable extends BaseController
         array $args
     ) {
         $workstation = \App::$http->readGetResult('/workstation/', ['resolveReferences' => 1])->getEntity();
-
+        $department = \App::$http->readGetResult('/scope/'. $workstation->scope['id'] .'/department/')->getEntity();
         $validator = $request->getAttribute('validator');
         $selectedDate = $validator->getParameter('selecteddate')->isString()->getValue();
-        $cluster = \App::$http->readGetResult('/scope/'. $workstation->scope['id'] .'/cluster/')->getEntity();
-        $processList = new ProcessList();
-
-        if (1 == $workstation->queue['clusterEnabled']) {
-            $resultList = \App::$http
-                ->readGetResult('/cluster/'. $cluster->id .'/process/'. $selectedDate .'/')->getCollection();
-        } else {
-            $resultList = \App::$http
-                ->readGetResult('/scope/'. $workstation->scope['id'] .'/process/'. $selectedDate .'/')->getCollection();
-        }
-        $processList = ($resultList) ? $resultList : $processList;
-        $department = \App::$http->readGetResult('/scope/'. $workstation->scope['id'] .'/department/')->getEntity();
-
-        $selectedDateTime = new \DateTimeImmutable($selectedDate);
-        $queueList = $processList
-            ->toQueueList($selectedDateTime)
-            ->withStatus(array('confirmed', 'queued', 'reserved'))
-            ->withSortedArrival();
-
-        $queueListMissed = $processList
-            ->toQueueList($selectedDateTime)
-            ->withStatus(array('missed'))
-            ->withSortedArrival();
+        $cluster = $this->readCluster($workstation);
 
         return \BO\Slim\Render::withHtml(
             $response,
@@ -57,10 +35,47 @@ class QueueTable extends BaseController
                 'department' => $department,
                 'source' => $workstation->getRedirect(),
                 'selectedDate' => ($selectedDate) ? $selectedDate : \App::$now->format('Y-m-d'),
-                'cluster' => ($cluster) ? $cluster : null,
-                'processList' => $queueList->toProcessList(),
-                'processListMissed' => $queueListMissed->toProcessList()
+                'cluster' => $cluster,
+                'processList' => $this->getQueueList($workstation, $selectedDate, ['confirmed', 'queued', 'reserved']),
+                'processListMissed' => $this->getQueueList($workstation, $selectedDate, ['missed'])
             )
         );
+    }
+
+    protected function readProcessList($workstation, $selectedDate)
+    {
+        if (1 == $workstation->queue['clusterEnabled'] && $cluster) {
+            $processList = \App::$http
+                ->readGetResult('/cluster/'. $cluster->id .'/process/'. $selectedDate .'/')->getCollection();
+        } else {
+            $processList = \App::$http
+                ->readGetResult('/scope/'. $workstation->scope['id'] .'/process/'. $selectedDate .'/')->getCollection();
+        }
+        return $processList;
+    }
+
+    protected function getQueueList($workstation, $selectedDate, $status)
+    {
+        $processList = $this->readProcessList($workstation, $selectedDate);
+        $selectedDateTime = new \DateTimeImmutable($selectedDate);
+        $queueList = $processList
+            ->toQueueList($selectedDateTime)
+            ->withStatus($status)
+            ->withSortedArrival();
+        return $queueList->toProcessList();
+    }
+
+    // ignore cluster not found exception for queue table view
+    protected function readCluster($workstation)
+    {
+        $cluster = null;
+        try {
+            $cluster = \App::$http->readGetResult('/scope/'. $workstation->scope['id'] .'/cluster/')->getEntity();
+        } catch (\BO\Zmsclient\Exception $exception) {
+            if ($exception->template != 'BO\Zmsapi\Exception\Cluster\ClusterNotFound') {
+                throw $exception;
+            }
+        }
+        return $cluster;
     }
 }
