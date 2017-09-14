7348c38b67c6e4ac08f0eeddcd3b5bc29a8e8340
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-09-14 10:45:44 +0200

    ref #29541 - unit tests 100% with json schema validation

--- tests/Zmsentities/ValidationTest.php
+++ tests/Zmsentities/ValidationTest.php
@@ -17,6 +17,22 @@ class ValidationTest extends Base
         }
     }
 
+    public function testTestValidObject()
+    {
+        $entity = (new \BO\Zmsentities\Scope())->getExample();
+        $entity->preferences['client']['emailFrom'] = "test.de";
+        try {
+            $entity->testValid();
+            $this->fail("Expected exception SchemaValidation not thrown");
+        } catch (\BO\Zmsentities\Exception\SchemaValidation $exception) {
+            $this->assertContains(
+                'Die E-Mail Adresse muss eine valide E-Mail im Format max@mustermann.de sein',
+                $exception->getMessage()
+            );
+            $this->assertEquals(400, $exception->getCode());
+        }
+    }
+
     public function testLocale()
     {
         $entity = new \BO\Zmsentities\Useraccount();
