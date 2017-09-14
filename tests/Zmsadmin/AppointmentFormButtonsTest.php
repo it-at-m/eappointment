48e9ba3e255a971b82cab0a8c6dc827fa54c4d6d
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-09-14 10:59:28 +0200

    ref #ref #31721 - unit test appointment form buttons

---
+++ tests/Zmsadmin/AppointmentFormButtonsTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace BO\Zmsadmin\Tests;
+
+class AppointmentFormButtonsTest extends Base
+{
+    protected $arguments = [];
+
+    protected $parameters = [];
+
+    protected $classname = "AppointmentFormButtons";
+
+    public function testRendering()
+    {
+        \App::$now = new \DateTimeImmutable('2016-04-01 11:55:00', new \DateTimeZone('Europe/Berlin'));
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'parameters' => ['resolveReferences' => 2],
+                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
+                ]
+            ]
+        );
+        $response = $this->render([], ['selecteddate' => '2016-04-01'], []);
+        $this->assertContains('Spontankunde', (string)$response->getBody());
+    }
+
+    public function testWithSelectedDate()
+    {
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'parameters' => ['resolveReferences' => 2],
+                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
+                ]
+            ]
+        );
+        $response = $this->render([], ['selecteddate' => '2016-05-27'], []);
+        $this->assertContains('Termin reservieren', (string)$response->getBody());
+    }
+
+    public function testWithSelectedProcess()
+    {
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'parameters' => ['resolveReferences' => 2],
+                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/process/100044/',
+                    'response' => $this->readFixture("GET_process_100044_57c2.json")
+                ]
+            ]
+        );
+        $response = $this->render([], ['selectedprocess' => '100044'], []);
+        $this->assertContains('Speichern', (string)$response->getBody());
+        $this->assertContains('Löschen', (string)$response->getBody());
+        $this->assertContains('Wartenummer drucken', (string)$response->getBody());
+        $this->assertContains('als neu hinzufügen', (string)$response->getBody());
+        $this->assertContains('Abbrechen', (string)$response->getBody());
+    }
+}
