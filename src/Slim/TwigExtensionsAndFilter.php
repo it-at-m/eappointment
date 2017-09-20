f22f6a708a96cb6d67bc8a3178820c1ab27e2829
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-09-20 11:57:22 +0200

    ref #31526 - trim whitespace and transform linebreaks to html

--- src/Slim/TwigExtensionsAndFilter.php
+++ src/Slim/TwigExtensionsAndFilter.php
@@ -23,9 +23,8 @@ class TwigExtensionsAndFilter extends TwigExtension
 
     public function decodeEntities($string)
     {
-        return html_entity_decode($string);
+        return trim(nl2br(html_entity_decode($string)));
     }
-
 
     public function msort($array, $key, $sort_flags = SORT_REGULAR)
     {
