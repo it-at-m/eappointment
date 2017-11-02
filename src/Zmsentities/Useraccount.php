9565e6c313620e02105c785cbc02f33cb7b6d0b1
Torsten Kunst <torsten.kunst@berlinonline.de>
2017-11-02 13:16:35 +0100

    ref #31382 - remove isOveraged test on testRights because of recursive test problems

--- src/Zmsentities/Useraccount.php
+++ src/Zmsentities/Useraccount.php
@@ -133,9 +133,9 @@ class Useraccount extends Schema\Entity
         return true;
     }
 
-    public function testRights(array $requiredRights, \DateTimeInterface $dateTime)
+    public function testRights(array $requiredRights)
     {
-        if ($this->hasId() && false === $this->isOveraged($dateTime)) {
+        if ($this->hasId()) {
             if (!$this->hasRights($requiredRights)) {
                 throw new Exception\UserAccountMissingRights(
                     "Missing rights " . htmlspecialchars(implode(',', $requiredRights))
