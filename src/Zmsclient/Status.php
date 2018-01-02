82647a50ea0716cefe8c28ce22f95b2ce8778eba
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-01-02 15:44:35 +0100

    Added option for status as closure to catch exceptions

--- src/Zmsclient/Status.php
+++ src/Zmsclient/Status.php
@@ -12,18 +12,28 @@ class Status
      */
     public static function testStatus($response, $status)
     {
-        if ($status['mail']['oldestSeconds'] > 300) {
-            $result = "WARN - Oldest mail with age in seconds: " . $status['mail']['oldestSeconds'];
-        } elseif ($status['notification']['oldestSeconds'] > 300) {
-            $result = "WARN - Oldest sms with age in seconds: " . $status['notification']['oldestSeconds'];
-        } elseif ($status['database']['logbin'] != 'ON') {
-            $result = "WARN - DB connection without replication log detected";
-        } elseif ($status['database']['clusterStatus'] == 'OFF') {
-            $result = "WARN - DB connection is not part of a galera cluster";
-        } elseif ($status['database']['nodeConnections'] > 50) {
-            $result = "WARN - DB connected thread over 50% of available connections";
-        } else {
-            $result = "OK - DB=" . $status['database']['nodeConnections'] . "%";
+        if ($status instanceof \Closure) {
+            try {
+                $status = $status();
+            } catch (\Exception $exception) {
+                $status = false;
+                $result = "FATAL - " . $exception->getMessage();
+            }
+        }
+        if ($status) {
+            if ($status['mail']['oldestSeconds'] > 300) {
+                $result = "WARN - Oldest mail with age in seconds: " . $status['mail']['oldestSeconds'];
+            } elseif ($status['notification']['oldestSeconds'] > 300) {
+                $result = "WARN - Oldest sms with age in seconds: " . $status['notification']['oldestSeconds'];
+            } elseif ($status['database']['logbin'] != 'ON') {
+                $result = "WARN - DB connection without replication log detected";
+            } elseif ($status['database']['clusterStatus'] == 'OFF') {
+                $result = "WARN - DB connection is not part of a galera cluster";
+            } elseif ($status['database']['nodeConnections'] > 50) {
+                $result = "WARN - DB connected thread over 50% of available connections";
+            } else {
+                $result = "OK - DB=" . $status['database']['nodeConnections'] . "%";
+            }
         }
         $response->getBody()->write($result);
         $response = $response->withHeader('Content-Type', 'text/plain');
