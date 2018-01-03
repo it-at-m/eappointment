8c14621a7f1e16a6120aaa31379616e7ea5f3908
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-01-03 15:06:56 +0100

    Set 404 HTTP on missing scopes

--- src/Zmsdb/Exception/CalendarWithoutScopes.php
+++ src/Zmsdb/Exception/CalendarWithoutScopes.php
@@ -4,5 +4,7 @@ namespace BO\Zmsdb\Exception;
 
 class CalendarWithoutScopes extends \Exception
 {
+    protected $code = 404;
 
+    protected $message = "No matching scopes found for given location(s)";
 }
