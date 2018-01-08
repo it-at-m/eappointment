cb36db141dc91064a7e0fb3663d5a8910512bb73
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-01-08 14:21:21 +0100

    Added migrations into testdata-Set for docker

--- migrations/32694-migrate-user-assignment.sql
+++ migrations/32694-migrate-user-assignment.sql
@@ -2,4 +2,4 @@ INSERT INTO nutzerzuordnung (nutzerid, behoerdenid)
 (SELECT n.NutzerID, n.BehoerdenID
 FROM nutzer n
   LEFT JOIN nutzerzuordnung nz ON n.NutzerID = nz.nutzerid
-	WHERE nz.nutzerid IS NULL)
+    WHERE nz.nutzerid IS NULL);
