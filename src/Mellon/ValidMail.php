0124b0311654ea8bb1c5a0e297061635056115ab
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-01-10 15:54:48 +0100

    Fixed VaildMail::hasDNS() with an ANY-Query

--- src/Mellon/ValidMail.php
+++ src/Mellon/ValidMail.php
@@ -34,7 +34,7 @@ class ValidMail extends \BO\Mellon\ValidString
         $this->validated = true;
         if ($this->value) {
             $domain = substr($this->value, strpos($this->value, '@') + 1);
-            $hasDNS = checkdnsrr($domain);
+            $hasDNS = checkdnsrr($domain, 'ANY');
             if (false === $hasDNS) {
                 $this->setFailure($message);
             }
