d61f375e23e3fb34d62c64a8e0b480f29a5038c9
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-01-17 14:19:02 +0100

    CSS fixes + improved login

--- src/Zmsstatistic/Index.php
+++ src/Zmsstatistic/Index.php
@@ -15,43 +15,60 @@ class Index extends BaseController
      * @SuppressWarnings(Param)
      * @return String
      */
-
     public function readResponse(
         \Psr\Http\Message\RequestInterface $request,
         \Psr\Http\Message\ResponseInterface $response,
         array $args
     ) {
+        try {
+            $workstation = \App::$http->readGetResult('/workstation/')->getEntity();
+        } catch (\Exception $workstationexception) {
+            $workstation = null;
+        }
         $form = LoginForm::fromLoginParameters();
         $validate = Validator::param('login_form_validate')->isBool()->getValue();
         $loginData = ($validate) ? $form->getStatus() : null;
         if ($loginData && !$form->hasFailed()) {
-            $userAccount = new \BO\Zmsentities\Useraccount(array(
-                'id' => $loginData['loginName']['value'],
-                'password' => $loginData['password']['value']
-            ));
-            try {
-                $workstation = \App::$http->readPostResult('/workstation/login/', $userAccount)->getEntity();
-            } catch (\BO\Zmsclient\Exception $exception) {
-                if ($exception->template == 'BO\Zmsapi\Exception\Useraccount\UserAlreadyLoggedIn') {
-                    \BO\Zmsclient\Auth::setKey($exception->data['authkey']);
-                    throw $exception;
-                } elseif ($exception->template == 'BO\Zmsapi\Exception\Useraccount\AuthKeyFound') {
-                    throw $exception;
-                } elseif ($exception->template == 'BO\Zmsapi\Exception\Useraccount\InvalidCredentials') {
-                    $exception->data['source'] = 'login';
-                    throw $exception;
-                }
-            }
-            if (array_key_exists('authkey', $workstation)) {
-                \BO\Zmsclient\Auth::setKey($workstation->authkey);
-                return \BO\Slim\Render::redirect('workstationSelect', array(), array());
-            }
+            return $this->testLogin($loginData, $response);
         }
         return \BO\Slim\Render::withHtml(
             $response,
             'page/index.twig',
             array(
                 'title' => 'Anmeldung',
+                'workstation' => $workstation,
+                'loginData' => $loginData
+            )
+        );
+    }
+
+    protected function testLogin($loginData, $response)
+    {
+        $userAccount = new \BO\Zmsentities\Useraccount(array(
+            'id' => $loginData['loginName']['value'],
+            'password' => $loginData['password']['value']
+        ));
+        try {
+            $workstation = \App::$http->readPostResult('/workstation/login/', $userAccount)->getEntity();
+            if (array_key_exists('authkey', $workstation)) {
+                \BO\Zmsclient\Auth::setKey($workstation->authkey);
+                return \BO\Slim\Render::redirect('workstationSelect', array(), array());
+            }
+        } catch (\BO\Zmsclient\Exception $exception) {
+            if ($exception->template == 'BO\Zmsapi\Exception\Useraccount\UserAlreadyLoggedIn') {
+                \BO\Zmsclient\Auth::setKey($exception->data['authkey']);
+                throw $exception;
+            } elseif ($exception->template == 'BO\Zmsapi\Exception\Useraccount\AuthKeyFound') {
+                throw $exception;
+            }
+        }
+        return \BO\Slim\Render::withHtml(
+            $response,
+            'page/index.twig',
+            array(
+                'title' => 'Anmeldung gescheitert',
+                'loginfailed' => true,
+                'workstation' => null,
                 'loginData' => $loginData
             )
         );
