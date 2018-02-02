a39b6850fe37e5d68da5be6ae3cdac7934f5f1c2
Torsten Kunst <torsten.kunst@berlinonline.de>
2018-02-02 13:01:10 +0100

    ref #33246 - add limit paramter to process search api call, add lessResolveData paramter to return processList with less data

--- src/Zmsapi/ProcessSearch.php
+++ src/Zmsapi/ProcessSearch.php
@@ -23,10 +23,15 @@ class ProcessSearch extends BaseController
     ) {
         (new Helper\User($request))->checkRights();
         $resolveReferences = Validator::param('resolveReferences')->isNumber()->setDefault(0)->getValue();
+        $lessResolvedData = Validator::param('lessResolvedData')->isNumber()->setDefault(0)->getValue();
+        $limit = Validator::param('limit')->isNumber()->setDefault(100)->getValue();
         $queryString = Validator::param('query')
             ->isString()
             ->getValue();
-        $processList = (new Process)->readSearch($queryString, $resolveReferences);
+        $processList = (new Process)->readSearch($queryString, $resolveReferences, $limit);
+        if ($lessResolvedData) {
+            $processList = $processList->withLessData();
+        }
 
         $message = Response\Message::create($request);
         $message->data = $processList;
