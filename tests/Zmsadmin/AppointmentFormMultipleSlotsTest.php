5f77a1c15d487871b53161d99e68d01c37d26508
Torsten Kunst <torsten.kunst@berlinonline.de>
2018-04-16 12:49:46 +0200

    refs #31454 - renamed Passwortschutz to Zugriffsschutz and moved in 2nd fieldset

--- tests/Zmsadmin/AppointmentFormMultipleSlotsTest.php
+++ tests/Zmsadmin/AppointmentFormMultipleSlotsTest.php
@@ -2,7 +2,7 @@
 
 namespace BO\Zmsadmin\Tests;
 
-class AppointmentFormTest extends Base
+class AppointmentFormMultipleSlotsTest extends Base
 {
     protected $arguments = [];
 
@@ -44,6 +44,6 @@ class AppointmentFormTest extends Base
             ]
         );
         $response = parent::testRendering();
-        $this->assertContains('Ameldung einer Wohnung (0)', (string)$response->getBody());
+        $this->assertContains('Anmeldung einer Wohnung (0)', (string)$response->getBody());
     }
 }
