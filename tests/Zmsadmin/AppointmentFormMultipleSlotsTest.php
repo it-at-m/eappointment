ef6ac7e2de628060d7773f0604703360164c9bf3
Torsten Kunst <torsten.kunst@berlinonline.de>
2018-04-16 11:35:06 +0200

    refs #31392, #33789 - display request timeSlotCount if multipeSlotcount enabled, modify week range from monday to sunday

---
+++ tests/Zmsadmin/AppointmentFormMultipleSlotsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BO\Zmsadmin\Tests;
+
+class AppointmentFormMultipleSlotsTest extends Base
+{
+    protected $arguments = [];
+
+    protected $parameters = [];
+
+    protected $classname = "AppointmentForm";
+
+    public function testRendering()
+    {
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'parameters' => ['resolveReferences' => 2],
+                    'response' => $this->readFixture("GET_Workstation_scope_141_multipleSlotsEnabled.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/cluster/',
+                    'response' => $this->readFixture("GET_cluster_109.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/department/',
+                    'response' => $this->readFixture("GET_department_74.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/request/',
+                    'response' => $this->readFixture("GET_scope_141_requestlist.json")
+                ],
+                [
+                    'function' => 'readPostResult',
+                    'url' => '/process/status/free/',
+                    'parameters' => ['slotType' => 'intern', 'slotsRequired' => 0, 'keepLessData' => ['availability']],
+                    'response' => $this->readFixture("GET_freeprocesslist_empty.json")
+                ]
+            ]
+        );
+        $response = parent::testRendering();
+        $this->assertContains('Anmeldung einer Wohnung (0)', (string)$response->getBody());
+    }
+}
