701d9f40b8f944414570b407875f5e0876a460fc
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-05-08 10:21:26 +0200

    Set importTestData to midnight or tests will fail

--- src/Zmsdb/Helper/CalculateSlots.php
+++ src/Zmsdb/Helper/CalculateSlots.php
@@ -21,7 +21,8 @@ class CalculateSlots
     {
         if ($this->verbose) {
             $time = round(microtime(true) - $this->startTime, 3);
-            error_log(sprintf("[CalculateSlots %07.3f] %s", "$time", $message));
+            $memory = memory_get_usage()/(1024*1024);
+            error_log(sprintf("[CalculateSlots %07.3fs %07.1fmb] %s", "$time", $memory, $message));
         }
         return $this;
     }
@@ -29,26 +30,34 @@ class CalculateSlots
     public function writeCalculations(\DateTimeInterface $now)
     {
         \BO\Zmsdb\Connection\Select::setTransaction();
+        $this->log("Fetch Slot list");
         $scopeList = (new \BO\Zmsdb\Scope())->readList(1);
-        $slotQuery = new \BO\Zmsdb\Slot();
         foreach ($scopeList as $scope) {
-            $this->log("Calculate slots for $scope");
-            $availabilityList = (new \BO\Zmsdb\Availability)
-                ->readList($scope->id)
-                ->withType('appointment')
-                ;
-            foreach ($availabilityList as $availability) {
-                $availability->scope = $scope; //dayoff is required
-                //$this->log("$availability");
-                if ($slotQuery->writeByAvailability($availability, $now)) {
-                    $this->log("Updated $availability");
-                }
-            }
+            $this->writeCalculatedScope($scope, $now);
         }
         $this->log("Update Slot-Process-Mapping");
+        $slotQuery = new \BO\Zmsdb\Slot();
         $slotQuery->updateSlotProcessMapping();
         $this->log("Commit changes (may take a while)");
         \BO\Zmsdb\Connection\Select::writeCommit();
         $this->log("Slot calculation finished");
+    }
+
+    protected function writeCalculatedScope(\BO\Zmsentities\Scope $scope, \DateTimeInterface $now)
+    {
+        $slotQuery = new \BO\Zmsdb\Slot();
+        $this->log("Calculate slots for $scope");
+        $availabilityList = (new \BO\Zmsdb\Availability)
+            ->readList($scope->id)
+            ->withType('appointment')
+            ;
+        foreach ($availabilityList as $availability) {
+            $availability->scope = clone $scope; //dayoff is required
+            //$this->log("$availability");
+            if ($slotQuery->writeByAvailability($availability, $now)) {
+                $this->log("Updated $availability");
+                \BO\Zmsdb\Connection\Select::writeCommit();
+            }
+        }
     }
 }
