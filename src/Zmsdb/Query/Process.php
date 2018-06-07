18139c89000611d4533782a50e7b9f22bda70114
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-06-07 11:20:38 +0200

    FOR UPDATE für nextid entfernt

--- src/Zmsdb/Query/Process.php
+++ src/Zmsdb/Query/Process.php
@@ -63,7 +63,7 @@ class Process extends Base implements MappingInterface
             FROM `process_sequence` ps LEFT JOIN `' . self::getTablename() . '` p
                 ON ps.processId = p.BuergerID
             WHERE p.`BuergerID` IS NULL
-            LIMIT '.$random.',1 FOR UPDATE';
+            LIMIT '.$random.',1';
     }
 
     public function getLockProcessId()
