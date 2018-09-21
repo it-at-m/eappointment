25bd01fbc2f840085d16e32c204bf75cdcffdba1
Mathias Fischer <mathias.fischer@berlinonline.de>
2018-09-21 17:16:48 +0200

    Avoid running calculateSlots if another calculation is running, wait at least 15 minutes

--- src/Zmsdb/Helper/CalculateSlots.php
+++ src/Zmsdb/Helper/CalculateSlots.php
@@ -88,13 +88,21 @@ class CalculateSlots
     {
         \BO\Zmsdb\Connection\Select::setTransaction();
         \BO\Zmsdb\Connection\Select::getWriteConnection();
-        $this->log("Fetch Slot list with time ". $now->format('c'));
         $this->readForceVerbose();
-        $updateTimestamp = $this->readLastRun();
+        $this->log("Calculate with time ". $now->format('c'));
         if ($this->readCalculateSkip()) {
             $this->log("Skip calculation due to config setting status.calculateSlotsSkip");
             return false;
         }
+        $startTimestamp = (new \BO\Zmsdb\Config)->readProperty('status__calculateSlotsLastStart', true);
+        $updateTimestamp = (new \BO\Zmsdb\Config)->readProperty('status__calculateSlotsLastRun', false);
+        if ($startTimestamp > $updateTimestamp && ((strtotime($startTimestamp) + (60 * 15)) > $now->getTimestamp())) {
+            $this->log("Skip calculation, last start on $startTimestamp is not yet finished, waiting 15 minutes.");
+            return false;
+        }
+        (new \BO\Zmsdb\Config)->replaceProperty('status__calculateSlotsLastStart', $now->format('Y-m-d H:i:s'));
+        \BO\Zmsdb\Connection\Select::writeCommit();
+        $updateTimestamp = $this->readLastRun();
         if ($repair) {
             $updateTimestamp = '';
         }
