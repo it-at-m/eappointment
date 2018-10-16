947c8fa231a92d8f66b0d28959d471e605384524
Noa R <noa.rave@berlinonline.de>
2018-10-16 13:46:30 +0200

    refs #34942 - don't show backwards navigation from current date

--- js/page/availabilityDay/widgets/calendarNavigation.js
+++ js/page/availabilityDay/widgets/calendarNavigation.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const calendarNavigation = (links) => {
+
     return (
         <div className="calendar-navigation">
-            <a href={links.previousDay} title="Tag zurück" className="icon prev">◀</a>
+            {(links.previousDay >= links.sameDay) &&  
+                    <a href={links.previousDay} title="Tag zurück" className="icon prev">◀</a>
+            }
             <a href={links.sameDay} className="today">Heute</a>
             <a href={links.nextDay} title="Tag vor" className="icon next">▶</a> 
         </div>
