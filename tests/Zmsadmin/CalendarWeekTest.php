b02a8261c59d811e3515085d6a60957120c4c62b
Noa R <noa.rave@berlinonline.de>
2018-11-13 11:02:01 +0100

    refs #35102 - refactor next/last week arrows and add tests

--- tests/Zmsadmin/CalendarWeekTest.php
+++ tests/Zmsadmin/CalendarWeekTest.php
@@ -76,4 +76,65 @@ class CalendarWeekTest extends Base
         $this->assertContains('<span class="pid">184432</span>', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
     }
+
+    public function testYearChange()
+    {
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'parameters' => ['resolveReferences' => 2],
+                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/cluster/',
+                    'response' => $this->readFixture("GET_cluster_109.json")
+                ],
+                [
+                    'function' => 'readPostResult',
+                    'url' => '/process/status/free/',
+                    'parameters' => ['slotType' => 'intern', 'slotsRequired' => 0],
+                    'response' => $this->readFixture("GET_freeprocesslist_empty.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2016-12-26/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ],[
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2016-12-27/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ],[
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2016-12-28/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ],[
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2016-12-29/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2016-12-30/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2016-12-31/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/scope/141/process/2017-01-01/',
+                    'response' => $this->readFixture("GET_processList_141_20160401.json")
+                ]
+            ]
+        );
+        $response = $this->render(['year' => 2016, 'weeknr' => 52], $this->parameters, []);
+        $this->assertContains('/2017/1/', (string)$response->getBody());
+        $this->assertContains('/2016/51/', (string)$response->getBody());
+        $this->assertEquals(200, $response->getStatusCode());
+    }
 }
