70a36eb31499423adde56867ff5ecea146eea33d
Torsten Kunst <torsten.kunst@berlinonline.de>
2018-11-13 16:54:07 +0100

    remove console.log from availability renderBody jsx

--- js/page/availabilityDay/form/renderBody.js
+++ js/page/availabilityDay/form/renderBody.js
@@ -3,18 +3,18 @@ import React from 'react'
 import * as Inputs from '../../../lib/inputs'
 import Errors from './errors'
 const { Label, FormGroup, Controls, Description } = Inputs
-import {range} from '../../../lib/utils'
+import { range } from '../../../lib/utils'
 
 const renderBody = (data, errors, onChange, onSave, onPublish, onDelete) => {
     return (
         <div>
-            <Errors {...{ errors }}/>
+            <Errors {...{ errors }} />
             <form>
                 <fieldset>
                     <FormGroup>
                         <Label>Anmerkung</Label>
                         <Controls>
-                            <Inputs.Text name="description" value={data.description} {...{onChange}} />
+                            <Inputs.Text name="description" value={data.description} {...{ onChange }} />
                             <Description>(optionale Angabe zur Kennzeichnung des Termins)
                                 {data.id ? " Die ID der Öffnungszeit ist " + data.id : " Die Öffnungszeit hat noch keine ID"}
                             </Description>
@@ -24,14 +24,14 @@ const renderBody = (data, errors, onChange, onSave, onPublish, onDelete) => {
                         <Label>Typ</Label>
                         <Controls>
                             <Inputs.Select name="type"
-                                attributes={{disabled: data.id ? 'disabled' : null}}
+                                attributes={{ disabled: data.id ? 'disabled' : null }}
                                 value={data.type}
-                                {...{ onChange}}
+                                {...{ onChange }}
                                 options={[
-                                    {value: "", name: "--Bitte wählen--"},
-                                    {value: "openinghours", name: "Spontankunden"},
-                                    {value: "appointment", name: "Terminkunden"},
-                                ]} {...{ onChange }}/>
+                                    { value: "", name: "--Bitte wählen--" },
+                                    { value: "openinghours", name: "Spontankunden" },
+                                    { value: "appointment", name: "Terminkunden" },
+                                ]} {...{ onChange }} />
                         </Controls>
                     </FormGroup>
 
@@ -42,15 +42,15 @@ const renderBody = (data, errors, onChange, onSave, onPublish, onDelete) => {
                                 value={data.repeat}
                                 {...{ onChange }}
                                 options={[
-                                    {value: "0", name: "einmaliger Termin"},
-                                    {value: "-1", name: "jede Woche"},
-                                    {value: "-2", name: "alle 2 Wochen"},
-                                    {value: "-3", name: "alle 3 Wochen"},
-                                    {value: "1", name: "jede 1. Woche im Monat"},
-                                    {value: "2", name: "jede 2. Woche im Monat"},
-                                    {value: "3", name: "jede 3. Woche im Monat"},
-                                    {value: "4", name: "jede 4. Woche im Monat"},
-                                    {value: "5", name: "jede letzte Woche im Monat"}
+                                    { value: "0", name: "einmaliger Termin" },
+                                    { value: "-1", name: "jede Woche" },
+                                    { value: "-2", name: "alle 2 Wochen" },
+                                    { value: "-3", name: "alle 3 Wochen" },
+                                    { value: "1", name: "jede 1. Woche im Monat" },
+                                    { value: "2", name: "jede 2. Woche im Monat" },
+                                    { value: "3", name: "jede 3. Woche im Monat" },
+                                    { value: "4", name: "jede 4. Woche im Monat" },
+                                    { value: "5", name: "jede letzte Woche im Monat" }
                                 ]} />
                         </Controls>
                     </FormGroup>
@@ -63,72 +63,72 @@ const renderBody = (data, errors, onChange, onSave, onPublish, onDelete) => {
                                 inline={true}
                                 {...{ onChange }}
                                 boxes={[
-                                    {value: "monday", label: "Mo"},
-                                    {value: "tuesday", label: "Di"},
-                                    {value: "wednesday", label: "Mi"},
-                                    {value: "thursday", label: "Do"},
-                                    {value: "friday", label: "Fr"},
-                                    {value: "saturday", label: "Sa"},
-                                        {value: "sunday", label: "So"}
-                                    ]} />
-                            </Controls>
-                        </FormGroup>
+                                    { value: "monday", label: "Mo" },
+                                    { value: "tuesday", label: "Di" },
+                                    { value: "wednesday", label: "Mi" },
+                                    { value: "thursday", label: "Do" },
+                                    { value: "friday", label: "Fr" },
+                                    { value: "saturday", label: "Sa" },
+                                    { value: "sunday", label: "So" }
+                                ]} />
+                        </Controls>
+                    </FormGroup>
 
-                        <FormGroup>
-                            <Label>Uhrzeit:</Label>
-                            <Controls>
-                        von <Inputs.Time name="startTime" value={data.startTime} {...{ onChange}} /> Uhr
-                        bis <Inputs.Time name="endTime" value={data.endTime} {...{ onChange}} /> Uhr
+                    <FormGroup>
+                        <Label>Uhrzeit:</Label>
+                        <Controls>
+                            von <Inputs.Time name="startTime" value={data.startTime} {...{ onChange }} /> Uhr
+                        bis <Inputs.Time name="endTime" value={data.endTime} {...{ onChange }} /> Uhr
                             </Controls>
-                        </FormGroup>
+                    </FormGroup>
 
-                        <FormGroup>
-                            <Label>Datum:</Label>
-                            <Controls>
-                        Startdatum: <Inputs.Date name="startDate" value={data.startDate} {...{ onChange}}/>
-                        {" "}
-                        Enddatum: <Inputs.Date name="endDate" value={data.endDate} {...{ onChange }}/>
-                            </Controls>
-                        </FormGroup>
+                    <FormGroup>
+                        <Label>Datum:</Label>
+                        <Controls>
+                            Startdatum: <Inputs.Date name="startDate" value={data.startDate} {...{ onChange }} />
+                            {" "}
+                            Enddatum: <Inputs.Date name="endDate" value={data.endDate} {...{ onChange }} />
+                        </Controls>
+                    </FormGroup>
 
-                        <FormGroup>
-                            <Label>Zeitschlitz:</Label>
-                            <Controls>
-                                <Inputs.Text name="slotTimeInMinutes"
-                                    value={data.slotTimeInMinutes}
-                                    width="1"
-                                    attributes={{maxLength: 3}}
-                                    {...{ onChange }}/>
-                                Minuten Abstand zweier aufeinander folgender Termine
+                    <FormGroup>
+                        <Label>Zeitschlitz:</Label>
+                        <Controls>
+                            <Inputs.Text name="slotTimeInMinutes"
+                                value={data.slotTimeInMinutes}
+                                width="1"
+                                attributes={{ maxLength: 3 }}
+                                {...{ onChange }} />
+                            Minuten Abstand zweier aufeinander folgender Termine
                                 <Label>
-                                    Die Dienstleistungen dürfen mehr als einen Zeitschlitz beanspruchen:&nbsp;
+                                Die Dienstleistungen dürfen mehr als einen Zeitschlitz beanspruchen:&nbsp;
                                     <Inputs.Checkbox name="multipleSlotsAllowed"
-                                        checked={true == data.multipleSlotsAllowed} {...{ onChange }} />
-                                </Label>
-                            </Controls>
-                        </FormGroup>
+                                    checked={true == data.multipleSlotsAllowed} {...{ onChange }} />
+                            </Label>
+                        </Controls>
+                    </FormGroup>
 
-                        <FormGroup>
-                            <Label>Buchbar:</Label>
-                            <Controls>
-                                von&nbsp;
+                    <FormGroup>
+                        <Label>Buchbar:</Label>
+                        <Controls>
+                            von&nbsp;
                                 <Inputs.Text name="open_from"
-                                    width="1"
-                                    value={data.open_from}
-                                    attributes={{placeholder: data.scope.preferences.appointment.startInDaysDefault}}
-                                    {...{ onChange }}
-                                />
-                                &nbsp;bis&nbsp;
+                                width="1"
+                                value={data.open_from}
+                                attributes={{ placeholder: data.scope.preferences.appointment.startInDaysDefault }}
+                                {...{ onChange }}
+                            />
+                            &nbsp;bis&nbsp;
                                 <Inputs.Text name="open_to"
-                                    width="1"
-                                    value={data.open_to}
-                                    attributes={{placeholder: data.scope.preferences.appointment.endInDaysDefault}}
-                                    {...{ onChange }}
-                                />&nbsp;
-                                Tage im voraus
+                                width="1"
+                                value={data.open_to}
+                                attributes={{ placeholder: data.scope.preferences.appointment.endInDaysDefault }}
+                                {...{ onChange }}
+                            />&nbsp;
+                            Tage im voraus
                                 <Description>(Keine Eingabe = Einstellungen vom Standort übernehmen)</Description>
-                            </Controls>
-                        </FormGroup>
+                        </Controls>
+                    </FormGroup>
                 </fieldset>
 
                 <fieldset>
@@ -140,62 +140,61 @@ const renderBody = (data, errors, onChange, onSave, onPublish, onDelete) => {
                                 value={data.workstationCount_intern}
                                 {...{ onChange }}
                                 options={range(0, 50).map(n => {
-                                        return {
-                                            value: `${n}`,
-                                            name: `${n}`
-                                        }
-                                    })}/>
+                                    return {
+                                        value: `${n}`,
+                                        name: `${n}`
+                                    }
+                                })} />
                         </Controls>
                     </FormGroup>
 
                     {data.type !== 'openinghours' ?
-                    <FormGroup>
-                        <Label>Callcenter</Label>
-                        <Controls>
-                            <Inputs.Select name="workstationCount_callcenter"
-                                value={data.workstationCount_callcenter}
-                                {...{ onChange }}
-                                options={range(0, data.workstationCount_intern).map(n => {
+                        <FormGroup>
+                            <Label>Callcenter</Label>
+                            <Controls>
+                                <Inputs.Select name="workstationCount_callcenter"
+                                    value={data.workstationCount_callcenter}
+                                    {...{ onChange }}
+                                    options={range(0, data.workstationCount_intern).map(n => {
                                         return {
                                             value: `${n}`,
                                             name: `${n}`
                                         }
-                                    })}/>
-                            <Description>wieviele der insgesamt verfügbaren Terminarbeitsplätze sollen für das Callcenter zur Verfügung gestellt werden.</Description>
-                        </Controls>
-                    </FormGroup> : null }
+                                    })} />
+                                <Description>wieviele der insgesamt verfügbaren Terminarbeitsplätze sollen für das Callcenter zur Verfügung gestellt werden.</Description>
+                            </Controls>
+                        </FormGroup> : null}
 
                     {data.type !== 'openinghours' ?
-                    <FormGroup>
-                        <Label>Internet</Label>
-                        <Controls>
-                            <Inputs.Select name="workstationCount_public"
-                                value={data.workstationCount_public}
-                                {...{ onChange }}
-                                options={range(0, data.workstationCount_callcenter).map(n => {
+                        <FormGroup>
+                            <Label>Internet</Label>
+                            <Controls>
+                                <Inputs.Select name="workstationCount_public"
+                                    value={data.workstationCount_public}
+                                    {...{ onChange }}
+                                    options={range(0, data.workstationCount_callcenter).map(n => {
                                         return {
                                             value: `${n}`,
                                             name: `${n}`
                                         }
-                                    })}/>
-                            <Description>wieviele der insgesamt verfügbaren Terminarbeitsplätze sollen für das Internet zur Verfügung gestellt werden.</Description>
-                        </Controls>
-                    </FormGroup> : null }
+                                    })} />
+                                <Description>wieviele der insgesamt verfügbaren Terminarbeitsplätze sollen für das Internet zur Verfügung gestellt werden.</Description>
+                            </Controls>
+                        </FormGroup> : null}
                 </fieldset>
-                {console.log(data)}
                 <div className="form-actions">
-                  {data.id ?  <button className="button-delete" type="delete" value="delete" onClick={onDelete}>Löschen</button> :
-                   <button className="button-abort" id="availability-abort" value="abort">Abbrechen</button>}         
+                    {data.id ? <button className="button-delete" type="delete" value="delete" onClick={onDelete}>Löschen</button> :
+                        <button className="button-abort" id="availability-abort" value="abort">Abbrechen</button>}
                     <div className="right">
                         <button className={data.__modified ? "button-new btn--b3igicon" : "btn"}
                             type="save"
                             value="save"
-                            onClick={onSave}>{data.__modified? "+ merken und später aktivieren" : "Schließen"}</button>
+                            onClick={onSave}>{data.__modified ? "+ merken und später aktivieren" : "Schließen"}</button>
                         {data.__modified ?
-                        <button className="button-save"
-                            type="save"
-                            value="publish"
-                            onClick={onPublish}>Alle Änderungen aktivieren</button> : null }
+                            <button className="button-save"
+                                type="save"
+                                value="publish"
+                                onClick={onPublish}>Alle Änderungen aktivieren</button> : null}
                     </div>
                 </div>
             </form>
