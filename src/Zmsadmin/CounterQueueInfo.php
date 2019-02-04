41aad56328a3e68375f5483524ee84f1063982d9
Torsten Kunst <torsten.kunst@berlinonline.de>
2019-02-04 14:39:48 +0100

    refs #34481 - hide queue info details if selected date is not today

--- src/Zmsadmin/CounterQueueInfo.php
+++ src/Zmsadmin/CounterQueueInfo.php
@@ -35,7 +35,8 @@ class CounterQueueInfo extends BaseController
             'block/queue/info.twig',
             array(
                 'workstation' => $workstation,
-                'workstationInfo' => $workstationInfo
+                'workstationInfo' => $workstationInfo,
+                'selectedDate' => $selectedDate
             )
         );
     }
