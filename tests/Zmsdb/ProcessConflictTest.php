01fce622513ad0fd26030244e7bc601cc637df26
Torsten Kunst <torsten.kunst@berlinonline.de>
2019-02-19 15:23:32 +0100

    refs #35744 - add unit tests for conflicts without queued processes

--- tests/Zmsdb/ProcessConflictTest.php
+++ tests/Zmsdb/ProcessConflictTest.php
@@ -19,7 +19,25 @@ class ProcessConflictTest extends Base
         $endDate = new \DateTimeImmutable("2016-04-30 23:59");
         $scope = (new \BO\Zmsdb\Scope())->readEntity(141, 1, true);
         $conflictList = (new \BO\Zmsdb\Process())->readConflictListByScopeAndTime($scope, $startDate, $endDate, 0);
-        $this->assertEquals(10, $conflictList->count());
+        $this->assertEquals(9, $conflictList->count());
+    }
+
+    public function testWithoutQueued()
+    {
+        $startDate = new \DateTimeImmutable("2016-04-01 08:55");
+        $endDate = new \DateTimeImmutable("2016-04-30 23:59");
+        $now = new \DateTimeImmutable("2016-04-01 11:55");
+        $query = new \BO\Zmsdb\ProcessStatusQueued();
+        $entity = (new \BO\Zmsentities\Process())->getExample();
+        $entity->scope['id'] = 141;
+        $entity->status = 'queued';
+        $entity->id = 0;
+        $entity->queue['number'] = 0;
+        $process = $query->writeNewFromAdmin($entity, $now);
+
+        $scope = (new \BO\Zmsdb\Scope())->readEntity(141, 1, true);
+        $conflictList = (new \BO\Zmsdb\Process())->readConflictListByScopeAndTime($scope, $startDate, $endDate, 0);
+        $this->assertEquals(9, $conflictList->count());
     }
 
     public function testOverbookedOnDay()
