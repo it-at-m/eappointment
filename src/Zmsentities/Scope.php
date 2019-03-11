2cea7bb33ca094773356c567d912dd042dab990b
Mathias Fischer <mathias.fischer@berlinonline.de>
2019-03-11 14:39:37 +0100

    Error-Log warning fixed

--- src/Zmsentities/Scope.php
+++ src/Zmsentities/Scope.php
@@ -46,7 +46,7 @@ class Scope extends Schema\Entity implements Useraccount\AccessInterface
     public function getDayoffList()
     {
         if (!$this->dayoff instanceof Collection\DayoffList) {
-            $this->dayoff = (! $this->dayoff) ? [] : $this->dayoff;
+            $this->dayoff = (!isset($this->dayoff) || !is_array($this->dayoff)) ? [] : $this->dayoff;
             $this->dayoff = new Collection\DayoffList($this->dayoff);
             foreach ($this->dayoff as $key => $dayoff) {
                 if (!$dayoff instanceof Dayoff) {
