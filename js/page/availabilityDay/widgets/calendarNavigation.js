b2306a25e09d43fb27e004ba0c0cdbc0c83aba29
Mathias Fischer <mathias.fischer@berlinonline.de>
2019-05-08 18:23:09 +0200

    refs #36282 - navigation of calendar fixed

--- js/page/availabilityDay/widgets/calendarNavigation.js
+++ js/page/availabilityDay/widgets/calendarNavigation.js
@@ -7,6 +7,9 @@ const calendarNavigation = (links) => {
             {(links.previousDay >= links.sameDay) &&  
                     <a href={links.previousDay} title="Tag zurück" className="icon prev">◀</a>
             }
+            {(links.previousDay >= links.sameDay) ||  
+                    <span title="Tag zurück nicht möglich" className="icon prev inactive">◀</span>
+            }
             <a href={links.sameDay} className="today">Heute</a>
             <a href={links.nextDay} title="Tag vor" className="icon next">▶</a> 
         </div>
