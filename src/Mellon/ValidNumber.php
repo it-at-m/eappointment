e4e31131dca3d0ea95365cb5f56f5593cb1d665f
Mathias Fischer <mathias.fischer@berlinonline.de>
2019-07-29 17:43:13 +0200

    Added number comparisions

--- src/Mellon/ValidNumber.php
+++ src/Mellon/ValidNumber.php
@@ -23,4 +23,89 @@ class ValidNumber extends Valid
     {
         return $this->validate($message, FILTER_VALIDATE_INT);
     }
+
+    /**
+     * Allow only numbers greater than the given value
+     *
+     * @param Int $number value to compare 
+     * @param String $message error message in case of failure
+     *
+     * @return self
+     */
+    public function isGreaterThan($number, $message = 'too small')
+    {
+        $this->validated = true;
+        if ($this->value < $number) {
+            $this->setFailure($message);
+        }
+        return $this;
+    }
+
+    /**
+     * Allow only numbers greater or equal than the given value
+     *
+     * @param Int $number value to compare 
+     * @param String $message error message in case of failure
+     *
+     * @return self
+     */
+    public function isGreaterEqualThan($number, $message = 'too small')
+    {
+        $this->validated = true;
+        if ($this->value <= $number) {
+            $this->setFailure($message);
+        }
+        return $this;
+    }
+
+    /**
+     * Allow only numbers equal to the given value
+     *
+     * @param Int $number value to compare 
+     * @param String $message error message in case of failure
+     *
+     * @return self
+     */
+    public function isEqualTo($number, $message = 'not matching')
+    {
+        $this->validated = true;
+        if ($this->value == $number) {
+            $this->setFailure($message);
+        }
+        return $this;
+    }
+
+    /**
+     * Allow only numbers lower or equal than the given value
+     *
+     * @param Int $number value to compare 
+     * @param String $message error message in case of failure
+     *
+     * @return self
+     */
+    public function isLowerEqualThan($number, $message = 'too small')
+    {
+        $this->validated = true;
+        if ($this->value >= $number) {
+            $this->setFailure($message);
+        }
+        return $this;
+    }
+
+    /**
+     * Allow only numbers lower than the given value
+     *
+     * @param Int $number value to compare 
+     * @param String $message error message in case of failure
+     *
+     * @return self
+     */
+    public function isLowerThan($number, $message = 'too small')
+    {
+        $this->validated = true;
+        if ($this->value > $number) {
+            $this->setFailure($message);
+        }
+        return $this;
+    }
 }
