f7ed21c20235a4c72617d4eca284e1a6886e6ff9
Mathias Fischer <mathias.fischer@berlinonline.de>
2020-02-18 15:49:29 +0100

    Use VENDOR_PATH in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,12 @@ ini_set('session.serialize_handler', 'php_serialize');
 define("APP_PATH", realpath(__DIR__));
 
 // use autoloading offered by composer, see composer.json for path settings
-require(APP_PATH . '/vendor/autoload.php');
+if (file_exists(APP_PATH . '/vendor/autoload.php')) {
+    define('VENDOR_PATH', APP_PATH . '/vendor');
+} else {
+    define('VENDOR_PATH', APP_PATH . '/../..');
+}
+require_once(VENDOR_PATH . '/autoload.php');
 
 // initialize the static \App singleton
 require(APP_PATH . '/config.php');
