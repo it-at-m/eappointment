69db73f253352182ea871318d63b96160e010f97
Mathias Fischer <mathias.fischer@berlinonline.de>
2020-02-18 15:10:54 +0100

    Use VENDOR_PATH in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,12 @@ chdir(__DIR__);
 define("APP_PATH", realpath(__DIR__));
 
 // use autoloading offered by composer, see composer.json for path settings
-require(APP_PATH . '/vendor/autoload.php');
+if (file_exists(APP_PATH . '/vendor/autoload.php')) {
+    define('VENDOR_PATH', APP_PATH . '/vendor');
+} else {
+    define('VENDOR_PATH', APP_PATH . '/../..');
+}
+require_once(VENDOR_PATH . '/autoload.php');
 
 // initialize the static \App singleton
 require(APP_PATH . '/config.php');
