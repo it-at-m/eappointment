5e52dfb71a3962462a3d461650d68720a0ec2ed0
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-03-12 14:33:27 +0100

    changed package name from masterlayout to bo-layout-admin

--- js/index.js
+++ js/index.js
@@ -38,16 +38,16 @@ import maxChars from './element/form/maxChars'
 import DialogHandler from './lib/dialogHandler'
 
 // Import JS from patternlab
-import accordion from 'masterlayout/source/js/behavior/accordion';
-import tabs from 'masterlayout/source/js/behavior/tabs';
-import collapse from 'masterlayout/source/js/behavior/collapse';
-import stickytrigger from 'masterlayout/source/js/behavior/form-actions--sticky-trigger';
-import navigationprimary from 'masterlayout/source/js/behavior/navigation-primary';
-import datepicker from 'masterlayout/source/js/behavior/datepicker';
-import infotext from 'masterlayout/source/js/behavior/form-infotext-flyin';
-import hamburger from 'masterlayout/source/js/behavior/hamburger';
-import metanavi from 'masterlayout/source/js/behavior/header-metanavi-popup';
-import formalerts from 'masterlayout/source/js/behavior/formalerts-flyin';
+import accordion from 'bo-layout-admin/source/js/behavior/accordion';
+import tabs from 'bo-layout-admin/source/js/behavior/tabs';
+import collapse from 'bo-layout-admin/source/js/behavior/collapse';
+import stickytrigger from 'bo-layout-admin/source/js/behavior/form-actions--sticky-trigger';
+import navigationprimary from 'bo-layout-admin/source/js/behavior/navigation-primary';
+import datepicker from 'bo-layout-admin/source/js/behavior/datepicker';
+import infotext from 'bo-layout-admin/source/js/behavior/form-infotext-flyin';
+import hamburger from 'bo-layout-admin/source/js/behavior/hamburger';
+import metanavi from 'bo-layout-admin/source/js/behavior/header-metanavi-popup';
+import formalerts from 'bo-layout-admin/source/js/behavior/formalerts-flyin';
 
 // load patternlab JS
 function loadResources() {
