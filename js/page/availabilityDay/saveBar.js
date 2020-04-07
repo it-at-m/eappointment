28f4ae2223f2b718e57646a31f931912bc52a6ab
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-04-07 15:43:25 +0200

    fix js linting error

--- js/page/availabilityDay/saveBar.js
+++ js/page/availabilityDay/saveBar.js
@@ -1,18 +1,24 @@
 import React from 'react'
-
 import moment from 'moment'
+import PropTypes from 'prop-types' 
 
 const formatDate = date => {
     const momentDate = moment(date)
     return `${momentDate.format('DD.MM.YYYY')} um ${momentDate.format('HH:mm')} Uhr`
-}
-
+} 
+ 
 const SaveBar = (props) => {
     return (
         <div className="message message--success">
             Öffnungszeiten gespeichert, {formatDate(props.lastSave)}
         </div>
     )
+}
+
+SaveBar.propTypes = {
+    lastSave: PropTypes.oneOfType([
+        PropTypes.number, PropTypes.string
+    ]).isRequired
 }
 
 export default SaveBar
