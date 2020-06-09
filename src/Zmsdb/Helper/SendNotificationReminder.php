040c81b5c64d819570cefd728a1f32943efc8f63
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-06-09 11:11:08 +0200

    refs #44008 - check if department exists before check for notification reminder enabled

--- src/Zmsdb/Helper/SendNotificationReminder.php
+++ src/Zmsdb/Helper/SendNotificationReminder.php
@@ -47,7 +47,10 @@ class SendNotificationReminder
     {
         $notification = null;
         $department = (new \BO\Zmsdb\Department)->readByScopeId($process->getScopeId(), 2);
-        if ($process->getFirstClient()->hasTelephone() && $department->hasNotificationReminderEnabled()) {
+        if (
+            $process->getFirstClient()->hasTelephone() && 
+            $department && $department->hasNotificationReminderEnabled()
+        ) {
             $config = (new \BO\Zmsdb\Config)->readEntity();
             $entity = (new \BO\Zmsentities\Notification)->toResolvedEntity($process, $config, $department);
             $notification = (new \BO\Zmsdb\Notification)->writeInQueue($entity, $this->dateTime);
