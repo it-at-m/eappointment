2565348f77fd9686d98fa9a2cc6fbd95405104f9
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-08-04 16:07:06 +0200

    add Status unit tests, fixed status template headline

--- tests/Zmsadmin/StatusTest.php
+++ tests/Zmsadmin/StatusTest.php
@@ -7,27 +7,48 @@ class StatusTest extends Base
     protected $arguments = [];
     protected $parameters = [];
 
-    protected function getApiCalls()
-    {
-        return [
-            [
-                'function' => 'readGetResult',
-                'url' => '/workstation/',
-                'response' => $this->readFixture("GET_workstation_basic.json")
-            ],
-            [
-                'function' => 'readGetResult',
-                'url' => '/status/',
-                'response' => $this->readFixture("GET_status.json")
-            ]
-        ];
-    }
 
     public function testRendering()
     {
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'response' => $this->readFixture("GET_workstation_basic.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/status/',
+                    'response' => $this->readFixture("GET_status.json")
+                ]
+            ]
+        );
         $response = parent::testRendering();
         $this->assertContains('API Version', (string)$response->getBody());
         //check processes.confirmed:
         $this->assertContains('86861', (string)$response->getBody());
+    }
+
+    public function testWithoutWorkstation()
+    {
+        $exception = new \BO\Zmsclient\Exception();
+        $exception->template = 'BO\Zmsaoi\Exception\Workstation\WorkstationNotFound';
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'exception' => $exception
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/status/',
+                    'response' => $this->readFixture("GET_status.json")
+                ]
+            ]
+        );
+        $response = parent::testRendering();
+        $this->assertNotContains('(Nur für Superuser sichtbar)', (string)$response->getBody());
     }
 }
