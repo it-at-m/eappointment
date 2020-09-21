076bb5db646d74f7391bed59a6f84511a8534373
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-09-21 10:47:13 +0200

    refs #44320 - disable abort Button if state not changed

--- js/page/availabilityDay/form/footerButtons.js
+++ js/page/availabilityDay/form/footerButtons.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types'
 
 const FooterButtons = (props) => {
     const { hasConflicts, stateChanged, data, onNew, onPublish, onAbort } = props
+    console.log(stateChanged)
     return (
         <div className="form-actions" style={{"marginTop":"0", "padding":"0.75em"}}>    
                  <button title="Neue Öffnungszeit anlegen und bearbeiten" className="button button--diamond button-new" onClick={onNew} disabled={(stateChanged || hasConflicts || data)}>neue Öffnungszeit</button> 
-                <button title="Alle Änderungen werden zurückgesetzt" className="button btn" type="abort" onClick={onAbort}>Abbrechen</button>
+                <button title="Alle Änderungen werden zurückgesetzt" className="button btn" type="abort" onClick={onAbort} disabled={(!stateChanged && !hasConflicts && !data)}>Abbrechen</button>
                 <button title="Alle Änderungen werden gespeichert" className="button button--positive button-save" type="save" value="publish" onClick={onPublish} disabled={(! stateChanged || hasConflicts || (data && ! data.__modified))}>Alle Änderungen aktivieren
                 </button>
              
