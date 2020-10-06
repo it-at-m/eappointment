6779bfa096634349a4cde6f7c87f7e51a7c339d4
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-10-06 15:49:15 +0200

    fixed freeProcessList in appointment form - do not show expired free processes

--- tests/Zmsadmin/AppointmentFormFreeProcessListTest.php
+++ tests/Zmsadmin/AppointmentFormFreeProcessListTest.php
@@ -33,6 +33,30 @@ class AppointmentFormFreeProcessListTest extends Base
         $this->assertContains('Spontankunde', (string)$response->getBody());
     }
 
+    public function testWithoutExpired()
+    {
+        \App::$now = new \DateTimeImmutable('2016-05-27 11:30:00', new \DateTimeZone('Europe/Berlin'));
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'parameters' => ['resolveReferences' => 2],
+                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
+                ],
+                [
+                    'function' => 'readPostResult',
+                    'url' => '/process/status/free/',
+                    'parameters' => ['slotType' => 'intern', 'slotsRequired' => 0],
+                    'response' => $this->readFixture("GET_freeprocesslist_20160527.json")
+                ]
+            ]
+        );
+        $response = $this->render([], ['selecteddate' => '2016-05-27'], []);
+        $this->assertNotContains('11:20 (noch 1 frei)', (string)$response->getBody());
+        $this->assertContains('11:40 (noch 1 frei)', (string)$response->getBody());
+    }
+
     // time select field has to be disabled if no slot is existing
     public function testWithSelectedProcessChangedDate()
     {
@@ -73,6 +97,7 @@ class AppointmentFormFreeProcessListTest extends Base
 
     public function testWithSelectedProcess()
     {
+        \App::$now = new \DateTimeImmutable('2016-04-01 11:55:00', new \DateTimeZone('Europe/Berlin'));
         $this->setApiCalls(
             [
                 [
@@ -105,7 +130,7 @@ class AppointmentFormFreeProcessListTest extends Base
             'selectedprocess' => 100044,
             'selectedscope' => 141
         ], []);
-        $this->assertContains('11:20 (noch 1 frei)', (string)$response->getBody());
+        $this->assertContains('11:40 (noch 1 frei)', (string)$response->getBody());
         $this->assertContains('17:00 (noch 0 frei)', (string)$response->getBody());
         $this->assertNotContains('Spontankunde', (string)$response->getBody());
     }
