726f5df67ee040a676d06831415ed105e9e97c99
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-10-09 13:04:18 +0200

    remove provider from BAll single Test

--- tests/Zmsdb/BAllTest.php
+++ tests/Zmsdb/BAllTest.php
@@ -11,7 +11,7 @@ class BAllTest extends Base
         122210,122217,122219,122227,122231,/*122238,*/122243,122252,122260,122262,
         122254,122271,122273,122277,122280,122282,122284,122291,122285,122286,
         122296,150230,122301,122297,122294,122312,122314,122304,122311,122309,
-        317869,324433,325341,324434,122281,324414,122283,122279,122246,122251,
+        317869,/*324433,*/325341,324434,122281,324414,122283,122279,122246,122251,
         122257,122208,122226
     ];
 
@@ -26,16 +26,8 @@ class BAllTest extends Base
            
             $this->writeTestExport($entity, 'provider' . $providerId . '_daylist.php');
             $dayList  = include($this->getFixturePath('provider' . $providerId . '_daylist.php'));
-            if ($providerId == 324433) {
-                var_dump($entity->days);
-                var_dump($dayList);
-                var_dump('BALL_EXPORT: ' . getenv("BALL_EXPORT"));
-            }
             foreach ($entity->days as $day) {
                 $key = $day->getDayHash();
-                if ($providerId == 324433) {
-                    var_dump($key);
-                }
                 $this->assertArrayHasKey($key, $dayList, "Day $key missing for provider=$providerId");
                 $testDay = new \BO\Zmsentities\Day($dayList[$key]);
                 $message = "Day $key has different value on provider=$providerId for ";
