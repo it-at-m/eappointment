0a40d65a36b07bc6e7d1571e925513ee681e1995
Torsten Kunst <torsten.kunst@berlinonline.de>
2020-11-17 13:02:09 +0100

    fixed some useraccount unit tests

--- tests/Zmsapi/DepartmentDeleteTest.php
+++ tests/Zmsapi/DepartmentDeleteTest.php
@@ -36,13 +36,13 @@ class DepartmentDeleteTest extends Base
         // The rights check does not know, if the department is missed because of rights or by lack of data
         $this->expectException('BO\Zmsentities\Exception\UserAccountMissingDepartment');
         $this->expectExceptionCode(403);
-        $this->render(['id' => 999], [], []);
+        $this->render(['id' => 998], [], []);
     }
 
     public function testNoRights()
     {
-        $this->setWorkstation()->getUseraccount()->setRights('department');
-        $this->expectException('BO\Zmsentities\Exception\UserAccountMissingDepartment');
+        $this->setWorkstation()->getUseraccount()->addDepartment(['id' => 74]);
+        $this->expectException('BO\Zmsentities\Exception\UserAccountMissingRights');
         $this->expectExceptionCode(403);
         $this->render(['id' => 999], [], []); //Test Department
     }
