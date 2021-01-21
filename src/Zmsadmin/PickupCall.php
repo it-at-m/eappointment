745e1d7e0b737b5060501ce639aa544cede45182
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-01-21 18:50:42 +0100

    refs #33898 - separate excpetion if process has been called as pickup that is not in pending status yet

--- src/Zmsadmin/PickupCall.php
+++ src/Zmsadmin/PickupCall.php
@@ -25,6 +25,9 @@ class PickupCall extends BaseController
         $workstation = \App::$http->readGetResult('/workstation/', ['resolveReferences' => 2])->getEntity();
         $inputNumber = Validator::value($args['id'])->isNumber()->getValue();
         $process = $this->readSelectedProcess($workstation, $inputNumber);
+        if ('pending' != $process->getStatus()) {
+            throw new Exception\Process\ProcessNotPending();
+        }
         $workstation->process = \App::$http->readPostResult('/process/status/pickup/', $process)->getEntity();
 
         return \BO\Slim\Render::withHtml(
