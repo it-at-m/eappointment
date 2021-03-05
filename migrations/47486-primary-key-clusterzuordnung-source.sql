8a39a5d5c4c2a4b4e878f9366830cfb85887648d
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-03-05 13:06:58 +0100

    update

--- migrations/47486-primary-key-clusterzuordnung-source.sql
+++ migrations/47486-primary-key-clusterzuordnung-source.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `nutzerzuordnung` DROP PRIMARY KEY, ADD PRIMARY KEY(`nutzerid`, `behoerdenid`);
-ALTER TABLE `source` DROP PRIMARY KEY, ADD PRIMARY KEY(`source`);
+ALTER TABLE `nutzerzuordnung` ADD PRIMARY KEY(`nutzerid`, `behoerdenid`);
+ALTER TABLE `source` ADD PRIMARY KEY(`source`);
