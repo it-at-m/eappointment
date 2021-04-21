b1798c04571f0df0268091c10bb92d60931a1f99
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-04-21 22:05:01 +0200

    refs #36078 - following process count -1 if slotCount >= 1

--- src/Zmsdb/Query/Process.php
+++ src/Zmsdb/Query/Process.php
@@ -637,7 +637,9 @@ class Process extends Base implements MappingInterface
         $data['IPAdresse'] = $process['createIP'];
         $data['AnzahlPersonen'] = $process->getClients()->count();
         // - 1 vor following processes count
-        $data['hatFolgetermine'] = $process->getFirstAppointment()->getSlotCount() - 1;
+        $data['hatFolgetermine'] = (1 <= $process->getFirstAppointment()->getSlotCount()) ? 
+            $process->getFirstAppointment()->getSlotCount() - 1 :
+            0;
         $data = $this->readAppointmentData($data, $process);
         $data = $this->readStatusData($data, $process, $dateTime);
         if (0 == $parentProcess) {
