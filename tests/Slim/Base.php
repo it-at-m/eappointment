f11dcdf50f856c72c2fb212d2c0a9924afbae61c
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-05-19 14:31:39 +0200

    add Slim base test file for phpunit

---
+++ tests/Slim/Base.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package 115Mandant
+ * @copyright BerlinOnline Stadtportal GmbH & Co. KG
+ **/
+
+namespace BO\Slim\Tests;
+
+abstract class Base extends \BO\Slim\PhpUnit\Base
+{
+    protected $namespace = '\\BO\\Slim\\Controller\\';
+}
