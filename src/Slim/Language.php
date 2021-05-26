fc018ee9350fdabf96bfae636a7651a8e2e28b51
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-05-26 10:36:29 +0200

    refactored translator class for symfony v5 packages

--- src/Slim/Language.php
+++ src/Slim/Language.php
@@ -6,7 +6,6 @@ use Psr\Http\Message\RequestInterface;
 
 // Symfony Translation Classes
 use Symfony\Component\Translation\Loader\JsonFileLoader;
-use Symfony\Component\Translation\IdentityTranslator;
 use Symfony\Component\Translation\Translator;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
 
@@ -127,7 +126,7 @@ class Language
         $default = $this->getCurrentLocale($this->getDefault());
         $current = $this->getCurrentLanguage();
         // First param is the "default language" to use.
-        $translator = new IdentityTranslator($this->getCurrentLocale($current), new MessageSelector());
+        $translator = new Translator($this->getCurrentLocale($current));
         // Set a fallback language incase you don't have a translation in the default language
         $translator->setFallbackLocales([$default]);
         // Add a loader that will get the php files we are going to store our translations in
