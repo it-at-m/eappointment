bfc37d40ad9f8a3e2aa2cd8d5854f0517cda43b9
Toni Blonske <toni.blonske@berlinonline.de>
2021-07-20 12:39:10 +0200

    add mysql entitys authority, location

---
+++ src/Dldb/MySQL/Entity/Authority.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package ClientDldb
+ * @copyright BerlinOnline Stadtportal GmbH & Co. KG
+ **/
+namespace BO\Dldb\MySQL\Entity;
+
+use \BO\Dldb\Entity\Authority as Base;
+use \BO\Dldb\MySQL\Collection\Authorities as Collection;
+
+/**
+ * @SuppressWarnings(Coupling)
+ */
+class Authority extends Base 
+{
+
+}
