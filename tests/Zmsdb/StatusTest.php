112b4dde17a8a11b85c66d499db853ec47db2692
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-07-21 10:54:05 +0200

    fixed status unit tests with arguments

--- tests/Zmsdb/StatusTest.php
+++ tests/Zmsdb/StatusTest.php
@@ -8,7 +8,8 @@ class StatusTest extends Base
 {
     public function testBasic()
     {
-        $status = (new Query())->readEntity();
+        $now = (new \DateTimeImmutable())->modify('+ 1 Hour');
+        $status = (new Query())->readEntity($now);
         //var_dump(json_encode($status, JSON_PRETTY_PRINT));
         $this->assertInstanceOf("\\BO\\Zmsentities\\Status", $status);
         //var_dump(\BO\Zmsdb\Connection\Select::getReadConnection()->getProfiler()->getProfiles());
