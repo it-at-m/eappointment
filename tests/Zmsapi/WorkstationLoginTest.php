b7bd508edebd99f8eae062a36172e2d47315c993
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-09-24 15:07:11 +0200

    refs #50845 - fixed some unit tests with hashed passwords

--- tests/Zmsapi/WorkstationLoginTest.php
+++ tests/Zmsapi/WorkstationLoginTest.php
@@ -10,7 +10,7 @@ class WorkstationLoginTest extends Base
 
     public static $loginName = 'superuser';
 
-    public static $authKey = 'c66aa72c84901eacb78fb9ef19bdb7bf';
+    public static $authKey = 'vorschau';
 
     public function __construct()
     {
