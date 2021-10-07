5251c7ee8e23ead811edef8abda18be7926c2e6e
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-10-07 15:15:28 +0200

    add Translator class for different translation file types

--- src/Slim/Language.php
+++ src/Slim/Language.php
@@ -4,11 +4,6 @@ namespace BO\Slim;
 
 use Psr\Http\Message\RequestInterface;
 
-// Symfony Translation Classes
-use Symfony\Component\Translation\Loader\JsonFileLoader;
-use Symfony\Component\Translation\Translator;
-use Symfony\Bridge\Twig\Extension\TranslationExtension;
-
 class Language
 {
     public static $supportedLanguages = array();
@@ -19,26 +14,18 @@ class Language
 
     protected static $translator = null;
 
-    /**
-     * @var \Psr\Http\Message\RequestInterface $request;
-     *
-     */
-    protected $request = null;
-
     public function __construct(RequestInterface $request, array $supportedLanguages)
     {
-        $this->request = $request;
         self::$supportedLanguages = $supportedLanguages;
-        $this->current = $this->getLanguageFromRequest();
+        $this->current = $this->getLanguageFromRequest($request);
         if (!$this->current) {
             $this->current = $this->getDefault();
         }
         $this->setCurrentLocale();
-        if (! self::$translator) {
-            self::$translator = $this->setTranslator();
-        } else {
-            self::$translator->setLocale($this->current);
-        }
+        $fallbackLocale = $this->getCurrentLocale($this->getDefault());
+        $defaultLocale = $this->getCurrentLocale($this->getCurrentLanguage());
+        $defaultLang = $this->getDefault();
+        LanguageTranslator::setTranslator($fallbackLocale, $defaultLocale, $this->default);
     }
 
     public function getCurrentLanguage($lang = '')
@@ -101,42 +88,31 @@ class Language
     }
 
     // Detect current language based on request URI or Parameter
-    protected function getLanguageFromRequest()
+    protected function getLanguageFromRequest($request = null)
     {
+        error_log('call language from request at: '. time());
         $current = null;
-        if (null !== $this->request) {
-            $current = $this->getLanguageFromUri();
-            if (! $current) {
-                $requestParamLang = $this->request->getParam('lang');
-                $current = ($requestParamLang) ? $requestParamLang : $this->getDefault();
+        if (null !== $request) {
+            $route = $request->getAttribute('route');
+            $lang = $route->getArgument('lang');
+            if (!empty($lang)) {
+                $current = $lang;
+            }
+            else {
+                $current = $this->getLanguageFromUri($request);
+                if (! $current) {
+                    $requestParamLang = $request->getParam('lang');
+                    $current = ($requestParamLang) ? $requestParamLang : $this->getDefault();
+                }
             }
         }
         return $current;
     }
 
-    protected function getLanguageFromUri()
+    protected function getLanguageFromUri($request)
     {
-        $queryString = $this->request->getUri()->getQuery();
+        $queryString = $request->getUri()->getQuery();
         parse_str($queryString, $queryArr);
         return isset($queryArr['lang']) ? $queryArr['lang'] : null;
-    }
-
-    protected function setTranslator()
-    {
-        $default = $this->getCurrentLocale($this->getDefault());
-        $current = $this->getCurrentLanguage();
-        // First param is the "default language" to use.
-        $translator = new Translator($this->getCurrentLocale($current));
-        // Set a fallback language incase you don't have a translation in the default language
-        $translator->setFallbackLocales([$default]);
-        // Add a loader that will get the php files we are going to store our translations in
-        $translator->addLoader('json', new JsonFileLoader());
-        // Add language files here
-        foreach (\App::$supportedLanguages as $language) {
-            $locale = $language['locale'];
-            $translator->addResource('json', \App::APP_PATH .'/lang/'. $locale .'.json', $locale);
-        }
-        \BO\Slim\Bootstrap::addTwigExtension(new TranslationExtension($translator));
-        return $translator;
     }
 }
---
+++ src/Slim/LanguageTranslator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BO\Slim;
+
+use Psr\Http\Message\RequestInterface;
+
+// Symfony Translation Classes
+use Symfony\Component\Translation\Loader\JsonFileLoader;
+use Symfony\Component\Translation\Loader\PoFileLoader;
+use Symfony\Component\Translation\Translator;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+
+class LanguageTranslator
+{
+    protected $translator = null;
+
+    protected $defaultLang;
+
+    protected $loaderTypes = [
+        'pofile' => 'setPoFileLoader',
+        'json' => 'setJsonFileLoader'
+    ];
+
+    public static function setTranslator($fallbackLocale, $defaultLocale, $defaultLang)
+    {
+        $translatorType = (\App::LANGUAGESOURCE) ? \App::LANGUAGESOURCE : 'pofile';
+        $instance = new static();
+        $instance->defaultLang = $defaultLang;
+        // First param is the "default language" to use.
+        $instance->translator = new Translator($defaultLocale);
+        // Set a fallback language incase you don't have a translation in the default language
+        $instance->translator->setFallbackLocales([$fallbackLocale]);
+        // Add a loader that will get the php files we are going to store our translations in
+        $initLoader = $instance->loaderTypes[$translatorType];
+        $instance->$initLoader();
+        \BO\Slim\Bootstrap::addTwigExtension(new TranslationExtension($instance->getTranslator()));
+        return $instance;
+    }
+
+    public function getTranslator() {
+        return $this->translator;
+    }
+
+    protected function setJsonFileLoader()
+    {
+        $this->translator->addLoader('json', new JsonFileLoader());
+        foreach (\App::$supportedLanguages as $language) {
+            $locale = $language['locale'];
+            $this->translator->addResource('json', \App::APP_PATH .'/lang/'. $locale .'.json', $locale);
+        }
+    }
+
+    protected function setPoFileLoader()
+    {
+        $this->translator->addLoader('pofile', new PoFileLoader());
+        foreach (\App::$supportedLanguages as $locale => $language) {
+            if ($locale != $this->defaultLang) {
+                $this->translator->addResource(
+                    'pofile', 
+                    \App::APP_PATH .'/lang/'. $locale .'.po', 
+                    $language['locale']
+                );
+            }
+        }
+    }
+}
