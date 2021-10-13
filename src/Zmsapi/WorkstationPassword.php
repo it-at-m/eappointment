f5b034c72bbb6852ab5958b7a57b96cd95a17a56
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-10-13 16:13:51 +0200

    refs #50845 Bugfix - fixed lost user rights on change workstation password, add mail to input

--- src/Zmsapi/WorkstationPassword.php
+++ src/Zmsapi/WorkstationPassword.php
@@ -21,19 +21,19 @@ class WorkstationPassword extends BaseController
         \Psr\Http\Message\ResponseInterface $response,
         array $args
     ) {
-        $workstation = (new Helper\User($request))->checkRights();
+        $workstation = (new Helper\User($request, 1))->checkRights();
+        $useraccount = $workstation->getUseraccount();
         $input = Validator::input()->isJson()->assertValid()->getValue();
-        $entity = new \BO\Zmsentities\Useraccount($input);
-        $entity->testValid();
-        Helper\UserAuth::testUseraccountExists($entity->getId());
-        $useraccount = Helper\UserAuth::getVerifiedUseraccount($entity);
-        Helper\UserAuth::testPasswordMatching($useraccount, $entity->password);
-        if (isset($entity->changePassword)) {
-            $useraccount->password = $useraccount->getHash(reset($entity->changePassword));
+        if ($input['email']) {
+            $useraccount->email = $input['email'];
+        }
+        Helper\UserAuth::testPasswordMatching($useraccount, $input['password']);
+        if (isset($input['changePassword'])) {
+            $useraccount->password = $useraccount->getHash(reset($input['changePassword']));
         }
 
         $message = Response\Message::create($request);
-        $message->data = (new Query)->updateEntity($workstation->getUseraccount()->getId(), $useraccount);
+        $message->data = (new Query)->updateEntity($useraccount->getId(), $useraccount);
 
         $response = Render::withLastModified($response, time(), '0');
         $response = Render::withJson($response, $message->setUpdatedMetaData(), $message->getStatuscode());
