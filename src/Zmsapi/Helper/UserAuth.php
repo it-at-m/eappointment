e734a8ae093dbf8a9af6e08ba74ad18d368c31f9
Torsten Kunst <torsten.kunst@berlinonline.de>
2021-10-14 10:18:45 +0200

    fixed code issues

--- src/Zmsapi/Helper/UserAuth.php
+++ src/Zmsapi/Helper/UserAuth.php
@@ -24,7 +24,7 @@ class UserAuth
     {
         // Do you have old, turbo-legacy, non-crypt hashes?
         $result = (strpos($useraccount->password, '$') !== 0) ?
-            ($useraccount->password === md5($password)) : 
+            ($useraccount->password === md5($password)) :
             password_verify($password, $useraccount->password);
         if (! $result) {
             $exception = new \BO\Zmsapi\Exception\Useraccount\InvalidCredentials();
