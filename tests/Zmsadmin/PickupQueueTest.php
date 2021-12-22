e3280189f88d1eee46b92507997a75291bd5c793
tkunst <torsten.kunst@berlinonline.de>
2021-12-22 11:03:45 +0100

    refs #52297 - fixed unittests with pickup queue next button pagination

--- tests/Zmsadmin/PickupQueueTest.php
+++ tests/Zmsadmin/PickupQueueTest.php
@@ -34,6 +34,18 @@ class PickupQueueTest extends Base
                 ],
                 [
                     'function' => 'readGetResult',
+                    'url' => '/workstation/process/pickup/',
+                    'parameters' => [
+                        'resolveReferences' => 1,
+                        'selectedScope' => 141,
+                        'limit' => 1000,
+                        'offset' => 1000,
+                        'gql' => \BO\Zmsadmin\Helper\GraphDefaults::getProcess()
+                    ],
+                    'response' => $this->readFixture("GET_pickupqueue_141.json")
+                ],
+                [
+                    'function' => 'readGetResult',
                     'url' => '/scope/141/department/',
                     'parameters' => [
                         'resolveReferences' => 2
@@ -54,7 +66,7 @@ class PickupQueueTest extends Base
         $this->assertStringContainsString('pickup-table', (string)$response->getBody());
         $this->assertStringContainsString('change-scope', (string)$response->getBody());
         $this->assertStringContainsString('data-limit="1000" data-offset="1000"', (string)$response->getBody());
-        $this->assertStringContainsString('Nächste 1000 Abholer anzeigen', (string)$response->getBody());
+        $this->assertStringContainsString('Nächste 100 Abholer anzeigen', (string)$response->getBody());
         $this->assertStringNotContainsString('Vorherige 1000 Abholer anzeigen', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
     }
