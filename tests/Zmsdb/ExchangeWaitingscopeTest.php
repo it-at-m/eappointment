fbc62750f7a4c288e951dffe34dc03bfdd74cce4
tkunst <torsten.kunst@berlinonline.de>
2022-05-19 11:41:49 +0200

    refs #35877 - The calculation for the waiting time of spontaneous customers was corrected

--- tests/Zmsdb/ExchangeWaitingscopeTest.php
+++ tests/Zmsdb/ExchangeWaitingscopeTest.php
@@ -50,31 +50,33 @@ class ExchangeWaitingscopeTest extends Base
                 ]
             ],
         ]);
-        $process = (new \BO\Zmsentities\Process(['scope' => $scope]))->addQueue(1, $now);
+        $process = (new \BO\Zmsdb\ProcessStatusQueued())->writeNewFromTicketprinter($scope, $now);
+
         $query->writeWaitingTimeCalculated($scope, $now);
         $entry = $query->readByDateTime($scope, $now);
         $this->assertEquals(7, $entry['waitingcalculated']);
-        $this->assertEquals(6, $entry['waitingcount']);
+        $this->assertEquals(1, $entry['waitingcount']);
         $this->assertEquals(0, $entry['waitingtime']);
         // raise values with later time
         $now =  new DateTime('2016-04-01 08:21:00');
+        (new \BO\Zmsdb\ProcessStatusQueued())->writeNewFromTicketprinter($scope, $now);
         $query->writeWaitingTimeCalculated($scope, $now);
         $entry = $query->readByDateTime($scope, $now);
-        $this->assertEquals(13, $entry['waitingcalculated']);
-        $this->assertEquals(12, $entry['waitingcount']);
+        $this->assertEquals(14, $entry['waitingcalculated']);
+        $this->assertEquals(2, $entry['waitingcount']);
         $this->assertEquals(0, $entry['waitingtime']);
         // highest values should not be decreased
         $now =  new DateTime('2016-04-01 08:17:00');
         $query->writeWaitingTimeCalculated($scope, $now);
         $entry = $query->readByDateTime($scope, $now);
-        $this->assertEquals(13, $entry['waitingcalculated']);
-        $this->assertEquals(12, $entry['waitingcount']);
+        $this->assertEquals(14, $entry['waitingcalculated']);
+        $this->assertEquals(2, $entry['waitingcount']);
         $this->assertEquals(0, $entry['waitingtime']);
         // set waitingtime
         $query->writeWaitingTime($process, $now);
         $entry = $query->readByDateTime($scope, $now);
-        $this->assertEquals(13, $entry['waitingcalculated']);
-        $this->assertEquals(12, $entry['waitingcount']);
+        $this->assertEquals(14, $entry['waitingcalculated']);
+        $this->assertEquals(2, $entry['waitingcount']);
         $this->assertEquals(6, $entry['waitingtime']);
         // higher waitingtime
         $now =  new DateTime('2016-04-01 08:19:00');
