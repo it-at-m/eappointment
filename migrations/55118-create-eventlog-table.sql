18ed6063ac3014d13dff3153957b2640074743e9
philipp <philipp.gaebler@berlinonline.de>
2022-07-19 17:18:10 +0200

    refs #55116 - migration script to create the eventlog table

---
+++ migrations/55118-create-eventlog-table.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `eventlog`;
+CREATE TABLE `eventlog` (
+    `eventId` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `eventName` CHAR(80) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+    `origin` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+    `referenceType` CHAR(80) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+    `reference` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL ,
+    `sessionid` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL ,
+    `contextjson` JSON NOT NULL DEFAULT ('{}') ,
+    `creationTimestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    `expirationTimestamp` DATETIME NOT NULL DEFAULT '9999-12-23 00:00:00',
+
+    INDEX (`reference`)
+)
+ENGINE = InnoDB
+CHARACTER SET utf8
+COLLATE utf8_unicode_ci;
