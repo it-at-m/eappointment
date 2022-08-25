9488d3904c36af87c8b0bc8270c1ae47392c6d09
Philipp Gäbler <philipp.gaebler@berlinonline.de>
2022-08-25 10:04:22 +0200

    refs #54692 - qrCode js with zmswebcalldisplay url und css adjustments

--- src/Zmscalldisplay/Index.php
+++ src/Zmscalldisplay/Index.php
@@ -7,19 +7,24 @@
  */
 namespace BO\Zmscalldisplay;
 
+use BO\Slim\Helper as SlimHelper;
+use \Psr\Http\Message\RequestInterface;
+use \Psr\Http\Message\ResponseInterface;
+use \Slim\Http\Request as SlimRequest;
+
 /**
  * Handle requests concerning services
  */
 class Index extends BaseController
 {
-
     /**
      * @SuppressWarnings(UnusedFormalParameter)
+     * @param RequestInterface|SlimRequest
      * @return String
      */
     public function readResponse(
-        \Psr\Http\Message\RequestInterface $request,
-        \Psr\Http\Message\ResponseInterface $response,
+        RequestInterface $request,
+        ResponseInterface $response,
         array $args
     ) {
         $validator = $request->getAttribute('validator');
@@ -29,18 +34,33 @@ class Index extends BaseController
             ->getValue();
         $calldisplayHelper = (new Helper\Calldisplay($request));
         $calldisplay = $calldisplayHelper->getEntity();
+        $parameters  = [
+            'debug' => \App::DEBUG,
+            'queueStatusRequested' => implode(',', $calldisplayHelper::getRequestedQueueStatus($request)),
+            'scopeList' => $calldisplay->getFullScopeList()->getIdsCsv(),
+            'title' => 'Aufrufanzeige',
+            'calldisplay' => $calldisplay,
+            'showQrCode' => false,
+        ];
+
+        if ($request->getQueryParam('qrcode') && $request->getQueryParam('qrcode') == 1
+            && ($request->getQueryParam('collections') || $request->getQueryParam('queue'))
+        ) {
+            $parameters['showQrCode'] = true;
+            $parameters['webcalldisplayUrl'] = $request->getUri()->getScheme() . '://'. $request->getUri()->getHost();
+            $parameters['webcalldisplayUrl'] .= '/terminvereinbarung/aufruf';
+            $parameters['webcalldisplayUrl'] .= urldecode(str_replace('/&', '/?', $request->getUri()->getQuery()));
+            $parameters['webcalldisplayUrl'] .= '&hmac=' . SlimHelper::hashQueryParameters(
+                ['collections' => $request->getQueryParam('collections'), 'queue' => $request->getQueryParam('queue')],
+                ['collections', 'queue']
+            );
+        }
 
         $template = (new Helper\TemplateFinder($defaultTemplate))->setCustomizedTemplate($calldisplay);
         return \BO\Slim\Render::withHtml(
             $response,
             $template->getTemplate(),
-            array(
-                'debug' => \App::DEBUG,
-                'queueStatusRequested' => implode(',', $calldisplayHelper::getRequestedQueueStatus($request)),
-                'scopeList' => $calldisplay->getFullScopeList()->getIdsCsv(),
-                'title' => 'Aufrufanzeige',
-                'calldisplay' => $calldisplay,
-            )
+            $parameters
         );
     }
 }
