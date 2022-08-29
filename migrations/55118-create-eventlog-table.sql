d8c03e68a5eb7b9f620e609005b6b91eea1af82d
tkunst <torsten.kunst@berlinonline.de>
2022-08-29 16:23:26 +0200

    remove commented line in migration that caused an error

--- migrations/55118-create-eventlog-table.sql
+++ migrations/55118-create-eventlog-table.sql
@@ -16,5 +16,4 @@ ENGINE = InnoDB
 CHARACTER SET utf8
 COLLATE utf8_unicode_ci;
 
---set to none to disable at first, can be activated in admin 
 INSERT INTO `config` SET `name` = "cron__deleteOldEventLogEntries", `value` = "none";
