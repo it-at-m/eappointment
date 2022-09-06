5edc1db49083a2e07dd08493b6a8ea7b63f86434
tkunst <torsten.kunst@berlinonline.de>
2022-09-06 11:54:26 +0200

    refs #55116 - add migrations to configure appointment features

---
+++ migrations/55116-config-appointment-features.sql
@@ -0,0 +1,6 @@
+INSERT INTO `config` SET `name` = "appointments__enableSummaryByMail", `value` = "stage,dev";
+INSERT INTO `config` SET `name` = "appointments__enableNextAppointmentView", `value` = "none";
+INSERT INTO `config` SET `name` = "appointments__enableTakenStatistic", `value` = "none";
+INSERT INTO `config` SET `name` = "appointments__enableTakenCountdown", `value` = "none";
+INSERT INTO `config` SET `name` = "appointments__setTakenCountdownLength", `value` = "60";
+INSERT INTO `config` SET `name` = "appointments__enableEmptyCalendar", `value` = "prod,stage,dev";
