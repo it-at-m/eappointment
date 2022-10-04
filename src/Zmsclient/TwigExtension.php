df84213259fa6d8a401e33a0903bd340db08fe37
Philipp Gäbler <philipp.gaebler@berlinonline.de>
2022-10-04 16:09:44 +0200

    refs #55432 - adjustment for dumping variables

--- src/Zmsclient/TwigExtension.php
+++ src/Zmsclient/TwigExtension.php
@@ -41,25 +41,16 @@ class TwigExtension extends \Twig_Extension
 
     public function dumpHttpLog()
     {
-        \D::config([
-            "display.show_call_info" => false,
-            "display.show_version" => false,
-            "sorting.arrays" => false,
-            "display.cascade" => [5,10,10],
-        ]);
         $output = '<h2>HTTP API-Log</h2>'
             .' <p>For debugging: This log contains HTTP calls. <strong>DISABLE FOR PRODUCTION!</strong></p>';
         foreach (Http::$log as $entry) {
             if ($entry instanceof \Psr\Http\Message\RequestInterface) {
-                $settings = new \D\DumpSettings(\D::OB, "Request " . $entry->getMethod() . " " . $entry->getUri());
                 $entry = $this->formatRequest($entry);
             } elseif ($entry instanceof \Psr\Http\Message\ResponseInterface) {
-                $settings = new \D\DumpSettings(\D::OB, "Response");
                 $entry = $this->formatResponse($entry);
-            } else {
-                $settings = new \D\DumpSettings(\D::OB);
             }
-            $output .= \D::UMP($entry, $settings);
+
+            $output .= \Tracy\Debugger::dump($entry, true);
         }
         return $output;
     }
