718f086ab6836abb6e6f3a756d96b85ecc756f09
tkunst <torsten.kunst@berlinonline.de>
2022-10-31 10:30:59 +0100

    refs #55510 - add aria-live attribute to conflict and error messages

--- js/page/availabilityDay/form/errors.js
+++ js/page/availabilityDay/form/errors.js
@@ -14,7 +14,7 @@ const renderErrors = errors => Object.keys(errors).map(key => {
 const Errors = (props) => {
     return (
         Object.keys(props.errorList).length > 0 ? 
-        <div className="message message--error">
+        <div className="message message--error" role="alert" aria-live="polite">
             <h3>Folgende Fehler sind bei der Prüfung Ihrer Eingaben aufgetreten:</h3>
             {renderErrors(props.errorList)}
         </div> : null
