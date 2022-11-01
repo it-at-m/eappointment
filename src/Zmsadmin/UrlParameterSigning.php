7c125540d52015b4e0d125140570ec3fb01f70b4
tkunst <torsten.kunst@berlinonline.de>
2022-11-01 13:24:06 +0100

    refs #54692 - reduced cyclomatic complexity of URLParamterSigning Controller

--- src/Zmsadmin/UrlParameterSigning.php
+++ src/Zmsadmin/UrlParameterSigning.php
@@ -37,60 +37,77 @@ class UrlParameterSigning extends BaseController
     ) {
         $validator = $request->getAttribute('validator');
         $data = $validator->getInput()->isJson()->assertValid()->getValue();
-
-        if (!isset($data['section']) || !isset($data['parameters'])) {
-            throw new BadRequest();
-        }
+        $this->testData($data);
 
         $workstation = \App::$http->readGetResult('/workstation/', ['resolveReferences' => 0])->getEntity();
         $collections = isset($data['parameters']['collections']) ? $data['parameters']['collections'] : [];
 
-        if ((isset($collections['scopelist']) && strlen($collections['scopelist']) > 0
-                || isset($collections['clusterlist']) && strlen($collections['clusterlist']))
-            && isset($workstation['scope']['id'])
-            && !Validator::value($workstation['scope']['id'])->isNumber()->hasFailed()
-        ) {
+        $hasScopeList = (isset($collections['scopelist']) && strlen($collections['scopelist']) > 0);
+        $hasClusterList = (isset($collections['clusterlist']) && strlen($collections['clusterlist']) > 0);
+        $hasValidScopeId = (
+            isset($workstation['scope']['id']) && 
+            !Validator::value($workstation['scope']['id'])->isNumber()->hasFailed()
+        );
+
+        if (($hasScopeList || $hasClusterList) && $hasValidScopeId) {
             $organisation = \App::$http->readGetResult(
                 '/scope/'. $workstation['scope']['id'] .'/organisation/',
                 ['resolveReferences' => 3]
             )->getEntity();
 
-            $scopeIds   = [];
-            $clusterIds = [];
-
-            foreach ($organisation->departments as $departmentData) {
-                $department = (new Department($departmentData))->withCompleteScopeList();
-                if (Property::__keyExists('scopes', $department)) {
-                    /** @var \BO\Zmsentities\Scope $scope */
-                    foreach ($department['scopes'] as $scope) {
-                        $scopeIds[$scope['id']] = $scope['id'];
-                    }
-                }
-                if (Property::__keyExists('clusters', $department)) {
-                    /** @var \BO\Zmsentities\Cluster $scope */
-                    foreach ($department['clusters'] as $cluster) {
-                        $clusterIds[$cluster['id']] = $cluster['id'];
-                    }
-                }
-            }
-
-            if (isset($collections['scopelist']) && strlen($collections['scopelist']) > 0) {
-                $requestedIds = explode(',', $collections['scopelist']);
-                if (count(array_diff($requestedIds, $scopeIds)) > 0) {
-                    throw new UserAccountAccessRightsFailed();
-                }
-            }
-
-            if (isset($collections['clusterlist']) && strlen($collections['clusterlist']) > 0) {
-                $requestedIds = explode(',', $collections['clusterlist']);
-                if (count(array_diff($requestedIds, $clusterIds)) > 0) {
-                    throw new UserAccountAccessRightsFailed();
-                }
-            }
+            $this->testScopeList($organisation, $collections);
+            $this->testClusterList($organisation, $collections);
         }
 
         $data['hmac'] = Helper::hashQueryParameters($data['section'], $data['parameters'], ['collections', 'queue']);
-
+        error_log(var_export($data,true));
         return Render::withJson($response, $data);
+    }
+
+    private function testData($data)
+    {
+        if (!isset($data['section']) || !isset($data['parameters'])) {
+            throw new BadRequest();
+        }
+    }
+
+    private function testScopeList($organisation, $collections)
+    {
+        $scopeIds = [];
+        foreach ($organisation->departments as $departmentData) {
+            $department = (new Department($departmentData))->withCompleteScopeList();
+            if (Property::__keyExists('scopes', $department)) {
+                /** @var \BO\Zmsentities\Scope $scope */
+                foreach ($department['scopes'] as $scope) {
+                    $scopeIds[$scope['id']] = $scope['id'];
+                }
+            }
+        }
+        if (isset($collections['scopelist']) && strlen($collections['scopelist']) > 0) {
+            $requestedIds = explode(',', $collections['scopelist']);
+            if (count(array_diff($requestedIds, $scopeIds)) > 0) {
+                throw new UserAccountAccessRightsFailed();
+            }
+        }
+    }
+
+    private function testClusterList($organisation, $collections)
+    {
+        $clusterIds = [];
+        foreach ($organisation->departments as $departmentData) {
+            $department = (new Department($departmentData))->withCompleteScopeList();
+            if (Property::__keyExists('clusters', $department)) {
+                /** @var \BO\Zmsentities\Cluster $scope */
+                foreach ($department['clusters'] as $cluster) {
+                    $clusterIds[$cluster['id']] = $cluster['id'];
+                }
+            }
+        }
+        if (isset($collections['clusterlist']) && strlen($collections['clusterlist']) > 0) {
+            $requestedIds = explode(',', $collections['clusterlist']);
+            if (count(array_diff($requestedIds, $clusterIds)) > 0) {
+                throw new UserAccountAccessRightsFailed();
+            }
+        }
     }
 }
