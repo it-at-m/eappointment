698f15bbde2017dddb4a424a793b2544937e1c2d
tkunst <torsten.kunst@berlinonline.de>
2022-11-02 11:03:18 +0100

    reduced complexity of hashing paramter helper

--- src/Slim/Helper.php
+++ src/Slim/Helper.php
@@ -30,16 +30,33 @@ class Helper
         return $dateFormatter->format($timestamp);
     }
 
+    
     public static function hashQueryParameters(
         string $section,
         array $queryVariables,
         array $parameters,
         string $hashFunction = 'md5'
     ) {
-        $content = $section;
+        $content = $section . self::getContentForHash($queryVariables, $parameters);
+        $hashString = $hashFunction($content . \App::$urlSignatureSecret);
+        $firstHalf  = substr($hashString, 0, floor(strlen($hashString) / 2));
+        $secondHalf = substr($hashString, strlen($firstHalf));
+        $alphabet   = '0123456789' . implode(range('A', 'Z')) . implode(range('a', 'z'));
+        $rotation   = 31;
+        // reducing the hash to half its length by combining first half and second half
+        for ($i = 0; $i < strlen($firstHalf); $i++) {
+            $rotation = (strpos($alphabet, $firstHalf[$i]) + ord($secondHalf[$i]) + $rotation) % strlen($alphabet);
+            $firstHalf[$i] = $alphabet[$rotation];
+        }
+        return $firstHalf;
+    }
+
+    protected static function getContentForHash(array $queryVariables, array $parameters)
+    {
+        $content = '';
         foreach ($parameters as $parameter) {
             if (isset($queryVariables[$parameter])) {
-                if (is_array($queryVariables[$parameter])) {
+                if (is_iterable($queryVariables[$parameter])) {
                     $parameterArray = $queryVariables[$parameter];
                     ksort($parameterArray);
                     $flat = [];
@@ -57,18 +74,6 @@ class Helper
                 $content .= 'NULL';
             }
         }
-
-        $hashString = $hashFunction($content . \App::$urlSignatureSecret);
-        $firstHalf  = substr($hashString, 0, floor(strlen($hashString) / 2));
-        $secondHalf = substr($hashString, strlen($firstHalf));
-        $alphabet   = '0123456789' . implode(range('A', 'Z')) . implode(range('a', 'z'));
-        $rotation   = 31;
-        // reducing the hash to half its length by combining first half and second half
-        for ($i = 0; $i < strlen($firstHalf); $i++) {
-            $rotation = (strpos($alphabet, $firstHalf[$i]) + ord($secondHalf[$i]) + $rotation) % strlen($alphabet);
-            $firstHalf[$i] = $alphabet[$rotation];
-        }
-
-        return $firstHalf;
+        return $content;
     }
 }
