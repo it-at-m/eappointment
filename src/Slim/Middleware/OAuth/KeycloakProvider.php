014dd0ef19e2a9c6bcd108523a7933370f845670
tkunst <torsten.kunst@berlinonline.de>
2022-11-10 14:44:43 +0100

    phpcbf fixes

--- src/Slim/Middleware/OAuth/KeycloakProvider.php
+++ src/Slim/Middleware/OAuth/KeycloakProvider.php
@@ -40,17 +40,17 @@ class KeycloakProvider extends Keycloak
     {
         $config_data = file_get_contents(\App::APP_PATH . '/keycloak.json');
         if (gettype($config_data) === 'string') {
-			$config_data = json_decode($config_data, TRUE);
-		}
-		$realmData['realm'] = $config_data['realm'];
-		$realmData['clientId'] = array_key_exists('resource', $config_data) ? 
-            $config_data['resource'] : 
+            $config_data = json_decode($config_data, true);
+        }
+        $realmData['realm'] = $config_data['realm'];
+        $realmData['clientId'] = array_key_exists('resource', $config_data) ?
+            $config_data['resource'] :
             $config_data['client_id'];
-        $realmData['redirectUri'] = $config_data['auth-redirect-url'] ? 
-            $config_data['auth-redirect-url'] : 
+        $realmData['redirectUri'] = $config_data['auth-redirect-url'] ?
+            $config_data['auth-redirect-url'] :
             'http://localhost';
-        $realmData['logoutUri'] = $config_data['logout-redirect-url'] ? 
-            $config_data['logout-redirect-url'] : 
+        $realmData['logoutUri'] = $config_data['logout-redirect-url'] ?
+            $config_data['logout-redirect-url'] :
             'http://localhost';
         return $realmData;
     }
@@ -59,26 +59,26 @@ class KeycloakProvider extends Keycloak
     {
         $config_data = file_get_contents(\App::APP_PATH . '/keycloak.json');
         if (gettype($config_data) === 'string') {
-			$config_data = json_decode($config_data, TRUE);
-		}
-		$realmData['realm'] = $config_data['realm'];
-		$realmData['clientId'] = array_key_exists('resource', $config_data) ? 
-            $config_data['resource'] : 
+            $config_data = json_decode($config_data, true);
+        }
+        $realmData['realm'] = $config_data['realm'];
+        $realmData['clientId'] = array_key_exists('resource', $config_data) ?
+            $config_data['resource'] :
             $config_data['client_id'];
-        $realmData['clientSecret'] = array_key_exists('credentials', $config_data) ? 
-            $config_data['credentials']['secret'] : 
-            (array_key_exists('secret', $config_data) ? 
-                $config_data['secret'] : 
-                NULL
+        $realmData['clientSecret'] = array_key_exists('credentials', $config_data) ?
+            $config_data['credentials']['secret'] :
+            (array_key_exists('secret', $config_data) ?
+                $config_data['secret'] :
+                null
             );
-        $realmData['authServerUrl'] = $config_data['auth-server-url'] ? 
-            $config_data['auth-server-url'] : 
+        $realmData['authServerUrl'] = $config_data['auth-server-url'] ?
+            $config_data['auth-server-url'] :
             'http://localhost';
-        $realmData['redirectUri'] = $config_data['auth-redirect-url'] ? 
-            $config_data['auth-redirect-url'] : 
+        $realmData['redirectUri'] = $config_data['auth-redirect-url'] ?
+            $config_data['auth-redirect-url'] :
             'http://localhost';
-        $realmData['logoutUri'] = $config_data['logout-redirect-url'] ? 
-            $config_data['logout-redirect-url'] : 
+        $realmData['logoutUri'] = $config_data['logout-redirect-url'] ?
+            $config_data['logout-redirect-url'] :
             'http://localhost';
         return $realmData;
     }
--- src/Slim/Middleware/OAuthMiddleware.php
+++ src/Slim/Middleware/OAuthMiddleware.php
@@ -12,12 +12,8 @@ class OAuthMiddleware
         ResponseInterface $response,
         callable $next
     ) {
-        if ("keycloak" === \App::OIDC_AUTHORIZATION_TYPE){
-            $response = $this->handleKeycloakInstance($request, $response);
-        }
-        if ("gitlab" === \App::OIDC_AUTHORIZATION_TYPE){
-            $response = $this->handleGitlabInstance($request, $response);
-        }
+        $methodName = 'handle'. \App::OIDC_AUTHORIZATION_TYPE . 'Instance';
+        $response = $this->$methodName($request, $response);
         return $next($request, $response);
     }
 
@@ -26,19 +22,17 @@ class OAuthMiddleware
         $instance = new KeycloakAuth();
         if ('logout/' === $request->getUri()->getPath()) {
             return $instance->doLogout($response);
-        }
-        if ('oidc/' === $request->getUri()->getPath()) {
+        } elseif ('oidc/' === $request->getUri()->getPath()) {
             if (! $request->getParam("code") && '' == \BO\Zmsclient\Auth::getKey()) {
                 $authUrl = $instance->getProvider()->getAuthorizationUrl();
                 \BO\Zmsclient\Auth::setKey($instance->getProvider()->getState());
                 return $response->withRedirect($authUrl, 301);
-            }
-            elseif ($request->getParam("state") !== \BO\Zmsclient\Auth::getKey()) {
-                \BO\Zmsclient\Auth::removeKey();   
+            } elseif ($request->getParam("state") !== \BO\Zmsclient\Auth::getKey()) {
+                \BO\Zmsclient\Auth::removeKey();
             }
             return $instance->doLogin($request, $response);
-        }
-        elseif ('workstation/' === $request->getUri()->getPath()) {
+        } elseif ('workstation/status/' !== $request->getUri()->getPath() && '/' !== $request->getUri()->getPath()) {
+            error_log($request->getUri()->getPath());
             if (! $instance->writeNewAccessTokenIfExpired($response)) {
                 return $instance->doLogout($response);
             }
