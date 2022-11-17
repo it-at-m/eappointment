691cf06fc7fa3392df4390ca7e6dcb997301ed91
tkunst <torsten.kunst@berlinonline.de>
2022-11-17 16:29:23 +0100

    refs #54692 - fixed qrcode printing in firefix, qrcode drawing in separate component

--- js/block/calldisplay/qrCode/index.js
+++ js/block/calldisplay/qrCode/index.js
@@ -1,13 +1,13 @@
-import React, { Component, useRef, useEffect } from 'react'
+import React, { Component } from 'react'
+import ReactToPrint, { PrintContextConsumer } from 'react-to-print';
 import PropTypes from 'prop-types'
-import $ from 'jquery'
-import qrGenerator from "qrcode-generator"
 import FocusTrap from 'focus-trap-react'
+import QrCodeComponent from './qrcode'
 
 class QrCodeView extends Component {
     constructor(props) {
         super(props)
-        this.ref = React.createRef();
+        this.lightBoxRef = React.createRef();
         this.handleTogglePopup = this.handleTogglePopup.bind(this);
         this.handleClickOutside = this.handleClickOutside.bind(this);
         this.keyDownHandler = this.keyDownHandler.bind(this);
@@ -24,7 +24,7 @@ class QrCodeView extends Component {
     }
 
     handleClickOutside(event) {
-        if (this.ref && !this.ref.current.contains(event.target)) {
+        if (this.lightBoxRef && !this.lightBoxRef.current.contains(event.target)) {
             this.handleTogglePopup(event);
         }
     }
@@ -40,71 +40,47 @@ class QrCodeView extends Component {
         }
     }
 
-    drawQrCode(canvas, options) {
-        console.log(canvas)
-        qrGenerator.stringToBytes = qrGenerator.stringToBytesFuncs['UTF-8']
-        const codeObj = qrGenerator(options.version, options.ecLevel)
-        codeObj.addData(options.text)
-        codeObj.make()
-
-        let ctx = canvas.getContext('2d')
-        ctx.fillStyle = options.backgroundColor
-        ctx.fillRect(0, 0, options.size, options.size)
-
-        const gridSize = codeObj.getModuleCount()
-        const blockSize = options.size / (gridSize + 2 * options.quiet)
-        const offset = options.quiet * blockSize
-
-        ctx.beginPath()
-        ctx.fillStyle = options.fillColor
-        for (let row = 0; row < gridSize; row += 1) {
-            for (let col = 0; col < gridSize; col += 1) {
-                let top = offset + row * blockSize
-                let left = offset + col * blockSize
-
-                if (codeObj.isDark(row, col)) {
-                    ctx.rect(left, top, blockSize, blockSize)
-                }
-            }
-        }
-
-        ctx.fill()
-    }
-
     render() {
         const headerstyles = {
             backgroundColor: '#cccccc',
             padding: '5px'
         };
-        const Canvas = props => {
-            const canvasRef = useRef(null)
-
-            useEffect(() => {
-                this.drawQrCode(canvasRef.current, options)
-              }, [])
-
-            const options = {
-                version: 10,
-                ecLevel: 'M',
-                fillColor: '#000',
-                backgroundColor: '#fff',
-                quiet: 5,
-                size: 400,
-                text: this.props.targetUrl
-            }
-            
-            return <canvas ref={canvasRef} {...props} />
-        }
         return (
             <div className="lightbox">
                 <FocusTrap>
-                    <div className="lightbox__content trap" role="dialog" aria-modal="true" ref={this.ref}>
-                        <div className="form-actions no-print" style={headerstyles}>
-                            <button className="button" onClick={(event) => { event.preventDefault(); window.print() }}>Drucken</button>
-                            <button className="button button--destructive" onClick={(event) => { this.handleTogglePopup(event); }}>X</button>
-                        </div>
+                    <div className="lightbox__content trap" role="dialog" aria-modal="true" ref={this.lightBoxRef}>
                         <div className="qrCodeView">
-                            <Canvas width="400" height="400" className="qrCode"></Canvas>
+                            <div className="form-actions no-print" style={headerstyles}>
+                                <ReactToPrint content={() => this.qrCodeRef}>
+                                    <PrintContextConsumer>
+                                        {({ handlePrint }) => (
+                                        <button 
+                                            className="button" 
+                                            onClick={(event) => { event.preventDefault(); handlePrint(); }}>Drucken</button>
+                                        )}
+                                    </PrintContextConsumer>
+                                </ReactToPrint>
+                                <button className="button button--destructive" onClick={(event) => { this.handleTogglePopup(event); }}>X</button>
+                            </div>
+                            <style>
+                                {`@media print {
+                                    .printContainer {
+                                        background-color: #ffffff;
+                                        display:flex;
+                                        justify-content:center;
+                                        align-items:center;
+                                        height:100%;
+                                    }
+                                    html, body{
+                                        height:100%;
+                                        width:100%;
+                                        }
+                                    }
+                                }`}
+                            </style>
+                            <div className="printContainer" ref={el => (this.qrCodeRef = el)}>
+                                <QrCodeComponent width="400" height="400" className="qrCode" { ... {'targetUrl' : this.props.targetUrl} } />
+                            </div>
                         </div>
                     </div>
                 </FocusTrap>
