f0f9f5378d14bdf33329f53fe2fd24c61ffbef11
tkunst <torsten.kunst@berlinonline.de>
2022-11-20 14:24:48 +0100

    refs #55398 - add provider and verification conditions to oidc migration

--- migrations/55398-config-oidc-settings.sql
+++ migrations/55398-config-oidc-settings.sql
@@ -1,4 +1,2 @@
-INSERT INTO `config` SET `name` = "mailings__sqlMaxLimit", `value` = "1000";
-INSERT INTO `config` SET `name` = "mailings__sqlBatchSize", `value` = "100";
-INSERT INTO `config` SET `name` = "notifications__sqlMaxLimit", `value` = "500";
-INSERT INTO `config` SET `name` = "notifications__sqlBatchSize", `value` = "100";
+INSERT INTO `config` SET `name` = "oidc__provider", `value` = "";
+INSERT INTO `config` SET `name` = "oidc__onlyVerifiedMail", `value` = "1";
