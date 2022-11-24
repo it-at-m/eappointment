cbf2403b932f42c084006eb7ceb3fc6ed6170b12
tkunst <torsten.kunst@berlinonline.de>
2022-11-24 16:17:44 +0100

    rebase with main

--- src/Slim/Middleware/OAuth/OAuth.php
+++ src/Slim/Middleware/OAuth/OAuth.php
@@ -9,21 +9,23 @@ use \BO\Slim\Profiler as Profiler;
 class OAuth
 {
     private $provider = null;
-    private $accessTokenPayload = "";
+    private $clientRoles = [];
+    private $userData = [];
 
     public function __construct() {
         $this->setProvider();
     }
 
     private function setProvider(){
-        $guzzyClient = new Client([
+        $client = new Client([
             'defaults' => [
                 \GuzzleHttp\RequestOptions::CONNECT_TIMEOUT => \App::ZMS_AUTHORIZATION_CONNECT_TIMEOUT,
                 \GuzzleHttp\RequestOptions::ALLOW_REDIRECTS => true
             ],
             \GuzzleHttp\RequestOptions::VERIFY => \App::ZMS_AUTHORIZATION_SSL_VERIFY,
         ]); //TODO: use zmsclient
-        //$zmsClient = \App::$http = new \BO\Zmsclient\Http(\App::HTTP_BASE_URL); \BO\Zmsclient\Psr7\Client::$curlopt = \App::$http_curl_config;
+        //$client = \App::$http = new \BO\Zmsclient\Http(\App::HTTP_BASE_URL); \BO\Zmsclient\Psr7\Client::$curlopt = \App::$http_curl_config;
+        //$client = \BO\Zmsclient\Psr7\Client::getClient(\App::$http_curl_config);
         $this->provider = new Keycloak([
             'authServerUrl'         => \App::ZMS_AUTHORIZATION_AUTHSERVERURL,
             'realm'                 => \App::ZMS_AUTHORIZATION_REALM,
@@ -31,23 +33,26 @@ class OAuth
             'clientSecret'          => \App::ZMS_AUTHORIZATION_CLIENT_SECRET,
             'redirectUri'           => \App::ZMS_AUTHORIZATION_REDIRECTURI,
         ]);
-        $this->provider->setHttpClient($guzzyClient);
+        $this->provider->setHttpClient($client);
     }
 
-    public function getAccessTokenPayload(){
-        return $this->accessTokenPayload;
+    public function getUserData(){
+        return $this->userData;
     }
 
-    public function Authorization($code, $state){
+    public function setAccessTokenPayload($code, $state){
         if (!isset($code)) {
             $this->getAuthorizationCode();
         }
 
-        $this->checkState($state);
+        $this->testState($state);
         $token = $this->getAccessToken($code);
 
         list($header, $payload, $signature)  = explode('.', $token->getToken());
-        $this->accessTokenPayload = json_decode(base64_decode($payload), true);
+        $accessTokenPayload = json_decode(base64_decode($payload), true);
+        $this->userData['preferred_username'] = $accessTokenPayload['preferred_username'];
+        $this->userData['email'] = $accessTokenPayload['email'];
+        $this->clientRoles = $accessTokenPayload['resource_access'][\App::ZMS_AUTHORIZATION_CLIENT_ID]['roles'];
     }
 
     private function getAuthorizationCode(){
@@ -57,7 +62,7 @@ class OAuth
         exit;
     }
 
-    private function checkState($state){
+    private function testState($state){
         if (empty($state) || ($state !== \BO\Zmsclient\Auth::getKey())) {
             \BO\Zmsclient\Auth::removeKey();
             throw new \Exception('Invalid state.');
@@ -78,8 +83,7 @@ class OAuth
         return $token;
     }
 
-    public function checkAccessRight(){
-        $resource_access_roles = $this->accessTokenPayload['resource_access'][\App::ZMS_AUTHORIZATION_CLIENT_ID]['roles'];
-        return in_array( \App::ZMS_AUTHORIZATION_ACCESS_ROLE ,$resource_access_roles );
+    public function testAccessRight(){
+        return in_array( \App::ZMS_AUTHORIZATION_ACCESS_ROLE ,$this->clientRoles );
     }
 }
