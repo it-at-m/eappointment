c603feb5ace6af72fae7b42904c665be6f765ab3
Tobias Gassmann <ex.gassmann@muenchen.de>
2023-02-20 13:03:03 +0100

    fix password fields to be disabled when oidc user

--- js/page/useraccount/index.js
+++ js/page/useraccount/index.js
@@ -3,45 +3,45 @@ import $ from 'jquery'
 import { stopEvent } from '../../lib/utils'
 
 class View extends BaseView {
-    constructor (element, options) {
+    constructor(element, options) {
         super(element);
         this.$main = $(element);
         this.includeUrl = options.includeurl;
         this.randomPassword = this.createRandomPassword();
         this.bindPublicMethods();
-        $(this.bindEvents());
+        $(() => {
+            this.bindEvents();
+            this.adjustCredentialsToOidc();
+        });
     }
 
     bindEvents() {
         this.$main.off('click').on('click', 'a.button-delete', (ev) => {
-            this.onConfirm(ev, "confirm_user_delete", () => {this.onDelete(ev)});
-        }).on('change', '#useOidcProvider', (event) => {
-            this.toggleCredentials(event);
+            this.onConfirm(ev, "confirm_user_delete", () => { this.onDelete(ev) });
+        }).on('change', '#useOidcProvider', () => {
+            this.adjustCredentialsToOidc();
         });
     }
 
-    onConfirm(event, template, callback)
-    {
-      stopEvent(event);
-      const userName  = $(event.currentTarget).data('name');
-      this.loadCall(`${this.includeUrl}/dialog/?template=${template}&parameter[name]=${userName}`).then((response) => {
-           this.loadDialog(response, callback, null, event.currentTarget);
-      });
+    onConfirm(event, template, callback) {
+        stopEvent(event);
+        const userName = $(event.currentTarget).data('name');
+        this.loadCall(`${this.includeUrl}/dialog/?template=${template}&parameter[name]=${userName}`).then((response) => {
+            this.loadDialog(response, callback, null, event.currentTarget);
+        });
     }
 
     onDelete(ev) {
         window.location.href = ev.target.href;
     }
 
-    toggleCredentials(event) {
+    adjustCredentialsToOidc() {
         this.$main.find('input[type="password"]').each((index, item) => {
-            if ($(event.target).val() != "") {
-                console.log(this.randomPassword, $(event.target).val())
-
+            if ($('#useOidcProvider').val() != "") {
                 $(item).prop('readonly', true);
                 $(item).val(this.randomPassword)
             } else {
-                $(event.target).prop('checked', false)
+                $('#useOidcProvider').prop('checked', false)
                 $(item).prop('readonly', false);
                 $(item).val('')
             }
@@ -50,14 +50,14 @@ class View extends BaseView {
 
     createRandomPassword() {
         var password = '';
-            var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
-                    'abcdefghijklmnopqrstuvwxyz0123456789@#$';      
-            for (let i = 1; i <= 8; i++) {
-                var char = Math.floor(Math.random()
-                            * str.length + 1);
-                  
-                password += str.charAt(char)
-            }
+        var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+            'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+        for (let i = 1; i <= 8; i++) {
+            var char = Math.floor(Math.random()
+                * str.length + 1);
+
+            password += str.charAt(char)
+        }
         return password;
     }
 }
