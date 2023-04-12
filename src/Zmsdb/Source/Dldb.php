7874c8f943112286dc75633e0ec7c9cac83459e3
Tobias Gassmann <ex.gassmann@muenchen.de>
2023-04-12 13:04:41 +0200

    adjust to max linelength...

--- src/Zmsdb/Source/Dldb.php
+++ src/Zmsdb/Source/Dldb.php
@@ -81,7 +81,10 @@ class Dldb extends \BO\Zmsdb\Base
                     }
 
                     $availability->slotTimeInMinutes = $providerData['slotTimeInMinutes'];
-                    $updatedEntity = (new \BO\Zmsdb\Availability())->updateEntity($availability->getId(), $availability, 2);
+                    
+                    $updatedEntity = 
+                        (new \BO\Zmsdb\Availability())->updateEntity($availability->getId(), $availability, 2);
+
                     (new \BO\Zmsdb\Slot)->writeByAvailability($updatedEntity, \App::$now);
                     (new \BO\Zmsdb\Helper\CalculateSlots(\App::DEBUG))
                         ->writePostProcessingByScope($updatedEntity->scope, \App::$now);
