b80ac8d46528ed1a8a466113450bac88d9539544
Tobias Gassmann <ex.gassmann@muenchen.de>
2023-05-11 12:21:13 +0200

    fix adding and editing of keycloak users

--- src/Zmsadmin/UseraccountAdd.php
+++ src/Zmsadmin/UseraccountAdd.php
@@ -42,6 +42,9 @@ class UseraccountAdd extends BaseController
             }
         }
 
+        $config = \App::$http->readGetResult('/config/', [], \App::CONFIG_SECURE_TOKEN)->getEntity();
+        $allowedProviderList = explode(',', $config->getPreference('oidc', 'provider'));
+
         return \BO\Slim\Render::withHtml(
             $response,
             'page/useraccountEdit.twig',
@@ -55,6 +58,7 @@ class UseraccountAdd extends BaseController
                 'exception' => (isset($result)) ? $result : null,
                 'userAccount' => (isset($result)) ? $input : null,
                 'selectedDepartment' => $selectedDepartment,
+                'oidcProviderList' => array_filter($allowedProviderList),                
                 'metadata' => $this->getSchemaConstraintList(Loader::asArray(Entity::$schema))
             ]
         );
@@ -63,6 +67,9 @@ class UseraccountAdd extends BaseController
     protected function writeNewEntity($input)
     {
         $entity = new Entity($input);
+        if (isset($input['oidcProvider']) && '' != $input['oidcProvider']) {
+            $entity->id = $entity->id. '@' .$input['oidcProvider'];
+        }        
         $entity = $entity->withCleanedUpFormData(true);
         try {
             $entity = \App::$http->readPostResult('/useraccount/', $entity)->getEntity();
