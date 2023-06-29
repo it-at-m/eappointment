f8d91b2226a106147bab7b1b713e51fdc830ba34
Igor Manjenčić <manjak.igor@gmail.com>
2023-06-29 07:49:38 +0000

    feat(ZMS-457): add calculation slots config

---
+++ migrations/16032025-config-recalculate-slots.sql
@@ -0,0 +1,2 @@
+INSERT INTO `config` SET `name` = "availability__calculateSlotsOnSave", `value` = "none";
+INSERT INTO `config` SET `name` = "availability__calculateSlotsOnDemand", `value` = "none";
