bb367fbca6f73b19a84c4ae28d103c838043486d
Igor Manjenčić <manjak.igor@gmail.com>
2023-09-11 13:00:30 +0000

    feat(ZMS-1154): fix primary key

--- src/Zmsdb/Query/Day.php
+++ src/Zmsdb/Query/Day.php
@@ -11,7 +11,7 @@ class Day extends Base
 
     const QUERY_CREATE_TEMPORARY_SCOPELIST = '
         CREATE TEMPORARY TABLE calendarscope (
-            scopeID INT PRIMARY KEY,
+            scopeID INT,
             year SMALLINT,
             month TINYINT,
             slotsRequired TINYINT,
