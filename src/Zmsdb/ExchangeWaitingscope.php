ea5dba79933e8bb4b33c5a9a9333fdae6f5b329b
Igor Manjenčić <manjak.igor@gmail.com>
2023-09-21 13:03:21 +0000

    Update file ExchangeWaitingscope.php

--- src/Zmsdb/ExchangeWaitingscope.php
+++ src/Zmsdb/ExchangeWaitingscope.php
@@ -153,8 +153,8 @@ class ExchangeWaitingscope extends Base implements Interfaces\ExchangeSubject
             : $queueEntry['waitingTimeEstimate'];
 
         $waitingCount = 0;
-        foreach ($queueList as $queue) {
-            if ($queue->getProcess()->getFirstAppointment()->toDateTime()->format('H') == $now->format('H')) {
+        foreach ($queueList as $entity) {
+            if ($entity->arrivalTime->format('H') == $now->format('H')) {
                 $waitingCount++;
             }
         }
