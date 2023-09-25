1498bb999669f4b896b110b1403edbc47e4ea119
Igor Manjenčić <manjak.igor@gmail.com>
2023-09-25 13:10:41 +0000

    feat(ZMS-1389): test

--- src/Zmsdb/ExchangeWaitingscope.php
+++ src/Zmsdb/ExchangeWaitingscope.php
@@ -158,9 +158,7 @@ class ExchangeWaitingscope extends Base implements Interfaces\ExchangeSubject
         } else {
             $queues = $queueList->withAppointment()->withoutStatus(['fake']);
             foreach ($queues as $queue) {
-                if ($queue->getProcess()) {
                     $waitingCount++;
-                }
             }
         }
 
