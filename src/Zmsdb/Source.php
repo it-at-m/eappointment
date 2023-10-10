5f6cdfa52aa5172fc29586b73a8519399dabf535
thomasafink <thomas.fink@muenchen.de>
2023-10-10 08:21:06 +0000

    Extend api with scope

--- src/Zmsdb/Source.php
+++ src/Zmsdb/Source.php
@@ -2,6 +2,7 @@
 
 namespace BO\Zmsdb;
 
+use BO\Zmsentities\Collection\ScopeList;
 use \BO\Zmsentities\Source as Entity;
 
 use \BO\Zmsentities\Collection\SourceList as Collection;
@@ -73,6 +74,8 @@ class Source extends Base
                 ->addList((new Request())->readListBySource($entity->source, $resolveReferences - 1));
             $entity['requestrelation'] = (new RequestRelationList())
                 ->addList((new RequestRelation())->readListBySource($entity->source));
+            $entity['scopes'] = (new ScopeList())
+                ->addList((new Scope())->readList($entity->source, $resolveReferences - 1));
         }
         return $entity;
     }
