5192d5282f28bd737e342df9c55e7ea9d3607af2
thomasafink <thomas.fink@muenchen.de>
2023-10-15 17:49:43 +0000

    Fix migrations

--- migrations/1697187247-add-standort-custom-text-field.sql
+++ migrations/1697187247-add-standort-custom-text-field.sql
@@ -3,5 +3,5 @@ ADD COLUMN `custom_text_field` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb
 
 ALTER TABLE standort 
 ADD COLUMN `custom_text_field_label` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-ADD COLUMN `custom_text_field_active` INT(5) DEFAULT NULL,
-ADD COLUMN `custom_text_field_required` INT(5) DEFAULT NULL;
+ADD COLUMN `custom_text_field_active` INT(5) NOT NULL DEFAULT 0,
+ADD COLUMN `custom_text_field_required` INT(5) NOT NULL DEFAULT 0;
