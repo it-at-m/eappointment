be0b174cab5f9ebcf2e07333dcb3029897ae55c7
thomasafink <thomas.fink@muenchen.de>
2023-10-15 17:02:22 +0000

    fix bug fill freetext field

--- src/Zmsentities/Process.php
+++ src/Zmsentities/Process.php
@@ -155,7 +155,6 @@ class Process extends Schema\Entity
         $this->addClientFromForm($requestData);
         $this->addReminderTimestamp($requestData, $dateTime);
         $this->addAmendment($requestData, $notice);
-        $this->addCustomTextfield($requestData, $notice);
         return $this;
     }
 
