927e64a3d85a843101d26ad826db5664c90b32ba
Tobias Holler <tobias.holler@muenchen.de>
2023-11-08 17:43:22 +0100

    zmsslim added optional basepath from envrionment variable

--- zmsslim/src/Slim/SlimApp.php
+++ zmsslim/src/Slim/SlimApp.php
@@ -21,17 +21,19 @@ class SlimApp extends \Slim\App
      */
     public function determineBasePath(): void
     {
-        $basePath = '';
-        $serverParams = $_SERVER;
+        $basePath = getenv('ZMS_MODULE_BASEPATH') !== false ? getenv('ZMS_MODULE_BASEPATH') : '';
+        if (empty($basePath)) {
+            $serverParams = $_SERVER;
 
-        if (!isset($serverParams['REQUEST_URI']) || !isset($serverParams['SCRIPT_NAME'])) {
-            return;
-        }
+            if (!isset($serverParams['REQUEST_URI']) || !isset($serverParams['SCRIPT_NAME'])) {
+                return;
+            }
 
-        while (min(strlen($serverParams['REQUEST_URI']), strlen($serverParams['SCRIPT_NAME'])) > strlen($basePath)
-            && strncmp($serverParams['REQUEST_URI'], $serverParams['SCRIPT_NAME'], strlen($basePath) + 1) === 0
-        ) {
-            $basePath = substr($serverParams['REQUEST_URI'], 0, strlen($basePath) + 1);
+            while (min(strlen($serverParams['REQUEST_URI']), strlen($serverParams['SCRIPT_NAME'])) > strlen($basePath)
+                && strncmp($serverParams['REQUEST_URI'], $serverParams['SCRIPT_NAME'], strlen($basePath) + 1) === 0
+            ) {
+                $basePath = substr($serverParams['REQUEST_URI'], 0, strlen($basePath) + 1);
+            }
         }
 
         $this->setBasePath(rtrim($basePath, '/'));
