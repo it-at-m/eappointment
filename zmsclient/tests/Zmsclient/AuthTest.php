4214c75bb67ac8a41d40fb9192c3c40d13273b47
thomasafink <thomas@thomasafink.com>
2024-01-09 17:04:07 +0100

    uncomment zmsclient mock tests

--- zmsclient/tests/Zmsclient/AuthTest.php
+++ zmsclient/tests/Zmsclient/AuthTest.php
@@ -7,12 +7,12 @@ class AuthTest extends Base
     /**
      * @runInSeparateProcess
      */
-    /*public function testBasic()
+    public function testBasic()
     {
         $this->assertNull(\BO\Zmsclient\Auth::getKey());
         \BO\Zmsclient\Auth::setKey(123456);
         $this->assertEquals(123456, \BO\Zmsclient\Auth::getKey());
         \BO\Zmsclient\Auth::removeKey();
         $this->assertEquals("", \BO\Zmsclient\Auth::getKey());
-    }*/
+    }
 }
