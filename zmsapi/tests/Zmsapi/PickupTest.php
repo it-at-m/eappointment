c7136cde0b0bbe0be81bb3ecfdd9110df2ab256e
thomasafink <thomas@thomasafink.com>
2024-01-19 13:36:30 +0100

    fix zmsapi test

--- zmsapi/tests/Zmsapi/PickupTest.php
+++ zmsapi/tests/Zmsapi/PickupTest.php
@@ -18,7 +18,7 @@ class PickupTest extends Base
         $response = (new ProcessFinishedTest())->render([], [
             '__body' => json_encode($entity)
         ], []);
-        $response = $this->render([], ['displayName' => '001'], []);
+        $response = $this->render([], [], []);
         $this->assertStringContainsString('process.json', (string)$response->getBody());
         $this->assertStringContainsString('"status":"pending"', (string)$response->getBody());
         $this->assertTrue(200 == $response->getStatusCode());
