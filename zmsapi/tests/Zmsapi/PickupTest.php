73b5689d8ddcc8c1be4db59033364995127727d7
thomasafink <thomas@thomasafink.com>
2024-01-25 11:20:32 +0100

    Add logging zmsapi unit tests

--- zmsapi/tests/Zmsapi/PickupTest.php
+++ zmsapi/tests/Zmsapi/PickupTest.php
@@ -14,35 +14,11 @@ class PickupTest extends Base
         User::$workstation->scope['id'] = 141;
         
         $entity = (new \BO\Zmsdb\Process)->readEntity(10030, new \BO\Zmsdb\Helper\NoAuth);
-        /*$entityJson = json_encode($entity);
-        $chunkSize = 1024; // Define the size of each chunk, e.g., 1024 bytes
-        
-        // Split the JSON string into chunks
-        $chunks = str_split($entityJson, $chunkSize);
-        
-        foreach ($chunks as $index => $chunk) {
-            // Prepend the chunk index to each log entry for easier reassembly or reference
-            error_log($chunk);
-        }*/
-
         $entity->status = 'pending';
-        /*$response = (new ProcessFinishedTest())->render([], [
+        $response = (new ProcessFinishedTest())->render([], [
             '__body' => json_encode($entity)
-        ], []);*/
-        $response = $this->render([], ['__body' => json_encode($entity)], []);
-
-        $responseJson = json_encode($response);
-        $chunkSize = 1024; // Define the size of each chunk, e.g., 1024 bytes
-        
-        // Split the JSON string into chunks
-        $chunks = str_split($responseJson, $chunkSize);
-        
-        foreach ($chunks as $index => $chunk) {
-            // Prepend the chunk index to each log entry for easier reassembly or reference
-            error_log($chunk);
-        }
-
-
+        ], []);
+        $response = $this->render([], [], []);
         $this->assertStringContainsString('process.json', (string)$response->getBody());
         $this->assertStringContainsString('"status":"pending"', (string)$response->getBody());
         $this->assertTrue(200 == $response->getStatusCode());
