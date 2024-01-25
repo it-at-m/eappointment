942efd3a2e0954f47b51728f84c43510a9c4f8ec
thomasafink <thomas@thomasafink.com>
2024-01-25 11:23:21 +0100

    Add logging zmsapi unit tests

--- zmsapi/tests/Zmsapi/PickupTest.php
+++ zmsapi/tests/Zmsapi/PickupTest.php
@@ -18,7 +18,16 @@ class PickupTest extends Base
         $response = (new ProcessFinishedTest())->render([], [
             '__body' => json_encode($entity)
         ], []);
-        $response = $this->render([], [], []);
+        $response = $this->render([], [
+            '__body' => '{
+            "providers": [
+                {
+                    "id": 122217,
+                    "source": "dldb",
+                    "displayName": "B\u00fcrgeramt Heerstra\u00dfe"
+                }
+            ]
+        }'], []);
         $this->assertStringContainsString('process.json', (string)$response->getBody());
         $this->assertStringContainsString('"status":"pending"', (string)$response->getBody());
         $this->assertTrue(200 == $response->getStatusCode());
