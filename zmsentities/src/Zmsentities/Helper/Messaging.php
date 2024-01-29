4ed5f122a222561e908a540025d6924aaf5422cf
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2024-01-29 16:51:35 +0100

    Feature 1683 pipeline unittests (#124)
    * Update combined-workflow.yaml
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_mandantory_unittests_data.sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_mandantory_unittests_data_2.sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_mandantory_unittests_data_3.sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_mandantory_unittests_data_4.sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_mandantory_unittests_data_5.sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_mandantory_unittests_data_6.sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_zmsbo_2 (1).sql.gz:Zone.Identifier
    
    * Delete zmsdb/tests/Zmsdb/fixtures/mysql_zmsbo_3.sql.gz:Zone.Identifier
    
    * remove zone files
    
    * remove zone files
    
    * remove zone files

--- zmsentities/src/Zmsentities/Helper/Messaging.php
+++ zmsentities/src/Zmsentities/Helper/Messaging.php
@@ -204,8 +204,6 @@ class Messaging
 
         if ($process) {
             $provider = $process->scope->provider;
-            //error_log($provider);
-            //print_r($provider, true);
             $providerName = $provider->displayName;
             $providerTemplateName = self::getProviderTemplateName($providerName);
             $providerTemplateFolder = 'custom/' . $providerTemplateName . '/';
