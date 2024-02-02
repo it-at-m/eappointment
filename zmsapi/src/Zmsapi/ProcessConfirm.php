2023195357d9f50073b2d3e8eaf751652a258901
thomasafink <thomas@thomasafink.com>
2024-02-02 15:04:48 +0100

    add optional scope admin mails on process changed

--- zmsapi/src/Zmsapi/ProcessConfirm.php
+++ zmsapi/src/Zmsapi/ProcessConfirm.php
@@ -57,7 +57,7 @@ class ProcessConfirm extends BaseController
     }
     protected function writeMails($request, $process)
     {
-        if ($process->hasScopeAdmin() && $process->sendAdminMailOnConfirmation() === 1) {
+        if ($process->hasScopeAdmin() && $process->sendAdminMailOnConfirmation()) {
             $authority = $request->getUri()->getAuthority();
             $validator = $request->getAttribute('validator');
             $initiator = $validator->getParameter('initiator')
