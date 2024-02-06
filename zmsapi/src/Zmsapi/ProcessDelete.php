78139ad565c0eee264fe57e725d74a55538c8ff8
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2024-02-06 16:31:02 +0100

    Feature 1882 erweiterung der benachrichtigungen an standortadmin (#137)
    * Add send email to admin when process confirmed
    
    * update twig description
    
    * Send mail confirmation to admin
    
    * remove error log
    
    * Fix grammar
    
    * Add the logic for when to send mail to admin
    
    * Add scope admin mail configurations to scope
    
    * set twig cache
    
    * add optional scope admin mails on process changed
    
    * add twig cache
    
    * Fixzmsapi test
    
    * Comment unit test

--- zmsapi/src/Zmsapi/ProcessDelete.php
+++ zmsapi/src/Zmsapi/ProcessDelete.php
@@ -10,7 +10,7 @@ use \BO\Slim\Render;
 use \BO\Zmsdb\Process;
 use \BO\Zmsdb\Mail;
 use \BO\Zmsdb\Config;
-use BO\Mellon\Validator;
+use \BO\Mellon\Validator;
 
 /**
  * @SuppressWarnings(CouplingBetweenObjects)
@@ -51,7 +51,7 @@ class ProcessDelete extends BaseController
 
     protected function writeMails($request, $process)
     {
-        if ($process->hasScopeAdmin()) {
+        if ($process->hasScopeAdmin() && $process->sendAdminMailOnDeleted()) {
             $authority = $request->getUri()->getAuthority();
             $validator = $request->getAttribute('validator');
             $initiator = $validator->getParameter('initiator')
