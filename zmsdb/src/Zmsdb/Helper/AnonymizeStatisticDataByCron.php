f84409f6696c9def88f92d76cc98a51ea9e5233f
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2024-02-26 09:08:01 +0100

    Update AnonymizeStatisticDataByCron.php

--- zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
+++ zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
@@ -7,7 +7,7 @@ use BO\Zmsdb\ProcessStatusArchived;
 class AnonymizeStatisticDataByCron
 {
     protected $verbose = false;
-    protected $timespan = '-90 days';
+    protected $timespan = '-2 days';
 
     public function __construct($verbose = false)
     {
