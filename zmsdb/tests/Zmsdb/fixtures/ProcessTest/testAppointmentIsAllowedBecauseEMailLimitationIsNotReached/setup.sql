a5ffef832f021273c1b27a7ff8a91265d3b7d3b2
Igor Manjencic <manjak.igor@gmail.com>
2024-03-04 16:36:22 +0100

    feat(ZMS-1784): fix tests

--- zmsdb/tests/Zmsdb/fixtures/ProcessTest/testAppointmentIsAllowedBecauseEMailLimitationIsNotReached/setup.sql
+++ zmsdb/tests/Zmsdb/fixtures/ProcessTest/testAppointmentIsAllowedBecauseEMailLimitationIsNotReached/setup.sql
@@ -1,2 +1,2 @@
 UPDATE standort SET appointments_per_mail = 2 WHERE StandortID = 140;
-UPDATE buerger SET EMail = "testmail@mail.com" WHERE absagecode = "a156";
+UPDATE buerger SET EMail = "testmail2@mail.com" WHERE absagecode = "a156";
