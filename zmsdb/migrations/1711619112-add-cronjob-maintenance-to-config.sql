aef16f005fc406c33096cb5b2289313ffad6d82b
Thomas Fink <thomas.fink@muenchen.de>
2024-04-02 11:14:53 +0200

    Add migrations for dldb backups and archive anonymization

---
+++ zmsdb/migrations/1711619112-add-cronjob-maintenance-to-config.sql
@@ -0,0 +1,5 @@
+INSERT INTO `config` (`name`, `value`, `changeTimestamp`) VALUES
+('anonymization__anonymizeCitizenArchiveDataInDays', '2', '2024-02-20 00:00:00');
+
+INSERT INTO `config` (`name`, `value`, `changeTimestamp`) VALUES
+('backups__setDLDBMapperBackupDateTimestamp', '0', '2024-02-20 00:00:00');
