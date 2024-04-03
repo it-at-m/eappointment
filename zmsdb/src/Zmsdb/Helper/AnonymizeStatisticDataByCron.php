30cd62f05dee078cba6cf8451928114e63272e54
Thomas Fink <thomas.fink@muenchen.de>
2024-04-03 17:14:53 +0200

    Add rollback mechanism for dldb mapper persistent volume first attempt

--- zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
+++ zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
@@ -7,16 +7,30 @@ use BO\Zmsdb\ProcessStatusArchived;
 class AnonymizeStatisticDataByCron
 {
     protected $verbose = false;
-    protected $timespan = '-2 days';
+    protected $timespan; // Timespan will be set based on the retention setting
 
     public function __construct($verbose = false)
     {
         $this->verbose = $verbose;
+        
+        // Fetching the configuration setting
+        $config = \App::$http->readGetResult('/config/', [], \App::CONFIG_SECURE_TOKEN)->getEntity();
+        
+        // Extracting the retention setting and converting it to an integer
+        $retentionSetting = explode(',', $config->getPreference('buergerarchiv', 'setRetentionPeriodDays'));
+        if (isset($retentionSetting[0]) && is_numeric($retentionSetting[0])) {
+            // Ensure it's a positive integer and assign it to timespan
+            $this->timespan = abs(intval($retentionSetting[0]));
+        } else {
+            // Default to 2 days if the setting is not set or not numeric
+            $this->timespan = 2;
+        }
     }
 
     public function startAnonymizing(\DateTimeImmutable $currentDate, $commit = false)
     {
-        $targetDate = $currentDate->modify($this->timespan);
+        // Adjust the currentDate based on the numeric timespan
+        $targetDate = $currentDate->modify("-{$this->timespan} days");
         $this->logMessage("INFO: Beginning anonymization for entries older than {$targetDate->format('Y-m-d')}.");
 
         if ($commit) {
@@ -39,4 +53,3 @@ class AnonymizeStatisticDataByCron
         }
     }
 }
-
