5de06d3e4354b83bc1a25c2927b49263cf85f3b5
Thomas Fink <thomas.fink@muenchen.de>
2024-04-04 14:08:43 +0200

    revert 69af621611e6fa31cb5c5c26f9d6d3439fb7bc1f1

--- zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
+++ zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
@@ -3,38 +3,21 @@
 namespace BO\Zmsdb\Helper;
 
 use BO\Zmsdb\ProcessStatusArchived;
-use BO\Zmsdb\Config as ConfigRepository;
 
 class AnonymizeStatisticDataByCron
 {
     protected $verbose = false;
-    protected $timespan; // Timespan will be set based on the retention setting
+
+    protected $timespan = '-90 days';
 
     public function __construct($verbose = false)
     {
         $this->verbose = $verbose;
-        
-        // Fetching the configuration setting
-        $config = (new ConfigRepository())->readEntity();
-        
-        // Extracting the retention setting and converting it to an integer
-        $envValue = getenv('ZMS_ENV');
-        $retentionSetting = explode(',', $config->getPreference('buergerarchiv', 'setRetentionPeriodDays'));
-        if (isset($retentionSetting) && is_numeric($retentionSetting)) {
-            // Ensure it's a positive integer and assign it to timespan
-            echo "Using retention period set in config {$retentionSetting}.";
-            $this->timespan = abs(intval($retentionSetting));
-        } else {
-            // Default to 2 days if the setting is not set or not numeric
-            echo "Using default retention period 2.";
-            $this->timespan = 2;
-        }
     }
 
     public function startAnonymizing(\DateTimeImmutable $currentDate, $commit = false)
     {
-        // Adjust the currentDate based on the numeric timespan
-        $targetDate = $currentDate->modify("-{$this->timespan} days");
+        $targetDate = $currentDate->modify($this->timespan);
         $this->logMessage("INFO: Beginning anonymization for entries older than {$targetDate->format('Y-m-d')}.");
 
         if ($commit) {
@@ -57,3 +40,4 @@ class AnonymizeStatisticDataByCron
         }
     }
 }
+
