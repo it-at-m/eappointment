69af621611e6fa31cb5c5c26f9d6d3439fb7bc1f
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2024-04-04 13:27:26 +0200

    Zms 1571 dldb mapper data rollback and update buergerarchiv anonymization (#274)
    * Add migrations for dldb backups and archive anonymization
    
    * Add migrations for dldb backups and archive anonymization
    
    * Add rollback mechanism for dldb mapper persistent volume first attempt
    
    * Add rollback mechanism for dldb mapper persistent volume first attempt
    
    * include ZMS_ENV
    
    * fix ZMS_ENV
    
    * fix ZMS_ENV
    
    * Add ZMS_ENV to anon archiv
    
    * Add logging
    
    ---------
    
    Co-authored-by: Thomas Fink <thomas.fink@muenchen.de>

--- zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
+++ zmsdb/src/Zmsdb/Helper/AnonymizeStatisticDataByCron.php
@@ -3,21 +3,38 @@
 namespace BO\Zmsdb\Helper;
 
 use BO\Zmsdb\ProcessStatusArchived;
+use BO\Zmsdb\Config as ConfigRepository;
 
 class AnonymizeStatisticDataByCron
 {
     protected $verbose = false;
-
-    protected $timespan = '-90 days';
+    protected $timespan; // Timespan will be set based on the retention setting
 
     public function __construct($verbose = false)
     {
         $this->verbose = $verbose;
+        
+        // Fetching the configuration setting
+        $config = (new ConfigRepository())->readEntity();
+        
+        // Extracting the retention setting and converting it to an integer
+        $envValue = getenv('ZMS_ENV');
+        $retentionSetting = explode(',', $config->getPreference('buergerarchiv', 'setRetentionPeriodDays'));
+        if (isset($retentionSetting) && is_numeric($retentionSetting)) {
+            // Ensure it's a positive integer and assign it to timespan
+            echo "Using retention period set in config {$retentionSetting}.";
+            $this->timespan = abs(intval($retentionSetting));
+        } else {
+            // Default to 2 days if the setting is not set or not numeric
+            echo "Using default retention period 2.";
+            $this->timespan = 2;
+        }
     }
 
     public function startAnonymizing(\DateTimeImmutable $currentDate, $commit = false)
     {
-        $targetDate = $currentDate->modify($this->timespan);
+        // Adjust the currentDate based on the numeric timespan
+        $targetDate = $currentDate->modify("-{$this->timespan} days");
         $this->logMessage("INFO: Beginning anonymization for entries older than {$targetDate->format('Y-m-d')}.");
 
         if ($commit) {
@@ -40,4 +57,3 @@ class AnonymizeStatisticDataByCron
         }
     }
 }
-
