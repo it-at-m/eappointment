abcfe8373eccde4070ca4cb1a9ce4e8331763143
Thomas Fink <thomas.fink@muenchen.de>
2024-04-05 16:41:38 +0200

    use none for rollback and cleanup logging

--- zmsdb/migrations/1711619112-add-cronjob-maintenance-to-config.sql
+++ zmsdb/migrations/1711619112-add-cronjob-maintenance-to-config.sql
@@ -1,8 +1,8 @@
 INSERT INTO `config` (`name`, `value`, `changeTimestamp`) VALUES
-('buergerarchiv__setRetentionPeriodDays', '2', '2024-02-20 00:00:00');
+('buergerarchiv__setRetentionPeriodDays', '90', '2024-02-20 00:00:00');
 
 INSERT INTO `config` (`name`, `value`, `changeTimestamp`) VALUES
-('dldbBackup__setRollbackDay', '0', '2024-02-20 00:00:00');
+('dldbBackup__setRollbackDay', 'none', '2024-02-20 00:00:00');
 
 INSERT INTO `config` (`name`, `value`, `changeTimestamp`) VALUES
 ('dldbBackup__setRetentionPeriodDays', '7', '2024-02-20 00:00:00');
