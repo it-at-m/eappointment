182b27465bb880be55c1757373d8f726c7b72253
tobias74 <tobias@gaszmann.de>
2024-06-19 09:55:42 +0300

    remove read only functionality

--- zmsadmin/js/page/availabilityDay/form/datepicker.js
+++ zmsadmin/js/page/availabilityDay/form/datepicker.js
@@ -9,7 +9,6 @@ import {weekDayList, repeat} from '../helpers'
 import * as Inputs from '../../../lib/inputs'
 const { Label, Description } = Inputs
 import de from 'date-fns/locale/de';
-import { ReadOnlyInput } from './readOnlyInput';
 //import {formatTimestampDate} from "../helpers"
 registerLocale('de', de)
 
@@ -342,14 +341,13 @@ class AvailabilityDatePicker extends Component
                                 strictParsing={true}
                                 open={this.state.timePickerIsOpen}
                                 ref={(timepicker) => { this.timepicker = timepicker }} 
-                                customInput={<ReadOnlyInput />}
 
                             />
                             <a href="#" aria-describedby={"help_" + this.props.attributes.id + "_time"} aria-label="Uhrzeitauswahl öffnen" className="calendar-placement icon" title={"startDate" == this.props.name ? "Uhrzeit von wählen" : "Uhrzeit bis wählen"} onClick={this.handleClockIcon} onKeyDown={this.tpKeyDownHandler}>
                                 <i className="far fa-clock" aria-hidden="true" />
                             </a>
                         </div>
-                        <Description attributes={{ "id": "help_" + this.props.attributes.id + "_time" }} value="Eingabe über Dropdown"></Description>
+                        <Description attributes={{ "id": "help_" + this.props.attributes.id + "_time" }} value="Eingabe der Uhrzeit im Format HH:MM"></Description>
                     </div>
                 </div>
             </div>
