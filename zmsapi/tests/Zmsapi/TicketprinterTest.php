2d391abf527dccb4727e8e2c245644b07ab0ff8e
Andrew Fink <andrewfink@MacBook-Pro-von-Andrew.local>
2024-07-01 17:01:47 +0200

    change fixure

--- zmsapi/tests/Zmsapi/TicketprinterTest.php
+++ zmsapi/tests/Zmsapi/TicketprinterTest.php
@@ -11,7 +11,7 @@ class TicketprinterTest extends Base
         $response = $this->render([], [
             '__body' => '{
                 "enabled": true,
-                "hash": "ac9df1f2983c3f94aebc1a9bd121bfecf5b374f2",
+                "hash": "1abcdefghijklmnopqrstuvwxyz",
                 "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links",
@@ -56,7 +56,7 @@ class TicketprinterTest extends Base
             '__body' => '{
                 "buttonlist": "s141,l[http://www.berlin.de/|Portal+Berlin.de]",
                 "enabled": true,
-                "hash": "ac9df1f2983c3f94aebc1a9bd121bfecf5b374f2",
+                "hash": "1abcdefghijklmnopqrstuvwxyz",
                 "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
@@ -74,7 +74,7 @@ class TicketprinterTest extends Base
             '__body' => '{
                 "buttonlist": "s139",
                 "enabled": true,
-                "hash": "ac9df1f2983c3f94aebc1a9bd121bfecf5b374f2",
+                "hash": "1abcdefghijklmnopqrstuvwxyz",
                 "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
@@ -90,7 +90,7 @@ class TicketprinterTest extends Base
             '__body' => '{
                 "buttonlist": "s106",
                 "enabled": true,
-                "hash": "ac9df1f2983c3f94aebc1a9bd121bfecf5b374f2",
+                "hash": "1abcdefghijklmnopqrstuvwxyz",
                 "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
