710f32bae205f8448bdd8942b14e5cd65584e638
Andrew Fink <andrewfink@MacBook-Pro-von-Andrew.local>
2024-07-01 17:02:53 +0200

    change fixure

--- zmsapi/tests/Zmsapi/TicketprinterTest.php
+++ zmsapi/tests/Zmsapi/TicketprinterTest.php
@@ -104,7 +104,7 @@ class TicketprinterTest extends Base
             '__body' => '{
                 "buttonlist": "s141",
                 "enabled": true,
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
+                "hash": "1abcdefghijklmnopqrstuvwxyz",
                 "id": 2,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
@@ -131,7 +131,7 @@ class TicketprinterTest extends Base
         $this->render([], [
             '__body' => '{
                 "buttonlist": "s141,l[http://www.berlin.de/|Portal+Berlin.de]",
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
+                "hash": "1abcdefghijklmnopqrstuvwxyz",
                 "id": 1234
             }'
         ], []);
