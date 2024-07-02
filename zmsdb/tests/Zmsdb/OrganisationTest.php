d16ddbd64a3eea15a036c935a0dcdb510a83167d
Andrew Fink <andrewfink@MacBook-Pro-von-Andrew.local>
2024-07-02 10:52:07 +0200

    change fixure to scope

--- zmsdb/tests/Zmsdb/OrganisationTest.php
+++ zmsdb/tests/Zmsdb/OrganisationTest.php
@@ -53,7 +53,7 @@ class OrganisationTest extends Base
     public function testReadByTicketprinterHash()
     {
         $query = new Query();
-        $entity = $query->readByHash('54abcdefghijklmnopqrstuvwxyz'); //Test Ticketprinter
+        $entity = $query->readByHash('71abcdefghijklmnopqrstuvwxyz'); //Test Ticketprinter
         $this->assertEntity("\\BO\\Zmsentities\\Organisation", $entity);
         $this->assertEquals('Pankow', $entity->name);
     }
