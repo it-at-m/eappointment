0f4595ca6a2b2ca47921a41d96f9734a037208dc
Andrew Fink <andrewfink@MacBook-Pro-von-Andrew.local>
2024-07-02 09:45:26 +0200

    revert fixture

--- zmsapi/tests/Zmsapi/TicketprinterTest.php
+++ zmsapi/tests/Zmsapi/TicketprinterTest.php
@@ -11,8 +11,8 @@ class TicketprinterTest extends Base
         $response = $this->render([], [
             '__body' => '{
                 "enabled": true,
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
-                "id": 4,
+                "hash": "71abcdefghijklmnopqrstuvwxyz",
+                "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links",
                 "buttons": [
@@ -20,10 +20,10 @@ class TicketprinterTest extends Base
                         "type": "scope",
                         "url": "/scope/101/",
                         "scope": {
-                            "id": 381
+                            "id": 141
                         },
                         "enabled": true,
-                        "name": "Bürgeramt Karow / Buch"
+                        "name": "Bürgeramt Heerstraße"
                     },
                     {
                         "type": "cluster",
@@ -54,10 +54,10 @@ class TicketprinterTest extends Base
     {
         $response = $this->render([], [
             '__body' => '{
-                "buttonlist": "s381,l[http://www.berlin.de/|Portal+Berlin.de]",
+                "buttonlist": "s141,l[http://www.berlin.de/|Portal+Berlin.de]",
                 "enabled": true,
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
-                "id": 4,
+                "hash": "71abcdefghijklmnopqrstuvwxyz",
+                "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
             }'
@@ -74,7 +74,7 @@ class TicketprinterTest extends Base
             '__body' => '{
                 "buttonlist": "s139",
                 "enabled": true,
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
+                "hash": "71abcdefghijklmnopqrstuvwxyz",
                 "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
@@ -90,8 +90,8 @@ class TicketprinterTest extends Base
             '__body' => '{
                 "buttonlist": "s106",
                 "enabled": true,
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
-                "id": 4,
+                "hash": "71abcdefghijklmnopqrstuvwxyz",
+                "id": 1,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
             }'
@@ -102,10 +102,10 @@ class TicketprinterTest extends Base
     {
         $response = $this->render([], [
             '__body' => '{
-                "buttonlist": "s381",
+                "buttonlist": "s141",
                 "enabled": true,
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
-                "id": 4,
+                "hash": "ac9df1f2983c3f94aebc1a9bd121bfecf5b374f3",
+                "id": 2,
                 "lastUpdate": 1447925326000,
                 "name": "Eingangsbereich links"
             }'
@@ -131,7 +131,7 @@ class TicketprinterTest extends Base
         $this->render([], [
             '__body' => '{
                 "buttonlist": "s141,l[http://www.berlin.de/|Portal+Berlin.de]",
-                "hash": "54abcdefghijklmnopqrstuvwxyz",
+                "hash": "71abcdefghijklmnopqrstuvwxyz",
                 "id": 1234
             }'
         ], []);
