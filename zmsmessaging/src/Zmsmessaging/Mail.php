72be57f770246928a800a23e114177f9555548cc
Thomas Fink <thomas.fink@muenchen.de>
2024-07-08 18:27:34 +0200

    fix unit tests

--- zmsmessaging/src/Zmsmessaging/Mail.php
+++ zmsmessaging/src/Zmsmessaging/Mail.php
@@ -50,10 +50,10 @@ class Mail extends BaseController
                     $log = new Mimepart(['mime' => 'text/plain']);
                     $log->content = $exception->getMessage();
                     if (isset($item['process']) && isset($item['process']['id'])) {
-                        $this->log("Init Queue Exception message: " . $log->content . ' - ' . \App::$now->format('c'));
-                        $this->log("Init Queue Exception log readPostResult start - " . \App::$now->format('c'));
-                        \App::$http->readPostResult('/log/process/' . $item['process']['id'] . '/', $log, ['error' => 1]);
-                        $this->log("Init Queue Exception log readPostResult finished - " . \App::$now->format('c'));
+                        $this->log("Init Queue Exception message: ". $log->content .' - '. \App::$now->format('c'));
+                        $this->log("Init Queue Exception log readPostResult start - ". \App::$now->format('c'));
+                        \App::$http->readPostResult('/log/process/'. $item['process']['id'] .'/', $log, ['error' => 1]);
+                        $this->log("Init Queue Exception log readPostResult finished - ". \App::$now->format('c'));
                     }
                 }
             }
