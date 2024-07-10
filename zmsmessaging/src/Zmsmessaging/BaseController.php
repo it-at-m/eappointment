8356879abbe13d1e583c286a9b0e121ac8748032
Thomas Fink <thomas.fink@muenchen.de>
2024-07-10 18:20:53 +0200

    try batch 3

--- zmsmessaging/src/Zmsmessaging/BaseController.php
+++ zmsmessaging/src/Zmsmessaging/BaseController.php
@@ -1,139 +1,167 @@
 <?php
 /**
  *
-* @package Zmsmessaging
-*
-*/
+ * @package Zmsmessaging
+ *
+ */
 namespace BO\Zmsmessaging;
 
-use \BO\Zmsentities\Mail;
-use \BO\Zmsentities\Notification;
 use \BO\Zmsentities\Mimepart;
-use \BO\Mellon\Validator;
 
-class BaseController
+class Mail extends BaseController
 {
-    protected $verbose = false;
-    protected static $logList = [];
-    protected $workstation = null;
+    protected $messagesQueue = null;
+    private $processMailScript;
     protected $startTime;
-    protected $maxRunTime = 50;
 
-    public function __construct($verbose = false, $maxRunTime = 50)
+    public function __construct($verbose = false, $maxRunTime = 50, $processMailScript = __DIR__ . '/process_mail.php')
     {
-        $this->verbose = $verbose;
         $this->startTime = microtime(true);
-        $this->maxRunTime = $maxRunTime;
+        parent::__construct($verbose, $maxRunTime);
+        $this->processMailScript = $this->findProcessMailScript($processMailScript);
+        $this->log("process_mail.php path: " . $this->processMailScript); // Log the path
+        $this->log("Read Mail QueueList start with limit " . \App::$mails_per_minute . " - " . \App::$now->format('c'));
+        $queueList = \App::$http->readGetResult('/mails/', [
+            'resolveReferences' => 2,
+            'limit' => \App::$mails_per_minute
+        ])->getCollection();
+        if (null !== $queueList) {
+            $this->messagesQueue = $this->convertCollectionToArray($queueList->sortByCustomKey('createTimestamp'));
+            $this->log("QueueList sorted by createTimestamp - " . \App::$now->format('c'));
+        }
     }
+
+    private function findProcessMailScript($path)
+    {
+        if (file_exists($path)) {
+            return realpath($path);
+        } else {
+            $this->log("process_mail.php not found at $path. Searching for file...");
+            $files = $this->searchFile(__DIR__, 'process_mail.php');
+            if (!empty($files)) {
+                $this->log("process_mail.php found at " . $files[0]);
+                return realpath($files[0]);
+            } else {
+                $this->log("process_mail.php could not be found.");
+                throw new \Exception("process_mail.php could not be found.");
+            }
+        }
+    }
+
+    private function searchFile($directory, $filename)
+    {
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($directory),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        $files = [];
+        foreach ($iterator as $file) {
+            if ($file->getFilename() === $filename) {
+                $files[] = $file->getPathname();
+            }
+        }
+
+        return $files;
+    }
+
+    private function convertCollectionToArray($collection)
+    {
+        $array = [];
+        foreach ($collection as $item) {
+            $array[] = $item;
+        }
+        return $array;
+    }
+
+    public function initQueueTransmission($action = false)
+    {
+        $resultList = [];
+        if ($this->messagesQueue && count($this->messagesQueue)) {
+            $batchSize = 3;
+            $batches = array_chunk($this->messagesQueue, $batchSize);
+            $processHandles = [];
+
+            foreach ($batches as $batch) {
+                $mailIds = array_map(fn($item) => $item['id'], $batch);
+                $encodedMailIds = implode(',', $mailIds);
+                $command = "php " . escapeshellarg($this->processMailScript) . " " . escapeshellarg($encodedMailIds);
+                $processHandles[] = $this->startProcess($command);
+            }
+
+            $this->monitorProcesses($processHandles);
+        } else {
+            $resultList[] = array(
+                'errorInfo' => 'No mail entry found in Database...'
+            );
+        }
+        return $resultList;
+    }
+
+    private function startProcess($command)
+    {
+        $descriptorSpec = [
+            0 => ["pipe", "r"], // stdin
+            1 => ["pipe", "w"], // stdout
+            2 => ["pipe", "w"]  // stderr
+        ];
+
+        $process = proc_open($command, $descriptorSpec, $pipes);
+        if (is_resource($process)) {
+            return [
+                'process' => $process,
+                'pipes' => $pipes
+            ];
+        } else {
+            $this->log("Failed to start process: $command");
+            return null;
+        }
+    }
+
+    private function monitorProcesses($processHandles)
+    {
+        $running = true;
+
+        while ($running) {
+            $running = false;
+            foreach ($processHandles as &$handle) {
+                if (is_resource($handle['process'])) {
+                    $status = proc_get_status($handle['process']);
+                    if ($status['running']) {
+                        $running = true;
+                    } else {
+                        $this->log("Process finished with command: " . $status['command']);
+                        proc_close($handle['process']);
+                        $handle['process'] = null;
+                    }
+                }
+            }
+            usleep(500000); // Sleep for 0.5 seconds before checking again
+        }
+        $this->logTotalExecutionTime(); // Log total execution time at the end
+    }
+
+    private function logTotalExecutionTime()
+    {
+        $endTime = microtime(true);
+        $executionTime = $endTime - $this->startTime;
+        $this->log(sprintf("Total execution time: %07.3f seconds", $executionTime));
+    }    
 
     public function log($message)
     {
+        if (is_array($message)) {
+            $message = print_r($message, true);
+        }
+        
         $time = $this->getSpendTime();
         $memory = memory_get_usage()/(1024*1024);
-        $text = sprintf("[Init Messaging log %07.3fs %07.1fmb] %s", "$time", $memory, $message);
+        $text = sprintf("[Init Messaging log %07.3fs %07.1fmb] %s", $time, $memory, $message);
         static::$logList[] = $text;
         if ($this->verbose) {
-            error_log('verbose is: '. $this->verbose);
+            error_log('verbose is: ' . $this->verbose);
             error_log($text);
         }
         return $this;
-    }
-
-    public static function getLogList()
-    {
-        return static::$logList;
-    }
-
-    public static function clearLogList()
-    {
-        static::$logList = [];
-    }
-
-    protected function getSpendTime()
-    {
-        $time = round(microtime(true) - $this->startTime, 3);
-        return $time;
-    }
-
-    protected function sendMailer(\BO\Zmsentities\Schema\Entity $entity, $mailer = null, $action = false)
-    {
-        // @codeCoverageIgnoreStart
-        $hasSendSuccess = ($action) ? $mailer->Send() : $action;
-        if (false !== $action && null !== $mailer && ! $hasSendSuccess) {
-            $this->log("Exception: SendingFailed  - ". \App::$now->format('c'));
-            throw new Exception\SendingFailed();
-        }
-        // @codeCoverageIgnoreEnd
-        $this->log("Send Mailer: sending succeeded - ". \App::$now->format('c'));
-        $log = new Mimepart(['mime' => 'text/plain']);
-        $log->content = ($entity instanceof Mail) ? $entity->subject : $entity->message;
-        $this->log("Send Mailer: log readPostResult start - ". \App::$now->format('c'));
-        \App::$http->readPostResult('/log/process/'. $entity->process['id'] .'/', $log);
-        $this->log("Send Mailer: log readPostResult finished - ". \App::$now->format('c'));
-        return $mailer;
-    }
-
-    protected function removeEntityOlderThanOneHour($entity)
-    {
-        if (3600 < \App::$now->getTimestamp() - $entity->createTimestamp) {
-            $this->log("Delete Entity: removeEntityOlderThanOneHour start - ". \App::$now->format('c'));
-            $this->deleteEntityFromQueue($entity);
-            $this->log("Delete Entity: removeEntityOlderThanOneHour finished - ". \App::$now->format('c'));
-            $log = new Mimepart(['mime' => 'text/plain']);
-            $log->content = 'Zmsmessaging Failure: Queue entry older than 1 hour has been removed';
-            $this->log("Delete Entity: log readPostResult start - ". \App::$now->format('c'));
-            \App::$http->readPostResult('/log/process/'. $entity->process['id'] .'/', $log, ['error' => 1]);
-            \App::$log->warning($log->content);
-            $this->log("Delete Entity: log readPostResult finished - ". \App::$now->format('c'));
-            return false;
-        }
-    }
-
-    public function deleteEntityFromQueue($entity)
-    {
-        $type = ($entity instanceof \BO\Zmsentities\Mail) ? 'mails' : 'notification';
-        try {
-            $this->log("Delete Entity: readDeleteResult start - ". \App::$now->format('c'));
-            $entity = \App::$http->readDeleteResult('/'. $type .'/'. $entity->id .'/')->getEntity();
-            $this->log("Delete Entity: readDeleteResult finished - ". \App::$now->format('c'));
-        } catch (\BO\Zmsclient\Exception $exception) {
-            throw $exception;
-        }
-        return ($entity) ? true : false;
-    }
-
-    public function testEntity($entity)
-    {
-        if (!isset($entity['department'])) {
-            throw new \Exception("Could not resolve department for message ".$entity['id']);
-        }
-        if (!isset($entity['department']['email'])) {
-            throw new \Exception(
-                "No mail address for department "
-                .$entity['department']['name']
-                ." (departmentID="
-                .$entity['department']['id']
-                ." Vorgang="
-                .$entity['process']['id']
-                .") "
-                .$entity['id']
-            );
-        }
-        if (! $entity->hasContent()) {
-            throw new \BO\Zmsmessaging\Exception\MailWithoutContent();
-        }
-
-        if ($entity instanceof Mail) {
-            $isMail = Validator::value($entity->getRecipient())->isMail()->getValue();
-            if (!$isMail) {
-                throw new \BO\Zmsmessaging\Exception\InvalidMailAddress();
-            }
-            if (\App::$verify_dns_enabled) {
-                $hasDns = Validator::value($entity->getRecipient())->isMail()->hasDNS()->getValue();
-                if (!$hasDns) {
-                    throw new \BO\Zmsmessaging\Exception\InvalidMailAddress();
-                }
-            }
-        }
     }
 }
