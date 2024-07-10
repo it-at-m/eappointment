9c0b1040698efcc2dfeae986d4787e15d860a3bc
Thomas Fink <thomas.fink@muenchen.de>
2024-07-10 10:02:35 +0200

    uncomment unit tests

--- zmsmessaging/src/Zmsmessaging/Mail.php
+++ zmsmessaging/src/Zmsmessaging/Mail.php
@@ -78,7 +78,7 @@ class Mail extends BaseController
     {
         $resultList = [];
         if ($this->messagesQueue && count($this->messagesQueue)) {
-            $batchSize = 5;
+            $batchSize = 2;
             $batches = array_chunk($this->messagesQueue, $batchSize);
             $processHandles = [];
 
