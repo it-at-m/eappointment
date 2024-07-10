2d842b257990e0ea050a8b7d40f923d8f5f30f46
Thomas Fink <thomas.fink@muenchen.de>
2024-07-10 13:04:33 +0200

    try batch 3

--- zmsmessaging/src/Zmsmessaging/Mail.php
+++ zmsmessaging/src/Zmsmessaging/Mail.php
@@ -86,7 +86,7 @@ class Mail extends BaseController
                 $mailIds = array_map(fn($item) => $item['id'], $batch);
                 $encodedMailIds = implode(',', $mailIds);
                 $command = "php " . escapeshellarg($this->processMailScript) . " " . escapeshellarg($encodedMailIds);
-                //$this->log("Starting process");
+                //$this->log("Starting process with command: $command");
                 $processHandles[] = $this->startProcess($command);
             }
 
@@ -109,7 +109,7 @@ class Mail extends BaseController
 
         $process = proc_open($command, $descriptorSpec, $pipes);
         if (is_resource($process)) {
-            //$this->log("Process started successfully");
+            $this->log("Process started successfully: $command");
         } else {
             $this->log("Failed to start process: $command");
         }
@@ -131,7 +131,7 @@ class Mail extends BaseController
                     if ($status['running']) {
                         $running = true;
                     } else {
-                        $this->log("Process finished");
+                        $this->log("Process finished with command: " . $status['command']);
                         proc_close($handle['process']);
                         $handle['process'] = null;
                     }
