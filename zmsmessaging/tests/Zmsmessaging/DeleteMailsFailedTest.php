cd76794b80ff0f6d7bb5b8293aa4d4915ce33dcb
Thomas Fink <thomas.fink@muenchen.de>
2024-07-11 16:02:19 +0200

    add lots of logging and disable queue deletion

--- zmsmessaging/tests/Zmsmessaging/DeleteMailsFailedTest.php
+++ zmsmessaging/tests/Zmsmessaging/DeleteMailsFailedTest.php
@@ -14,7 +14,7 @@ class DeleteMailsFailedTest extends Base
                 'url' => '/mails/',
                 'parameters' => [
                     'resolveReferences' => 2,
-                    'limit' => 50
+                    'limit' => 500
                 ],
                 'response' => $this->readFixture("GET_mails_queue.json"),
             ],
