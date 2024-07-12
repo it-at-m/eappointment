ea37d9d3272e0f8e1ebd292e87bdf12f5fd7a03a
Thomas Fink <thomas.fink@muenchen.de>
2024-07-12 11:47:02 +0200

    batch size 5

--- zmsmessaging/src/Zmsmessaging/Mail.php
+++ zmsmessaging/src/Zmsmessaging/Mail.php
@@ -93,7 +93,7 @@ class Mail extends BaseController
         $resultList = [];
         if ($this->messagesQueue && count($this->messagesQueue)) {
             $this->log("Messages found in queue, count: " . count($this->messagesQueue));
-            $batchSize = 10;
+            $batchSize = 5;
             $batches = array_chunk($this->messagesQueue, $batchSize);
             $processHandles = [];
 
