028e1a77621242bb911544f2d3f352fcdb3fbc96
Thomas Fink <thomas.fink@muenchen.de>
2024-07-15 16:28:59 +0200

    refactor

--- zmsmessaging/src/Zmsmessaging/process_mail.php
+++ zmsmessaging/src/Zmsmessaging/process_mail.php
@@ -1,5 +1,7 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception as PHPMailerException;
 use BO\Zmsmessaging\BaseController;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -12,23 +14,28 @@ class MailProcessor extends BaseController
         parent::__construct($verbose, $maxRunTime);
     }
 
-    protected function getMailById($itemId)
+    private function getMailById($itemId)
     {
         $endpoint = '/mails/' . $itemId . '/';
+        //$this->log("Fetching mail data from API endpoint: $endpoint\n\n");
+        //echo "Fetching mail data from API endpoint: $endpoint\n\n";
+
         try {
             $response = \App::$http->readGetResult($endpoint);
+            //$this->log("API Response: " . print_r($response, true) . "\n\n");
+            //echo "API Response: " . print_r($response, true) . "\n\n";
             return $response->getEntity();
         } catch (\Exception $e) {
             $this->log("Error fetching mail data: " . $e->getMessage() . "\n\n");
+            echo "Error fetching mail data: " . $e->getMessage() . "\n\n";
             return null;
         }
     }
 
-    protected function sendAndDeleteEmail($itemId)
+    public function sendAndDeleteEmail($itemId)
     {
-        $this->log("Fetching mail data for ID: $itemId\n\n");
-        echo "Fetching mail data for ID: $itemId\n\n";
-
+        $this->log("Fetching mail data for ID: $itemId");
+        echo "\nFetching mail data for ID: $itemId\n";
 
         $mailData = $this->getMailById($itemId);
 
@@ -38,8 +45,16 @@ class MailProcessor extends BaseController
             return;
         }
 
+        //$this->log("Mail data: " . print_r($mailData, true));
+        //echo "Mail data: " . print_r($mailData, true) . "\n\n";
+
         if ($mailData) {
+            //$this->log("Mail data found for ID: $itemId\n\n");
+            //echo "Mail data found for ID: $itemId\n\n";
             $entity = new \BO\Zmsentities\Mail($mailData);
+
+            //$this->log("Build Mailer: testEntity() - " . \App::$now->format('c'));
+            //echo "Build Mailer: testEntity() - " . \App::$now->format('c') . "\n\n";
             $this->testEntity($entity);
             $encoding = 'base64';
 
@@ -53,6 +68,9 @@ class MailProcessor extends BaseController
                 }
             }
 
+            //$this->log("Build Mailer: new PHPMailer() - " . \App::$now->format('c'));
+            //echo "Build Mailer: new PHPMailer() - " . \App::$now->format('c') . "\n\n";
+
             try {
                 $mailer = new PHPMailer(true);
                 $mailer->CharSet = 'UTF-8';
@@ -64,9 +82,13 @@ class MailProcessor extends BaseController
                 $mailer->AltBody = (isset($textPart)) ? $textPart : '';
                 $mailer->Body = (isset($htmlPart)) ? $htmlPart : '';
                 $mailer->SetFrom($entity['department']['email'], $entity['department']['name']);
+                //$this->log("Build Mailer: addAddress() - " . \App::$now->format('c'));
+                //echo "Build Mailer: addAddress() - " . \App::$now->format('c') . "\n\n";
                 $mailer->AddAddress($entity->getRecipient(), $entity->client['familyName']);
 
                 if (null !== $entity->getIcsPart()) {
+                    //$this->log("Build Mailer: AddStringAttachment() - " . \App::$now->format('c'));
+                    //echo "Build Mailer: AddStringAttachment() - " . \App::$now->format('c') . "\n\n";
                     $mailer->AddStringAttachment(
                         $icsPart,
                         "Termin.ics",
@@ -104,34 +126,34 @@ class MailProcessor extends BaseController
                     );
                     $this->deleteEntityFromQueue($entity);
                     $this->log("Mail sent and deleted successfully for ID: $itemId" . "\n\n");
+                    echo "Mail sent and deleted successfully for ID: $itemId\n\n";
                 } else {
                     $result = array(
                         'errorInfo' => $result->ErrorInfo
                     );
                     $this->log("Mail could not be sent. PHPMailer Error: {$result['errorInfo']}\n\n");
+                    echo "Mail could not be sent. PHPMailer Error: {$result['errorInfo']}\n\n";
                 }
 
             } catch (PHPMailerException $e) {
                 $this->log("Mail could not be sent. PHPMailer Error: {$e->getMessage()}\n\n");
+                echo "Mail could not be sent. PHPMailer Error: {$e->getMessage()}\n\n";
             } catch (Exception $e) {
                 $this->log("Mail could not be sent. General Error: {$e->getMessage()}\n\n");
+                echo "Mail could not be sent. General Error: {$e->getMessage()}\n\n";
             }
 
         } else {
             $this->log("Mail data not found for ID: $itemId\n\n");
+            echo "Mail data not found for ID: $itemId\n\n";
         }
-    }    
+    }
 }
 
 if ($argc > 1) {
     $mailIds = explode(',', $argv[1]);
     $processor = new MailProcessor();
     foreach ($mailIds as $mailId) {
-        $processor->log("Processing mail ID: $mailId");
-        echo "Processing mail ID: $mailId";
         $processor->sendAndDeleteEmail($mailId);
     }
-} else {
-    error_log("No mail IDs provided to process_mail.php");
-    echo "No mail IDs provided to process_mail.php";
 }
