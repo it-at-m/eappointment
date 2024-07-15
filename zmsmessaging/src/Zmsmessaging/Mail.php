ec26652743b6adc52ecb6d15108a57aac2ad8af7
Thomas Fink <thomas.fink@muenchen.de>
2024-07-15 14:57:35 +0200

    try resolve references 1

--- zmsmessaging/src/Zmsmessaging/Mail.php
+++ zmsmessaging/src/Zmsmessaging/Mail.php
@@ -19,7 +19,7 @@ class Mail extends BaseController
         $this->log("process_mail.php path: " . $this->processMailScript);
         $this->log("Read Mail QueueList start with limit " . \App::$mails_per_minute . " - " . \App::$now->format('c'));
         $queueList = \App::$http->readGetResult('/mails/', [
-            'resolveReferences' => 2,
+            'resolveReferences' => 1,
             'limit' => \App::$mails_per_minute
         ])->getCollection();
         if (null !== $queueList) {
