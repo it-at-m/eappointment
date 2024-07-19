174097c0f23a79f5a1d859d52ecf2250cedbb251
Thomas <thomas.fink@muenchen.de>
2024-07-19 15:27:00 +0200

    fix test

--- zmsmessaging/tests/Zmsmessaging/SendMailTest.php
+++ zmsmessaging/tests/Zmsmessaging/SendMailTest.php
@@ -28,11 +28,6 @@ class SendMailTest extends Base
                     'function' => 'readDeleteResult',
                     'url' => '/mails/1234/',
                     'response' => $this->readFixture("GET_mail.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/mails/1234/',
-                    'response' => $this->readFixture("GET_mail.json")
                 ]
             ]
         );
