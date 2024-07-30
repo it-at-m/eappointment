ef9a393b10f9fd1b823c6ad778f9ecfdb3603a98
Andrew Fink <thomasafink@Andrews-MacBook-Air.local>
2024-07-30 13:06:59 +0200

    Set limit to 200

--- zmsmessaging/tests/Zmsmessaging/DeleteMailsFailedTest.php
+++ zmsmessaging/tests/Zmsmessaging/DeleteMailsFailedTest.php
@@ -14,7 +14,7 @@ class DeleteMailsFailedTest extends Base
                 'url' => '/mails/',
                 'parameters' => [
                     'resolveReferences' => 2,
-                    'limit' => 300
+                    'limit' => 200
                 ],
                 'response' => $this->readFixture("GET_mails_queue.json"),
             ],
