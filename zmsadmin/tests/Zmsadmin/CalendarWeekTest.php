4614af44a26455b878f7d035a3b23e13f24c610e
Igor Manjencic <manjak.igor@gmail.com>
2024-08-07 08:32:55 +0200

    feat(ZMS-2828): fix tests

--- zmsadmin/tests/Zmsadmin/CalendarWeekTest.php
+++ zmsadmin/tests/Zmsadmin/CalendarWeekTest.php
@@ -136,7 +136,7 @@ class CalendarWeekTest extends Base
                 ]
             ]
         );
-        $response = $this->render($this->arguments, $this->parameters, []);
+        $response = $this->render($this->arguments, ['year' => 2016, 'weeknr' => 22], []);
         $this->assertStringContainsString('timeslot--free', (string)$response->getBody());
         $this->assertStringContainsString('Jetzt einen Termin um 13:10 buchen', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
