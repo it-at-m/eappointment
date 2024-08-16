0ea31f7816d9b7979d307010fc23ac9f74fd3cd6
Thomas Fink <thomasfink@Thomass-MacBook-Air.local>
2024-08-16 11:05:10 +0200

    add scope list endpoint

--- zmscitizenapi/src/Zmscitizenapi/ScopesList.php
+++ zmscitizenapi/src/Zmscitizenapi/ScopesList.php
@@ -10,6 +10,29 @@ class ScopesList extends BaseController
 {
     public function readResponse(RequestInterface $request, ResponseInterface $response, array $args)
     {
-        return Render::withJson($response, []);
+        $sources = \App::$http->readGetResult('/source/' . \App::$source_name . '/', [
+            'resolveReferences' => 2,
+        ])->getEntity();
+
+        $scopeList = $sources->getScopeList() ?? [];
+        $scopesProjectionList = [];
+
+        foreach ($scopeList as $scope) {
+            $scopesProjectionList[] = [
+                "id" => $scope->id,
+                "provider" => $scope->provider,
+                "shortName" => $scope->shortName,
+                "telephoneActivated" => $scope->getTelephoneActivated(),
+                "telephoneRequired" => $scope->getTelephoneRequired(),
+                "customTextfieldActivated" => $scope->getCustomTextfieldActivated(),
+                "customTextfieldRequired" => $scope->getCustomTextfieldRequired(),
+                "customTextfieldLabel" => $scope->getCustomTextfieldLabel(),
+                "captchaActivatedRequired" => $scope->getCaptchaActivatedRequired()
+            ];
+        }
+
+        return Render::withJson($response, [
+            "scopes" => $scopesProjectionList,
+        ]);
     }
 }
