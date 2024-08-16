ce9903e1e0aa32b3f6edc252378694e52ffbba3a
Thomas Fink <thomasfink@Thomass-MacBook-Air.local>
2024-08-16 10:18:18 +0200

    map offices to scope at office endpoint

--- zmscitizenapi/src/Zmscitizenapi/OfficesList.php
+++ zmscitizenapi/src/Zmscitizenapi/OfficesList.php
@@ -10,20 +10,45 @@ class OfficesList extends BaseController
 {
     public function readResponse(RequestInterface $request, ResponseInterface $response, array $args)
     {
-        $sources = \App::$http->readGetResult('/source/'.\App::$source_name.'/', [
+        $sources = \App::$http->readGetResult('/source/' . \App::$source_name . '/', [
             'resolveReferences' => 2,
         ])->getEntity();
 
+        $scopeList = $sources->getScopeList();
         $providerProjectionList = [];
+
         foreach ($sources->getProviderList() as $provider) {
-            $providerProjectionList[] = [
+            $matchingScope = null;
+            foreach ($scopeList as $scope) {
+                if ($scope->provider->id == $provider->id) {
+                    $matchingScope = $scope;
+                    break;
+                }
+            }
+            $providerData = [
                 "id" => $provider->id,
                 "name" => $provider->displayName ?? $provider->name,
             ];
+            if ($matchingScope) {
+                $providerData["scope"] = [
+                    "id" => $matchingScope->id,
+                    "provider" => $matchingScope->provider,
+                    "shortName" => $matchingScope->shortName,
+                    "telephoneActivated" => $matchingScope->getTelephoneActivated(),
+                    "telephoneRequired" => $matchingScope->getTelephoneRequired(),
+                    "customTextfieldActivated" => $matchingScope->getCustomTextfieldActivated(),
+                    "customTextfieldRequired" => $matchingScope->getCustomTextfieldRequired(),
+                    "customTextfieldLabel" => $matchingScope->getCustomTextfieldLabel(),
+                    "captchaActivatedRequired" => $matchingScope->getCaptchaActivatedRequired()
+                ];
+            }
+            
+            $providerProjectionList[] = $providerData;
         }
 
         return Render::withJson($response, [
             "offices" => $providerProjectionList,
         ]);
     }
+    
 }
