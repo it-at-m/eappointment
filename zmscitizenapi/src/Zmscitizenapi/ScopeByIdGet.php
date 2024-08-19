4b2097d22e18e390f5eb72f10227c68b54d53b7c
Thomas Fink <thomasfink@Thomass-MacBook-Air.local>
2024-08-19 12:21:45 +0200

    add scope by id list endpoint

--- zmscitizenapi/src/Zmscitizenapi/ScopeByIdGet.php
+++ zmscitizenapi/src/Zmscitizenapi/ScopeByIdGet.php
@@ -10,6 +10,64 @@ class ScopeByIdGet extends BaseController
 {
     public function readResponse(RequestInterface $request, ResponseInterface $response, array $args)
     {
-        return Render::withJson($response, []);
+        $scopeIds = explode(',', $request->getQueryParams()['scopeId'] ?? '');
+
+        if (empty($scopeIds) || $scopeIds == ['']) {
+            $response = $response->withStatus(400)
+                                 ->withHeader('Content-Type', 'application/json');
+            $response->getBody()->write(json_encode(['error' => 'Invalid scopeId(s)']));
+            return $response;
+        }
+
+        $sources = \App::$http->readGetResult('/source/' . \App::$source_name . '/', [
+            'resolveReferences' => 2,
+        ])->getEntity();
+        $scopeList = $sources->getScopeList();
+
+        $scopes = [];
+        $notFoundIds = [];
+
+        foreach ($scopeIds as $scopeId) {
+            $found = false;
+            foreach ($scopeList as $scopeItem) {
+                if ($scopeItem->id == $scopeId) {
+                    $scopes[] = [
+                        "id" => $scopeItem->id,
+                        "provider" => [
+                            "id" => $scopeItem->provider->id,
+                            "source" => $scopeItem->provider->source,
+                        ],
+                        "shortName" => $scopeItem->shortName,
+                        "telephoneActivated" => $scopeItem->getTelephoneActivated(),
+                        "telephoneRequired" => $scopeItem->getTelephoneRequired(),
+                        "customTextfieldActivated" => $scopeItem->getCustomTextfieldActivated(),
+                        "customTextfieldRequired" => $scopeItem->getCustomTextfieldRequired(),
+                        "captchaActivatedRequired" => $scopeItem->getCaptchaActivatedRequired(),
+                    ];
+                    $found = true;
+                    break;
+                }
+            }
+            if (!$found) {
+                $notFoundIds[] = $scopeId;
+            }
+        }
+
+        if (empty($scopes)) {
+            $response = $response->withStatus(404)
+                                 ->withHeader('Content-Type', 'application/json');
+            $response->getBody()->write(json_encode(['error' => 'Scope(s) not found']));
+            return $response;
+        }
+
+        $responseContent = ['scopes' => $scopes];
+        if (!empty($notFoundIds)) {
+            $responseContent['warning'] = 'The following scopeId(s) were not found: ' . implode(', ', $notFoundIds);
+        }
+
+        $response = $response->withStatus(200)
+                             ->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($responseContent));
+        return $response;
     }
 }
--- zmscitizenapi/tests/Zmscitizenapi/ScopeByIdGetTest.php
+++ zmscitizenapi/tests/Zmscitizenapi/ScopeByIdGetTest.php
@@ -2,10 +2,160 @@
 
 namespace BO\Zmscitizenapi\Tests;
 
+use BO\Slim\Render;
+
 class ScopeByIdGetTest extends Base
 {
-    public function testRendering() {
-        $responseData = $this->renderJson();
-        $this->assertEqualsCanonicalizing([], $responseData);
+
+    public function testRendering()
+    {
+        $this->setApiCalls([
+            [
+                'function' => 'readGetResult',
+                'url' => '/source/unittest/',
+                'parameters' => [
+                    'resolveReferences' => 2,
+                ],
+                'response' => $this->readFixture("GET_SourceGet_MultipleScopes.json"),
+            ]
+        ]);
+        $response = $this->render([], [
+            'scopeId' => '1'
+        ], []);
+        $expectedResponse = [
+            'scopes' => [
+                [
+                    'id' => '1',
+                    'provider' => [
+                        'id' => '9999998',
+                        'source' => 'unittest',
+                    ],
+                    'shortName' => 'Scope 1',
+                    'telephoneActivated' => '1',
+                    'telephoneRequired' => '0',
+                    'customTextfieldActivated' => '1',
+                    'customTextfieldRequired' => '0',
+                    'captchaActivatedRequired' => '1',
+                ]
+            ]
+        ];
+        $this->assertEqualsCanonicalizing($expectedResponse, json_decode((string)$response->getBody(), true));
     }
+  
+    public function testRenderingMulti()
+    {
+        $this->setApiCalls([
+            [
+                'function' => 'readGetResult',
+                'url' => '/source/unittest/',
+                'parameters' => [
+                    'resolveReferences' => 2,
+                ],
+                'response' => $this->readFixture("GET_SourceGet_MultipleScopes.json"),
+            ]
+        ]);
+        $response = $this->render([], [
+            'scopeId' => '1,2'
+        ], []);
+        $expectedResponse = [
+            'scopes' => [
+                [
+                    'id' => '1',
+                    'provider' => [
+                        'id' => '9999998',
+                        'source' => 'unittest',
+                    ],
+                    'shortName' => 'Scope 1',
+                    'telephoneActivated' => '1',
+                    'telephoneRequired' => '0',
+                    'customTextfieldActivated' => '1',
+                    'customTextfieldRequired' => '0',
+                    'captchaActivatedRequired' => '1',
+                ],
+                [
+                    'id' => '2',
+                    'provider' => [
+                        'id' => '9999999',
+                        'source' => 'unittest',
+                    ],
+                    'shortName' => 'Scope 2',
+                    'telephoneActivated' => '0',
+                    'telephoneRequired' => '1',
+                    'customTextfieldActivated' => '0',
+                    'customTextfieldRequired' => '1',
+                    'captchaActivatedRequired' => '0',
+                ],
+            ]
+        ];
+        $this->assertEqualsCanonicalizing($expectedResponse, json_decode((string)$response->getBody(), true));
+    }
+
+    public function testScopeNotFound()
+    {
+        $this->setApiCalls([
+            [
+                'function' => 'readGetResult',
+                'url' => '/source/unittest/',
+                'parameters' => [
+                    'resolveReferences' => 2,
+                ],
+                'response' => $this->readFixture("GET_SourceGet_MultipleScopes.json"),
+            ]
+        ]);
+        $response = $this->render([], [
+            'scopeId' => '99'
+        ], []);
+        $expectedResponse = [
+            'error' => 'Scope(s) not found'
+        ];
+        $this->assertEqualsCanonicalizing($expectedResponse, json_decode((string)$response->getBody(), true));
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testNoScopeIdProvided()
+    {
+        $response = $this->render([], [], []);
+        $expectedResponse = [
+            'error' => 'Invalid scopeId(s)'
+        ];
+        $this->assertEqualsCanonicalizing($expectedResponse, json_decode((string)$response->getBody(), true));
+        $this->assertEquals(400, $response->getStatusCode());
+    }
+
+    public function testPartialResultsWithWarning()
+    {
+        $this->setApiCalls([
+            [
+                'function' => 'readGetResult',
+                'url' => '/source/unittest/',
+                'parameters' => [
+                    'resolveReferences' => 2,
+                ],
+                'response' => $this->readFixture("GET_SourceGet_MultipleScopes.json"),
+            ]
+        ]);    
+        $response = $this->render([], [
+            'scopeId' => '1,99'
+        ], []);   
+        $expectedResponse = [
+            'scopes' => [
+                [
+                    'id' => '1',
+                    'provider' => [
+                        'id' => '9999998',
+                        'source' => 'unittest',
+                    ],
+                    'shortName' => 'Scope 1',
+                    'telephoneActivated' => '1',
+                    'telephoneRequired' => '0',
+                    'customTextfieldActivated' => '1',
+                    'customTextfieldRequired' => '0',
+                    'captchaActivatedRequired' => '1',
+                ]
+            ],
+            'warning' => 'The following scopeId(s) were not found: 99'
+        ];    
+        $this->assertEqualsCanonicalizing($expectedResponse, json_decode((string)$response->getBody(), true));
+        $this->assertEquals(200, $response->getStatusCode());
+    }       
 }
