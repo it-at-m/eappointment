8b8545e8852d5de1cd6b692f818843f4db71daf5
Thomas Fink <thomasfink@Thomass-MacBook-Air.local>
2024-08-19 15:06:41 +0200

    add missing text label

--- zmscitizenapi/src/Zmscitizenapi/ServicesByOfficeList.php
+++ zmscitizenapi/src/Zmscitizenapi/ServicesByOfficeList.php
@@ -10,6 +10,44 @@ class ServicesByOfficeList extends BaseController
 {
     public function readResponse(RequestInterface $request, ResponseInterface $response, array $args)
     {
-        return Render::withJson($response, []);
+        $officeId = $request->getQueryParams()['officeId'] ?? null;
+
+        if (is_null($officeId)) {
+            return $response->withStatus(400)
+                ->withHeader('Content-Type', 'application/json')
+                ->getBody()->write(json_encode(['error' => 'Invalid or missing officeId']));
+        }
+
+        $sources = \App::$http->readGetResult('/source/' . \App::$source_name . '/', [
+            'resolveReferences' => 2,
+        ])->getEntity();
+
+        $requestRelationList = $sources->getRequestRelationList();
+        $serviceIds = [];
+
+        foreach ($requestRelationList as $relation) {
+            if ($relation->provider->id == $officeId) {
+                $serviceIds[] = $relation->request->id;
+            }
+        }
+
+        $services = [];
+        foreach ($sources->getRequestList() as $request) {
+            if (in_array($request->id, $serviceIds)) {
+                $services[] = [
+                    'id' => $request->id,
+                    'name' => $request->name,
+                    'description' => $request->description ?? ''
+                ];
+            }
+        }
+
+        if (empty($services)) {
+            return $response->withStatus(404)
+                ->withHeader('Content-Type', 'application/json')
+                ->getBody()->write(json_encode(['error' => 'No services found for the provided officeId']));
+        }
+
+        return Render::withJson($response, ['services' => $services]);
     }
 }
