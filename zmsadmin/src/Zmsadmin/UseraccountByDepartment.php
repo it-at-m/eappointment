89562cdae7fe9dc7fe704820f0fe06a9051cddbb
Igor Manjencic <manjak.igor@gmail.com>
2024-09-30 08:25:29 +0200

    feat(ZMS-3141): optimize user list page

--- zmsadmin/src/Zmsadmin/UseraccountByDepartment.php
+++ zmsadmin/src/Zmsadmin/UseraccountByDepartment.php
@@ -6,7 +6,11 @@
 
 namespace BO\Zmsadmin;
 
+use App;
+use BO\Slim\Render;
 use \BO\Zmsentities\Collection\UseraccountList as Collection;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class UseraccountByDepartment extends BaseController
 {
@@ -15,19 +19,34 @@ class UseraccountByDepartment extends BaseController
      * @return String
      */
     public function readResponse(
-        \Psr\Http\Message\RequestInterface $request,
-        \Psr\Http\Message\ResponseInterface $response,
+        RequestInterface $request,
+        ResponseInterface $response,
         array $args
-    ) {
+    ): ResponseInterface
+    {
         $departmentId = $args['id'];
-        $workstation = \App::$http->readGetResult('/workstation/', ['resolveReferences' => 2])->getEntity();
-        $department = \App::$http->readGetResult("/department/$departmentId/")->getEntity();
-        $useraccountList = \App::$http->readGetResult("/department/$departmentId/useraccount/")->getCollection();
-        $workstationList = \App::$http->readGetResult("/department/$departmentId/workstation/")->getCollection();
 
-        $ownerList = \App::$http->readGetResult('/owner/', array('resolveReferences' => 2))->getCollection();
+        $workstation = App::$http->readGetResult('/workstation/', ['resolveReferences' => 1])->getEntity();
+        $department = App::$http->readGetResult("/department/$departmentId/")->getEntity();
+        $useraccountList = App::$http->readGetResult(
+            "/department/$departmentId/useraccount/",
+            ['resolveReferences' => 0]
+        )->getCollection();
+        $workstationList = App::$http->readGetResult(
+            "/department/$departmentId/workstation/",
+            ['resolveReferences' => 0]
+        )->getCollection();
+        $scopeList = App::$http->readGetResult('/scope/', ['resolveReferences' => 0])->getCollection();
 
-        return \BO\Slim\Render::withHtml(
+        /** @var Workstation $workstationItem */
+        foreach ($workstationList as $workstationItem) {
+            if ($scopeId = $workstationItem->getScope()->getId()) {
+                $workstationItem->scope = $scopeList->getEntity($scopeId);
+            }
+        }
+        $ownerList = App::$http->readGetResult('/owner/', array('resolveReferences' => 2))->getCollection();
+
+        return Render::withHtml(
             $response,
             'page/useraccount.twig',
             array(
