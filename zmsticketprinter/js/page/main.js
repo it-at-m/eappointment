1cdee9b695efc6c665f2c7f7bade8114ca03aae3
Igor Manjencic <manjak.igor@gmail.com>
2024-10-02 11:17:17 +0200

    fix(ZMS-3166): fix refreshing

--- zmsticketprinter/js/page/main.js
+++ zmsticketprinter/js/page/main.js
@@ -1,27 +1,26 @@
 import BaseView from '../lib/baseview';
 import $ from "jquery";
+
 class View extends BaseView {
 
     constructor (element) {
         super(element);
-        this.bindPublicMethods('setReloadInterval', 'reloadPage');
-        console.log('Redirect to home url every 30 seconds.');
-        this.setReloadInterval();
+        this.bindPublicMethods('setInterval', 'reloadPage');
+        console.log('Redirect to home url every 30 seconds');
+        $(function() {this.setInterval});
     }
 
     reloadPage () {
-        console.log('reload...')
-
-        $.get( this.getUrl('/home/'), function( response ) {
-            $("body").html($($.parseHTML(response)));
-        });
+        console.log('reload..');
+        window.location.href = this.getUrl('/home/');
     }
 
-    setReloadInterval () {
-        console.log('setInterval...');
+    setInterval () {
+        console.log('setInterval..');
         var reloadTime = window.bo.zmsticketprinter.reloadInterval;
         setInterval(this.reloadPage, reloadTime * 1000);
     }
+
     getUrl (relativePath) {
         let includepath = window.bo.zmsticketprinter.includepath;
         return includepath + relativePath;
