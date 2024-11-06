c15dea60bb29ddd0ea551f01c87156aa0f640b7d
Thomas Fink <thomas.fink@muenchen.de>
2024-11-06 16:04:32 +0100

    fix(MPDZBS-877): fix config.example.php

--- zmscitizenapi/config.example.php
+++ zmscitizenapi/config.example.php
@@ -1,19 +1,6 @@
 <?php
-define('ZMS_API_URL', function() {
-    $url = filter_var(getenv('ZMS_API_URL'), FILTER_SANITIZE_URL);
-    if ($url === false || empty($url)) {
-        return 'https://localhost/terminvereinbarung/api/2';
-    }
-    if (!filter_var($url, FILTER_VALIDATE_URL)) {
-        throw new \InvalidArgumentException('Invalid ZMS_API_URL provided');
-    }
-    return $url;
-});
-
-define('MAINTENANCE_MODE_ENABLED', function() {
-    $value = filter_var(getenv('MAINTENANCE_MODE_ENABLED'), FILTER_SANITIZE_STRING);
-    return in_array(strtolower($value), ["1", "true", "yes"], true);
-});
+define('ZMS_API_URL', getenv('ZMS_API_URL') ? getenv('ZMS_API_URL') : 'https://localhost/terminvereinbarung/api/2');
+define('MAINTENANCE_MODE_ENABLED', in_array(strtolower(getenv('MAINTENANCE_MODE_ENABLED')), ["1", "true", "yes"]));
 
 class App extends \BO\Zmscitizenapi\Application
 {
