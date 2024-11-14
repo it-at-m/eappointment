5faea71b407e34ed69ffc8ed072eeebec0c4f101
Tom Fink <tomfink@Toms-MacBook-Air.local>
2024-11-14 19:32:30 +0100

    fix(ZMS-3253): try fix some  unit tests

--- zmsapi/src/Zmsapi/AvailabilityAdd.php
+++ zmsapi/src/Zmsapi/AvailabilityAdd.php
@@ -71,6 +71,7 @@ class AvailabilityAdd extends BaseController
 
         $validation = $mergedCollection->validateInputs($startDate, $endDate);
         error_log("AvailabilityAdd");
+        error_log(json_encode($newAvailability));
         error_log("**********");
         error_log(json_encode($validation));
         if (count($validation) > 0) {
