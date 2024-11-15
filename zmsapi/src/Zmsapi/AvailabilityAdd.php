402531941d0a24cd7eed4cfd444e652bdb8de62c
Tom Fink <tomfink@Toms-MacBook-Air.local>
2024-11-15 19:10:08 +0100

    fix(ZMS-3253): remove error_logs

--- zmsapi/src/Zmsapi/AvailabilityAdd.php
+++ zmsapi/src/Zmsapi/AvailabilityAdd.php
@@ -85,14 +85,14 @@ class AvailabilityAdd extends BaseController
         }
 
         if (count($validation) > 0) {
-            error_log(json_encode($validation));
+            //error_log(json_encode($validation));
             throw new AvailabilityUpdateFailed();
         }        
     
         [$earliestStartDateTime, $latestEndDateTime] = $this->getDateTimeRangeFromCollection($mergedCollection,  \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $input['selectedDate'] . ' 00:00:00'));
         $conflicts = $mergedCollection->getConflicts($earliestStartDateTime, $latestEndDateTime);
         if ($conflicts->count() > 0) {
-            error_log(json_encode($conflicts));
+            //error_log(json_encode($conflicts));
             throw new AvailabilityUpdateFailed();
         }
 
