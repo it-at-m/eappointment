cffc6db4eed7307a55c70d046a9f113238358eb9
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2024-11-21 13:41:15 +0100

    clean(ZMS-2375):  set Twig cache

--- zmsstatistic/src/Zmsstatistic/Application.php
+++ zmsstatistic/src/Zmsstatistic/Application.php
@@ -23,7 +23,7 @@ class Application extends \BO\Slim\Application
 
     const DEBUG = false;
 
-    const TWIG_CACHE = false;
+    const TWIG_CACHE = '/cache/';
 
     const SESSION_DURATION = ZMS_STATISTIC_SESSION_DURATION;
 
