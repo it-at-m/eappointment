7c33566b92c1e2c37bdfed7ffb0badb17ff48286
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2024-11-29 10:16:45 +0100

    clean(ZMS-3377):  added new migration to change default text

---
+++ zmsdb/migrations/91732870750-custom-textfiled-label.sql
@@ -0,0 +1,3 @@
+UPDATE `standort`
+SET `custom_text_field_label` = 'Zusätzliche Bemerkungen (für Bürger*innen sichtbar)'
+WHERE `custom_text_field_label` = '' OR `custom_text_field_label` IS NULL OR `custom_text_field_label` = 'Zusätzliche Bemerkungen';
