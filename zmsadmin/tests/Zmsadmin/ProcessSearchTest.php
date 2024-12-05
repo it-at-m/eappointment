6803c8ebc03d9f61c29d9ee82b30a224200f6c9a
Igor Manjencic <manjak.igor@gmail.com>
2024-12-05 10:31:11 +0100

    feat(ZMS-3433): fix tests

--- zmsadmin/tests/Zmsadmin/ProcessSearchTest.php
+++ zmsadmin/tests/Zmsadmin/ProcessSearchTest.php
@@ -7,7 +7,7 @@ class ProcessSearchTest extends Base
     protected $arguments = [];
 
     protected $parameters = [
-        'query' => 'Test BO'
+        'query' => 'Test%20BO'
     ];
 
     protected $classname = "ProcessSearch";
@@ -27,14 +27,19 @@ class ProcessSearchTest extends Base
                     'url' => '/process/search/',
                     'parameters' => [
                         'resolveReferences' => 1,
-                        'query' => 'Test BO'
+                        'query' => 'Test%20BO'
                     ],
                     'response' => $this->readFixture("GET_searchresult.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/log/process/Test%20BO/',
+                    'response' => $this->readFixture("GET_loglist.json")
                 ]
             ]
         );
         $response = $this->render($this->arguments, $this->parameters, []);
-        $this->assertStringContainsString('Suchergebnisse für "Test BO"', (string)$response->getBody());
+        $this->assertStringContainsString('Suchergebnisse für "Test%20BO"', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
     }
 
@@ -86,41 +91,20 @@ class ProcessSearchTest extends Base
                     'url' => '/process/search/',
                     'parameters' => [
                         'resolveReferences' => 1,
-                        'query' => 'Test BO'
+                        'query' => 'Test%20BO'
                     ],
                     'response' => $this->readFixture("GET_searchresult_others.json")
+                ],
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/log/process/Test%20BO/',
+                    'response' => $this->readFixture("GET_loglist.json")
                 ]
             ]
         );
         $response = $this->render($this->arguments, $this->parameters, []);
         $this->assertStringContainsString('data-processList-count="5"', (string)$response->getBody());
         $this->assertStringContainsString('data-processListOther-count="0"', (string)$response->getBody());
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testShowingLogData()
-    {
-        $this->setApiCalls(
-            [
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/workstation/',
-                    'parameters' => ['resolveReferences' => 2],
-                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/process/search/',
-                    'parameters' => [
-                        'resolveReferences' => 1,
-                        'query' => 'Personalausweis beantragen'
-                    ],
-                    'response' => $this->readFixture("GET_searchresult.json")
-                ]
-            ]
-        );
-        $response = $this->render($this->arguments, $this->parameters, []);
-        $this->assertStringContainsString('Personalausweis beantragen"', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
     }
 }
