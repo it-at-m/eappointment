636981712bcae79e17d324909eabc338b2dcf3b7
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2025-01-22 16:59:46 +0100

    Update bootstrap.php

--- zmscitizenapi/bootstrap.php
+++ zmscitizenapi/bootstrap.php
@@ -42,7 +42,7 @@ $logger = new LoggerService();
 // Security middleware (order is important)
 App::$slim->add(new \BO\Zmscitizenapi\Middleware\LanguageMiddleware($logger));
 App::$slim->add(new \BO\Zmscitizenapi\Middleware\RequestLoggingMiddleware($logger));
-App::$slim->add(new \BO\Zmscitizenapi\Middleware\SecurityHeadersMiddleware($logger));
+//App::$slim->add(new \BO\Zmscitizenapi\Middleware\SecurityHeadersMiddleware($logger));
 //App::$slim->add(new \BO\Zmscitizenapi\Middleware\CorsMiddleware($logger));
 //App::$slim->add(new \BO\Zmscitizenapi\Middleware\CsrfMiddleware($logger));
 App::$slim->add(new \BO\Zmscitizenapi\Middleware\RateLimitingMiddleware($cache, $logger));
