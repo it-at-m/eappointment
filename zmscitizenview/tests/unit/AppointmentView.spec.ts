9e85e17e6092555e47a5d2de9a59d5005c5ac696
lehju <julia.lehmann@muenchen.de>
2025-02-04 16:38:10 +0100

    feat(ZMS-2936): add test

---
+++ zmscitizenview/tests/unit/AppointmentView.spec.ts
@@ -0,0 +1,40 @@
+import { shallowMount} from '@vue/test-utils';
+import { describe, it, expect, beforeEach } from 'vitest';
+import AppointmentView from './../../src/components/Appointment/AppointmentView.vue';
+import { createI18n } from "vue-i18n";
+import deDE from "./../../src/utils/de-DE.json";
+import enUS from "./../../src/utils/de-DE.json";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "de-DE",
+  messages: {
+    "de-DE": deDE,
+    "en-US": enUS,
+  },
+});
+
+describe('AppointmentView', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(AppointmentView, {
+      props: {
+        baseUrl: '',
+        t: i18n,
+      },
+    });
+  });
+
+  describe('Rendering', () => {
+    it('renders the component correctly', () => {
+      expect(wrapper.exists()).toBe(true);
+      expect(wrapper.findComponent({ name: 'muc-stepper' }).exists()).toBe(true);
+    });
+    it('displays the Service Finder when currentView is 0', async () => {
+      await wrapper.setData({ currentView: 0 });
+      expect(wrapper.findComponent({ name: 'service-finder' }).exists()).toBe(true);
+    });
+  });
+
+});
+
