7fdf903ced4f37be4083bf7758029c18a79a421a
Igor Manjenčić <manjak.igor@gmail.com>
2025-02-05 15:02:41 +0100

    feat(ZMS-3460): create endpoint for free appointments grouped by office (#797)
    * feat(ZMS-3460): create endpoint for free appointments grouped by office
    
    * feat(ZMS-3460): fix test
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix tests
    
    * feat(ZMS-3460): fix route
    
    * feat(ZMS-3460): refactoring
    
    * feat(ZMS-3460): show provider, not scope id
    
    * feat(ZMS-3460): fix provider id
    
    ---------
    
    Co-authored-by: Thomas Fink <tom@Thomass-MacBook-Air.local>

---
+++ zmscitizenapi/src/Zmscitizenapi/Models/AvailableAppointmentsByOffice.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace BO\Zmscitizenapi\Models;
+
+use BO\Zmsentities\Schema\Entity;
+use InvalidArgumentException;
+use JsonSerializable;
+
+class AvailableAppointmentsByOffice extends Entity implements JsonSerializable
+{
+
+    public static $schema = 'citizenapi/availableAppointmentsByOffice.json';
+
+    /** @var array|null */
+    public array|null $officeAppointments = [];
+
+    /**
+     * @param array $officeAppointments
+     */
+    public function __construct(array $officeAppointments = [])
+    {
+
+        $this->officeAppointments = $officeAppointments;
+
+        $this->ensureValid();
+    }
+
+    private function ensureValid(): void
+    {
+        if (!$this->testValid()) {
+            throw new InvalidArgumentException('The provided data is invalid according to the schema.');
+        }
+    }
+
+    /**
+     * Converts the model data back into an array for serialization.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'offices' => array_map(function($appointments, $officeId) {
+                return [
+                    'officeId' => $officeId,
+                    'appointments' => $appointments
+                ];
+            }, $this->officeAppointments, array_keys($this->officeAppointments))
+        ];
+    }
+
+    /**
+     * Implementation of JsonSerializable.
+     */
+    public function jsonSerialize(): mixed
+    {
+        return $this->toArray();
+    }
+}
