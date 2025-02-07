c14d27765eb259fafc7a99a8d75b6f94faef3fed
Thomas Fink <tom@Thomass-MacBook-Air.local>
2025-02-07 18:09:13 +0100

    fix(ZMS-3253): update button when disabled

--- zmsadmin/js/page/availabilityDay/form/formButtons.js
+++ zmsadmin/js/page/availabilityDay/form/formButtons.js
@@ -3,33 +3,33 @@ import PropTypes from 'prop-types'
 import moment from 'moment'
 
 const FormButtons = (props) => {
-    const { 
-        data, 
-        onCopy, 
-        onExclusion, 
-        onEditInFuture, 
-        onUpdateSingle, 
-        onDelete, 
-        selectedDate, 
-        hasConflicts, 
-        hasErrors, 
+    const {
+        data,
+        onCopy,
+        onExclusion,
+        onEditInFuture,
+        onUpdateSingle,
+        onDelete,
+        selectedDate,
+        hasConflicts,
+        hasErrors,
         hasSlotCountError,
         isCreatingExclusion
     } = props;
-    
+
     const disabled = ((data && (!data.id || data.__modified === true)) || hasConflicts || hasErrors || hasSlotCountError);
     const isPastDate = moment.unix(selectedDate).isBefore(moment(), 'day');
-    
+
     return (
         <div className="body">
             <div className="form-actions">
                 <button onClick={onDelete}
                     title="Ausgewählte Öffnungszeit löschen"
-                    className="button button--destructive button-delete" 
+                    className="button button--destructive button-delete"
                     disabled={disabled}>Löschen</button>
                 <button onClick={onCopy}
                     title="Öffnungszeit kopieren und bearbeiten"
-                    className="button button--diamond" 
+                    className="button button--diamond"
                     disabled={disabled || isPastDate}>Kopieren</button>
                 <button
                     onClick={onExclusion}
@@ -41,12 +41,23 @@ const FormButtons = (props) => {
                 </button>
                 <button onClick={onEditInFuture}
                     title="Öffnungszeit ab diesem Tag ändern"
-                    className="button button--diamond" 
+                    className="button button--diamond"
                     disabled={disabled || data.startDate == selectedDate || isPastDate}>Ab diesem Tag ändern</button>
                 <button onClick={onUpdateSingle}
                     title="Öffnungszeit aktualisieren"
-                    className="button button--diamond" 
-                    disabled={disabled || isCreatingExclusion || isPastDate}>Aktualisieren</button>
+                    className="button button--diamond"
+                    disabled={
+                        (data && !data.id) ||
+                        !data.__modified ||
+                        hasConflicts ||
+                        hasErrors ||
+                        hasSlotCountError ||
+                        isCreatingExclusion ||
+                        isPastDate
+                    }
+                >
+                    Aktualisieren
+                </button>
             </div>
         </div>
     )
