75f2396d9b79e8b9d6fc57caffaad966c37fe34b
Thomas Fink <tom@Thomass-MacBook-Air.local>
2025-02-10 11:33:00 +0100

    clean(ZMS-3414): zmsticketprinter readResponse function complexity

--- zmsticketprinter/src/Zmsticketprinter/Index.php
+++ zmsticketprinter/src/Zmsticketprinter/Index.php
@@ -23,84 +23,45 @@ class Index extends BaseController
      * @SuppressWarnings(UnusedFormalParameter)
      * @return ResponseInterface
      */
-    public function readResponse(
-        RequestInterface $request,
-        ResponseInterface $response,
-        array $args
-    ) {
+    public function readResponse(RequestInterface $request, ResponseInterface $response, array $args)
+    {
         Helper\HomeUrl::create($request);
-        $config = \App::$http->readGetResult('/config/', [], \App::SECURE_TOKEN)->getEntity();
+        $config = $this->getConfig();
+
         $validator = $request->getAttribute('validator');
-        $defaultTemplate = $validator->getParameter("template")
-            ->isPath()
-            ->setDefault('default');
-        $languageConfig = str_replace(' ', '+', $validator->getParameter("config")->isString()->getValue());
-        $languageConfig = json_decode(base64_decode($languageConfig), true);
-        $currentLang = $validator->getParameter("lang")->isString()->getValue();
-        $queryString = isset($_SERVER['QUERY_STRING']) ? str_replace('/&', '', $_SERVER['QUERY_STRING']) : '';
+        $defaultTemplate = $this->getDefaultTemplate($validator);
+        $languageConfig = $this->getLanguageConfig($validator);
 
-        if (! strpos($queryString, 'lang=')) {
-            $queryString .= '&lang=de';
-            $currentLang = 'de';
-        }
+        $currentLang = $this->getCurrentLanguage($validator);
+        $queryString = $this->getQueryStringWithLang();
 
-        $translations = [
-            'printText' => ''
-        ];
-        $languages = [];
-        $defaultLanguage = 'de';
+        $translations = $this->getTranslations($languageConfig, $currentLang);
+        $defaultLanguage = $languageConfig['defaultLanguage'] ?? 'de';
+        $languages = $this->getAvailableLanguages($languageConfig);
 
-        if ($languageConfig) {
-            $defaultLanguage = $languageConfig['defaultLanguage'] ?? '';
-            foreach ($languageConfig['languages'] as $language) {
-                $languages[] = $language['language'];
-
-                if ($language['language'] !== $currentLang) {
-                    continue;
-                }
-
-                foreach ($language['translations'] as $requestId => $translation) {
-                    $translations[$requestId] = $translation;
-                }
-            }
-        }
-
-        if (empty($currentLang) || $currentLang === 'de') {
-            $translations['printText'] = $languageConfig['defaultPrintText'] ?? '';
-        }
-
-        $ticketprinterHelper = (new Helper\Ticketprinter($args, $request));
+        $ticketprinterHelper = new Helper\Ticketprinter($args, $request);
         $ticketprinter = $ticketprinterHelper->getEntity();
+
         $ticketprinter->testValid();
         $scope = $ticketprinter->getScopeList()->getFirst();
-        $department = \App::$http->readGetResult('/scope/' . $scope->id . '/department/')->getEntity();
+        $department = $this->getDepartment($scope);
         $organisation = $ticketprinterHelper->getOrganisation();
 
-        //Check whether at least one button is not active (no opening hours stored or location deactivated)
-        $hasDisabledButton = false;
-        foreach ($ticketprinter->buttons as $button) {
-            if (!isset($button['enabled']) || $button['enabled'] != 1) {
-                $hasDisabledButton = true;
-                break;
-            }
-        }
-
-        if (1 == count($ticketprinter->buttons) && 'scope' == $ticketprinter->buttons[0]['type']) {
+        if ($this->shouldRedirectToScope($ticketprinter)) {
             return Render::redirect(
                 'TicketprinterByScope',
-                array(
-                    'scopeId' => $ticketprinter->buttons[0]['scope']['id']
-                ),
+                ['scopeId' => $ticketprinter->buttons[0]['scope']['id']],
                 $this->getQueryString($validator, $ticketprinter, $defaultTemplate)
             );
         }
+
         $template = (new Helper\TemplateFinder($defaultTemplate->getValue()))
             ->setCustomizedTemplate($ticketprinter, $organisation);
 
         return Render::withHtml(
             $response,
             $template->getTemplate(),
-            array(
+            [
                 'debug' => \App::DEBUG,
                 'refreshInSeconds' => 30,
                 'urlQueryString' => $queryString,
@@ -116,9 +77,83 @@ class Index extends BaseController
                 'defaultLanguage' => $defaultLanguage,
                 'languages' => $languages,
                 'translations' => $translations,
-                'hasDisabledButton' => $hasDisabledButton
-            )
+                'hasDisabledButton' => $this->hasDisabledButton($ticketprinter)
+            ]
         );
+    }
+
+    private function getConfig()
+    {
+        return \App::$http->readGetResult('/config/', [], \App::SECURE_TOKEN)->getEntity();
+    }
+
+    private function getDefaultTemplate($validator)
+    {
+        return $validator->getParameter("template")
+            ->isPath()
+            ->setDefault('default');
+    }
+
+    private function getLanguageConfig($validator)
+    {
+        return json_decode(base64_decode(str_replace(' ', '+', $validator->getParameter("config")->isString()->getValue())), true);
+    }
+
+    private function getCurrentLanguage($validator)
+    {
+        return $validator->getParameter("lang")->isString()->getValue();
+    }
+
+    private function getQueryStringWithLang()
+    {
+        $queryString = $_SERVER['QUERY_STRING'] ?? '';
+        if (!strpos($queryString, 'lang=')) {
+            $queryString .= '&lang=de';
+        }
+        return str_replace('/&', '', $queryString);
+    }
+
+    private function getTranslations($languageConfig, $currentLang)
+    {
+        $translations = ['printText' => ''];
+        if ($languageConfig) {
+            foreach ($languageConfig['languages'] as $language) {
+                if ($language['language'] !== $currentLang)
+                    continue;
+                foreach ($language['translations'] as $requestId => $translation) {
+                    $translations[$requestId] = $translation;
+                }
+            }
+            if (empty($currentLang) || $currentLang === 'de') {
+                $translations['printText'] = $languageConfig['defaultPrintText'] ?? '';
+            }
+        }
+        return $translations;
+    }
+
+    private function getAvailableLanguages($languageConfig)
+    {
+        return array_column($languageConfig['languages'] ?? [], 'language');
+    }
+
+    private function getDepartment($scope)
+    {
+        return \App::$http->readGetResult('/scope/' . $scope->id . '/department/')->getEntity();
+    }
+
+    private function shouldRedirectToScope($ticketprinter)
+    {
+        return count($ticketprinter->buttons) === 1 && $ticketprinter->buttons[0]['type'] === 'scope';
+    }
+
+    private function hasDisabledButton($ticketprinter)
+    {
+        foreach ($ticketprinter->buttons as $button) {
+            if (!isset($button['enabled']) || $button['enabled'] != 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -129,14 +164,13 @@ class Index extends BaseController
      */
     protected function getQueryString($validator, $ticketprinter, $defaultTemplate)
     {
-        $query = ($defaultTemplate->getValue() == 'default') ? [] : ['template' => $defaultTemplate->getValue()];
+        $query = ($defaultTemplate->getValue() === 'default') ? [] : ['template' => $defaultTemplate->getValue()];
         if (isset($ticketprinter['home'])) {
             $homeUrl = $validator::value($ticketprinter['home'])->isUrl()->getValue();
             if ($homeUrl) {
                 $query['ticketprinter[home]'] = $homeUrl;
             }
         }
-
         return $query;
     }
 }
