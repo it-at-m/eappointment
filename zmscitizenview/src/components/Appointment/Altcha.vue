2b7273500f45f962a7e5a7ee53b1fa04ef9db050
Fabinatix97 <fabian@fabi-online.de>
2025-04-07 13:15:57 +0200

    feat(TV-57): update Altcha.vue component and add api endpoints

--- zmscitizenview/src/components/Appointment/Altcha.vue
+++ zmscitizenview/src/components/Appointment/Altcha.vue
@@ -1,114 +1,97 @@
+<template>
+  <div v-if="captchaEnabled && captchaChallengeUrl && captchaVerifyUrl">
+    <altcha-widget
+      :challengeurl="captchaChallengeUrl"
+      :verifyurl="captchaVerifyUrl"
+      ref="altchaWidget"
+    />
+  </div>
+  <div v-else>
+    <p>Das Captcha konnte nicht geladen werden.</p>
+  </div>
+</template>
+
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, watch } from "vue";
+import type { AltchaWidget } from "@/types/AltchaTypes";
+
+import { nextTick, onMounted, onUnmounted, ref } from "vue";
 
 import {
   getAPIBaseURL,
+  VUE_APP_ZMS_API_CAPTCHA_CHALLENGE_ENDPOINT,
   VUE_APP_ZMS_API_CAPTCHA_DETAILS_ENDPOINT,
+  VUE_APP_ZMS_API_CAPTCHA_VERIFY_ENDPOINT,
 } from "@/utils/Constants";
 
 import "altcha";
 
-const altchaWidget = ref<HTMLElement | null>(null);
+const altchaWidget = ref<Partial<AltchaWidget> | null>(null);
+const getWidget = () => altchaWidget.value as AltchaWidget;
+
 const captchaChallengeUrl = ref<string | null>(null);
 const captchaVerifyUrl = ref<string | null>(null);
 const captchaEnabled = ref<boolean>(true);
 
-const props = defineProps({
-  payload: {
-    type: String,
-    required: false,
-  },
-});
 const emit = defineEmits<{
-  (e: "update:payload", value: string): void;
   (e: "validationResult", value: boolean): void;
 }>();
-
-const internalValue = ref(props.payload);
-
-watch(internalValue, (v) => {
-  emit("update:payload", v || "");
-});
 
 const fetchCaptchaDetails = async () => {
   try {
     const response = await fetch(
-      `${getAPIBaseURL(import.meta.env.VITE_VUE_APP_API_URL)}${VUE_APP_ZMS_API_CAPTCHA_DETAILS_ENDPOINT}` // corresponds to https://zms.ddev.site/terminvereinbarung/api/citizen/captcha-details/
+      `${getAPIBaseURL(import.meta.env.VITE_VUE_APP_API_URL)}${VUE_APP_ZMS_API_CAPTCHA_DETAILS_ENDPOINT}` // https://zms.ddev.site/terminvereinbarung/api/citizen/captcha-details/
     );
     if (!response.ok) throw new Error("Fehler beim Laden der Captcha-Daten");
-
     const data = await response.json();
     console.log("DATA:", data);
-    captchaChallengeUrl.value = data.captchaChallenge;
-    captchaVerifyUrl.value = data.captchaVerify;
+    captchaChallengeUrl.value = `${getAPIBaseURL(import.meta.env.VITE_VUE_APP_API_URL)}${VUE_APP_ZMS_API_CAPTCHA_CHALLENGE_ENDPOINT}`; // http://zms.ddev.site/terminvereinbarung/api/citizen/captcha-challenge/
+    captchaVerifyUrl.value = `${getAPIBaseURL(import.meta.env.VITE_VUE_APP_API_URL)}${VUE_APP_ZMS_API_CAPTCHA_VERIFY_ENDPOINT}`; // http://zms.ddev.site/terminvereinbarung/api/citizen/captcha-verify/
     captchaEnabled.value = data.captchaEnabled;
   } catch (error) {
     console.error("Fehler beim Abrufen der Captcha-Details:", error);
   }
 };
 
-const verifyCaptcha = async (payload: string) => {
-  console.log("VERIFIYING");
-  if (!captchaVerifyUrl.value) return;
-  console.log("VERIFY");
-  try {
-    const response = await fetch(captchaVerifyUrl.value, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        siteKey: "zms-dev",
-        payload: payload,
-      }),
-    });
-
-    const data = await response.json();
-    if (data.success) {
+const handleStateChange = (ev: CustomEvent | Event) => {
+  if ("detail" in ev) {
+    console.log("EVENT:", ev);
+    const state = ev.detail.state;
+    console.log("STATE:", state);
+    if (state === "verified") {
       emit("validationResult", true);
     } else {
       emit("validationResult", false);
-      internalValue.value = "";
     }
-  } catch (error) {
-    console.error("Fehler bei der Captcha-Validierung:", error);
-    emit("validationResult", false);
   }
 };
 
-const onStateChange = (ev: CustomEvent | Event) => {
-  if ("detail" in ev) {
-    const { payload, state } = ev.detail;
-    console.log("STATE:", state);
-    if (state === "verified" && payload) {
-      internalValue.value = payload;
-      verifyCaptcha(payload);
-      console.log("FINISHED");
-    } else {
-      internalValue.value = "";
-      emit("validationResult", false);
-    }
+const configureWidget = () => {
+  const widget = getWidget();
+  if (widget) {
+    widget.configure({
+      strings: {
+        error: "Verifizierung fehlgeschlagen. Versuche es später noch einmal.",
+        expired: "Verifizierung abgelaufen. Versuche es erneut.",
+        footer:
+          'Geschützt durch <a href=\"https://altcha.org/\" target=\"_blank\" aria-label=\"Besuche Altcha.org\">ALTCHA</a>',
+        label: "Ich bin kein Bot.",
+        verified: "Erfolgreich verifiziert!",
+        verifying: "Überprüfe...",
+        waitAlert: "Überprüfung läuft... bitte warten.",
+      },
+    });
   }
 };
 
 onMounted(async () => {
   await fetchCaptchaDetails();
-
-  if (altchaWidget.value) {
-    altchaWidget.value.addEventListener("statechange", onStateChange);
-  }
+  getWidget()?.addEventListener("statechange", handleStateChange);
+  nextTick(() => {
+    configureWidget();
+  });
 });
 
 onUnmounted(() => {
-  if (altchaWidget.value) {
-    altchaWidget.value.removeEventListener("statechange", onStateChange);
-  }
+  getWidget()?.removeEventListener("statechange", handleStateChange);
 });
 </script>
-
-<template>
-  <altcha-widget
-    v-if="captchaEnabled && captchaChallengeUrl"
-    :challengeurl="captchaChallengeUrl"
-    ref="altchaWidget"
-    debug
-  ></altcha-widget>
-</template>
