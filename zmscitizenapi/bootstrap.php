87aece75878a3e648c855103e3318b5035bce7a9
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2025-04-25 10:22:14 +0200

    Update bootstrap.php

--- zmscitizenapi/bootstrap.php
+++ zmscitizenapi/bootstrap.php
@@ -44,7 +44,7 @@ App::$slim->add(new \BO\Zmscitizenapi\Middleware\LanguageMiddleware($logger));
 App::$slim->add(new \BO\Zmscitizenapi\Middleware\RequestLoggingMiddleware($logger));
 App::$slim->add(new \BO\Zmscitizenapi\Middleware\SecurityHeadersMiddleware($logger));
 // Todo: Enable CORS when the old frontend is replaced with Zmscitizenview
-//App::$slim->add(new \BO\Zmscitizenapi\Middleware\CorsMiddleware($logger));
+App::$slim->add(new \BO\Zmscitizenapi\Middleware\CorsMiddleware($logger));
 // Todo: Implement CSRF with Zmscitizenview
 //App::$slim->add(new \BO\Zmscitizenapi\Middleware\CsrfMiddleware($logger));
 App::$slim->add(new \BO\Zmscitizenapi\Middleware\RateLimitingMiddleware($cache, $logger));
