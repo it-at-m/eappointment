393d6ee8c151f4bbf418adf9cbe37bf7f9e0bacb
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-04-30 12:34:22 +0200

    feat(ZMSKVR-327)  Migration für die Tabelle 'gesamtkalender' erstellen

---
+++ zmsdb/migrations/91745403315-create-gesamtkalender-table.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS `gesamtkalender`;
+CREATE TABLE `gesamtkalender` (
+    `id`         INT          UNSIGNED NOT NULL AUTO_INCREMENT,
+    `scope_id`   INT          UNSIGNED NOT NULL,
+    `time`       DATETIME              NOT NULL,
+    `process_id` INT          UNSIGNED NULL,
+    `slots`      INT          UNSIGNED NULL,
+    `status`     VARCHAR(64)           NOT NULL DEFAULT 'closed',
+    `updated_at` TIMESTAMP             NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uk_scope_time` (`scope_id`, `time`),
+
+    KEY `idx_status`    (`status`),
+    KEY `idx_process`   (`process_id`),
+    KEY `idx_updated`   (`updated_at`)
+) ENGINE = InnoDB
+    DEFAULT CHARSET = utf8
+    COLLATE = utf8_unicode_ci;
