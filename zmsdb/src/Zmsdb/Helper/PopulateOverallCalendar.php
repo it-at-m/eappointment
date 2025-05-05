49515adc0407d65ad94f217220b1e699f6f3d898
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-05-05 12:27:14 +0200

    feat(ZMSKVR-327) generate records for calender considering seats

--- zmsdb/src/Zmsdb/Helper/PopulateOverallCalendar.php
+++ zmsdb/src/Zmsdb/Helper/PopulateOverallCalendar.php
@@ -2,9 +2,11 @@
 
 namespace BO\Zmsdb\Helper;
 
+use BO\Zmsdb\Availability;
+use BO\Zmsdb\Connection\Select;
 use BO\Zmsdb\OverallCalendar;
-use BO\Zmsdb\Query\OverallCalendar as Q;
-use BO\Zmsentities\Availability;
+use BO\Zmsdb\Scope;
+use DateTimeInterface;
 
 class PopulateOverallCalendar extends CalculateSlots
 {
@@ -18,63 +20,55 @@ class PopulateOverallCalendar extends CalculateSlots
     }
 
 
-    public function writeCalendar(\DateTimeInterface $now): void
+    public function writeCalendar(DateTimeInterface $now): void
     {
         $this->log('PopulateOverallCalendar gestartet ' . $now->format('c'));
 
-        foreach ((new \BO\Zmsdb\Scope())->readList() as $scope) {
-            $this->writeClosedRaster($scope->id, $now);
+        foreach ((new Scope())->readList() as $scope) {
             $this->updateFreeByAvailabilities($scope->id, $now);
         }
-        \BO\Zmsdb\Connection\Select::writeCommit();
+        Select::writeCommit();
         $this->log('PopulateOverallCalendar beendet');
     }
 
-
-    private function writeClosedRaster(int $scopeId, \DateTimeInterface $now): void
+    private function updateFreeByAvailabilities(int $scopeId,
+                                                DateTimeInterface $now): void
     {
-        if ($this->cal->existsToday($scopeId)) {
-            return;
-        }
-        $start = (new \DateTimeImmutable('today', $now->getTimezone()))->setTime(0, 0);
-        for ($i = 0; $i < 288; $i++) {
-            $slotTime = $start->add(new \DateInterval('PT' . ($i * 5) . 'M'));
-            $this->cal->insertClosed($scopeId, $slotTime);
-        }
-        $this->log("Raster 'closed' für Scope $scopeId erzeugt");
-    }
 
-
-    private function updateFreeByAvailabilities(int $scopeId, \DateTimeInterface $now): void
-    {
-        $from = $now->setTime(0, 0)->format('Y-m-d H:i:s');
+        $from = $now->setTime(0,0)->format('Y-m-d H:i:s');
         $to   = (clone $now)->modify('+' . self::MAX_DAYS . ' days 23:59:59')
             ->format('Y-m-d H:i:s');
 
-        $this->cal->resetRange($scopeId, $from, $to);
+        $this->cal->deleteFreeRange($scopeId, $from, $to);
 
-        $availList = (new \BO\Zmsdb\Availability())
-            ->readAvailabilityListByScope(
-                (new \BO\Zmsentities\Scope(['id' => $scopeId]))
-            );
+        $availList = (new Availability())
+            ->readAvailabilityListByScope(new \BO\Zmsentities\Scope(['id'=>$scopeId]));
 
-        foreach ($availList as $a) {
-            $cursor = $a->getBookableStart($now);
-            $endAll = $a->getBookableEnd($now);
+        foreach ($availList as $availability) {
+
+            $cursor = $availability->getBookableStart($now);
+            $endAll = $availability->getBookableEnd($now);
 
             while ($cursor <= $endAll) {
-                if ($a->hasDate($cursor, $now)) {
-                    $dayStart  = $cursor->setTime(...explode(':', $a->startTime));
-                    $dayFinish = $cursor->setTime(...explode(':', $a->endTime));
-                    $this->cal->openRange(
-                        $scopeId,
-                        $dayStart ->format('Y-m-d H:i:s'),
-                        $dayFinish->format('Y-m-d H:i:s')
-                    );
+
+                if ($availability->hasDate($cursor, $now)) {
+
+                    $slot     = $cursor->setTime(...explode(':', $availability->startTime));
+                    $slotEnd  = $cursor->setTime(...explode(':', $availability->endTime));
+                    $maxSeat  = (int)$availability->workstationCount['intern'];
+
+                    while ($slot < $slotEnd) {
+
+                        for ($seat = 1; $seat <= $maxSeat; $seat++) {
+                            $this->cal->insertSlot($scopeId, $slot, $seat);
+                        }
+                        $slot = $slot->modify('+5 minutes');
+                    }
                 }
                 $cursor = $cursor->modify('+1 day');
             }
         }
-        $this->log("Status 'free' für Scope $scopeId gesetzt");
+
+        $this->log("Freie Slots (inkl. Seats) für Scope $scopeId neu aufgebaut");
     }
 }
