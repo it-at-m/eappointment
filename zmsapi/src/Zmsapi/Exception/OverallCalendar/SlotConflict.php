e7b4cb8cdb6c7564d0c58e8a04dd4596b9e6aa29
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-05-05 12:21:54 +0200

    feat(ZMSKVR-327)  Exceptions for zmsdb and zmsapi

---
+++ zmsapi/src/Zmsapi/Exception/OverallCalendar/SlotConflict.php
@@ -0,0 +1,10 @@
+<?php
+namespace BO\Zmsapi\Exception\OverallCalendar;
+
+use Exception;
+
+class SlotConflict extends Exception
+{
+    protected $statusCode = 409;
+    protected $message    = 'Die gewünschten Slots sind nicht verfügbar.';
+}
