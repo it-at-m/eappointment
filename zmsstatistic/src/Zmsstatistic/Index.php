c013a76376ebfc9b4f4704e0d43c4ecd78c01f3a
ThomasAFink <thomas.fink@muenchen.de>
2025-06-06 15:41:31 +0200

    fix: handle null values in getProviderList to resolve PHP 8.3 explode deprecation

--- zmsstatistic/src/Zmsstatistic/Index.php
+++ zmsstatistic/src/Zmsstatistic/Index.php
@@ -138,7 +138,8 @@ class Index extends BaseController
     }
     protected function getProviderList($config)
     {
-        $allowedProviderList = explode(',', $config->getPreference('oidc', 'provider'));
+        $providerConfig = $config->getPreference('oidc', 'provider');
+        $allowedProviderList = $providerConfig ? explode(',', $providerConfig) : [];
         $oidcproviderlist = [];
         foreach (\BO\Slim\Middleware\OAuthMiddleware::$authInstances as $provider => $authInstance) {
             if (
