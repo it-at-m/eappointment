0ea9ebd71f918e8484adfcfd5adafc553a339b08
ThomasAFink <thomas.fink@muenchen.de>
2025-06-11 10:10:17 +0200

    fix(ZMSKVR-204): update test to handle validation message object

--- zmsadmin/tests/Zmsadmin/ProcessReserveTest.php
+++ zmsadmin/tests/Zmsadmin/ProcessReserveTest.php
@@ -318,8 +318,8 @@ class ProcessReserveTest extends Base
         
         $responseData = json_decode((string)$response->getBody(), true);
         $this->assertTrue($responseData['failed']);
-        $this->assertStringContainsString('den Standort muss eine', $responseData['email']['messages'][0]);
-        $this->assertStringContainsString('E-Mail Adresse eingetragen werden', $responseData['email']['messages'][0]);
+        $this->assertStringContainsString('den Standort muss eine', $responseData['email']['messages'][0]['message']);
+        $this->assertStringContainsString('E-Mail Adresse eingetragen werden', $responseData['email']['messages'][0]['message']);
         $this->assertEquals(200, $response->getStatusCode());
     }
 
