90e217ba40822ee14551f4a2c454b8c55db83319
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-06-16 08:21:32 +0200

    zmskvr-474: zmsadmin/src/Zmsadmin/OverallCalendar.php rendert die Twig

---
+++ zmsadmin/src/Zmsadmin/OverallCalendar.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BO\Zmsadmin;
+
+use BO\Mellon\Validator;
+use BO\Zmsdb\Request;
+use BO\Zmsentities\Collection\RequestList;
+use BO\Zmsentities\Department as DepartmentEntity;
+use BO\Zmsentities\Collection\DepartmentList;
+
+class OverallCalendar extends BaseController
+{
+    /**
+     * @SuppressWarnings(Param)
+     * @return String
+     */
+    public function readResponse(
+        \Psr\Http\Message\RequestInterface $request,
+        \Psr\Http\Message\ResponseInterface $response,
+        array $args
+    ) {
+        $workstation = \App::$http->readGetResult('/workstation/', ['resolveReferences' => 1])->getEntity();
+
+        if (!$workstation->hasSuperUseraccount()) {
+            throw new Exception\NotAllowed();
+        }
+        $scopeId = $workstation->scope->id ?? null;
+
+        $department = null;
+        if ($scopeId) {
+            $departmentApiResult = \App::$http->readGetResult(
+                '/scope/' . $scopeId . '/department/',
+                ['resolveReferences' => 2]
+            );
+            $data = $departmentApiResult ? $departmentApiResult->getEntity() : null;
+
+            if ($data) {
+                $department = new \BO\Zmsentities\Department($data);
+            }
+        }
+        $departmentScopes = $department->getScopeList();
+
+        return \BO\Slim\Render::withHtml(
+            $response,
+            'page/overallCalendar.twig',
+            array(
+                'title' => 'Wochenkalender',
+                'workstation' => $workstation,
+                'scopeList' => $departmentScopes,
+                'menuActive' => 'overallcalendar',
+                'hideNavigation'=> true,
+            )
+        );
+    }
+}
