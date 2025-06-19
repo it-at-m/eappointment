76b778700122e17fec654a26d770a5bf4244ec09
ThomasAFink <thomas.fink@muenchen.de>
2025-06-19 13:22:02 +0200

    fix(ZMSKVR-96); readd keepLessData

--- zmsapi/src/Zmsapi/ProcessFreeUnique.php
+++ zmsapi/src/Zmsapi/ProcessFreeUnique.php
@@ -25,6 +25,7 @@ class ProcessFreeUnique extends BaseController
         $slotsRequired = Validator::param('slotsRequired')->isNumber()->getValue();
         $groupData = Validator::param('groupData')->isNumber()->getValue();
         $slotType = Validator::param('slotType')->isString()->getValue();
+        $keepLessData = Validator::param('keepLessData')->isArray()->setDefault([])->getValue();
         if ($slotType || $slotsRequired) {
             (new Helper\User($request))->checkRights();
         } else {
@@ -36,7 +37,8 @@ class ProcessFreeUnique extends BaseController
         $calendar = new \BO\Zmsentities\Calendar($calendarData);
         $message = Response\Message::create($request);
         $processList = (new Query())
-            ->readFreeProcesses($calendar, \App::getNow(), $slotType, $slotsRequired, $groupData ? true : false);
+            ->readFreeProcesses($calendar, \App::getNow(), $slotType, $slotsRequired, $groupData ? true : false)
+            ->withLessData($keepLessData);
 
         // Deduplicate processes with same provider (office) and appointment date
         $uniqueProcesses = [];
