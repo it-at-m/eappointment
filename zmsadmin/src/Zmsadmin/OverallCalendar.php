ffa3e7818997bf6cbe05d63f07876167ec01d062
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-07-01 07:14:17 +0200

    zmskvr-562: only superuser

--- zmsadmin/src/Zmsadmin/OverallCalendar.php
+++ zmsadmin/src/Zmsadmin/OverallCalendar.php
@@ -24,7 +24,7 @@ class OverallCalendar extends BaseController
             throw new \Exception('Unable to retrieve workstation data');
         }
         $workstation = $result->getEntity();
-        if (!$workstation->getUseraccount()->hasRights(['scope'])) {
+        if (!$workstation->getUseraccount()->hasRights(['superuser'])) {
             throw new \BO\Zmsentities\Exception\UserAccountMissingRights();
         }
 
