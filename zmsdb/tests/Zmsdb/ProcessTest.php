46e7534a8929480d500a369cb9b5fdc39c62e6ea
Igor Manjencic <manjak.igor@gmail.com>
2025-07-02 19:33:08 +0200

    feat(ZMSKVR-537): fix test

--- zmsdb/tests/Zmsdb/ProcessTest.php
+++ zmsdb/tests/Zmsdb/ProcessTest.php
@@ -139,9 +139,8 @@ class ProcessTest extends Base
         $this->assertEquals('Unbekannt', $process->getClients()->getLast()->familyName);
 
         $processList = $query->readEntityList($process->id);
-        foreach ($processList as $processItem) {
-            $this->assertNotEquals('reserved', $processItem->status);
-        }
+        $processItem = $processList->getFirst();
+        $this->assertNotEquals('reserved', $processItem->status);
 
         $process = $query->deleteEntity($process->id, $process->authKey);
 
