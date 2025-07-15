b4b775b73caf3c864ae168c5af16d981c91eb8ef
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-07-15 13:28:24 +0200

    zmskvr-599: Fremdschlüssel-Constraint angepasst

---
+++ zmsdb/migrations/91752567095-gesamtkalender-availability-nullable.sql
@@ -0,0 +1,14 @@
+ALTER TABLE `gesamtkalender`
+    MODIFY `availability_id` INT UNSIGNED NULL;
+
+ALTER TABLE `gesamtkalender`
+    ADD INDEX `idx_scope_time_seat` (`scope_id`, `time`, `seat`);
+
+ALTER TABLE `gesamtkalender`
+DROP INDEX `uk_scope_time`;
+
+ALTER TABLE `gesamtkalender`
+    ADD UNIQUE KEY `uk_scope_time` (`scope_id`, `time`, `seat`);
+
+ALTER TABLE `gesamtkalender`
+DROP INDEX `idx_scope_time_seat`;
