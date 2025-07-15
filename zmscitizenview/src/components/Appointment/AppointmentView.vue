24b32d61f0c9939ae0a535eadeb9ab75cd9be322
fabian.weiss <fabian.weiss@muenchen.de>
2025-07-15 10:38:28 +0200

    feat(ZMSKVR-590): minor refactoring

--- zmscitizenview/src/components/Appointment/AppointmentView.vue
+++ zmscitizenview/src/components/Appointment/AppointmentView.vue
@@ -605,6 +605,39 @@ const parseAppointmentHash = (hash: string): AppointmentHash | null => {
 };
 
 onMounted(() => {
+  if (props.serviceId && props.locationId) {
+    fetchServicesAndProviders(
+      props.serviceId,
+      props.locationId,
+      props.baseUrl ?? undefined
+    ).then((data) => {
+      services.value = data.services;
+      relations.value = data.relations;
+      offices.value = data.offices;
+
+      const foundService = services.value.find(
+        (service) => service.id == props.serviceId
+      );
+      const foundOffice = offices.value.find(
+        (office) => office.id == props.locationId
+      );
+
+      if (foundService && foundOffice) {
+        selectedService.value = foundService as ServiceImpl;
+        selectedService.value.count = 1;
+        selectedService.value.providers = getProviders(
+          selectedService.value.id,
+          null
+        );
+        preselectedLocationId.value = props.locationId;
+        selectedProvider.value = foundOffice as OfficeImpl;
+        selectedServiceMap.value = new Map<string, number>();
+        selectedServiceMap.value.set(foundService.id, 1);
+        currentView.value = 1;
+      }
+    });
+  }
+
   if (props.confirmAppointmentHash) {
     const appointmentData = parseAppointmentHash(props.confirmAppointmentHash);
     if (!appointmentData) {
@@ -709,37 +742,6 @@ onMounted(() => {
           }
         }
       );
-    });
-  } else if (props.serviceId && props.locationId) {
-    fetchServicesAndProviders(
-      props.serviceId,
-      props.locationId,
-      props.baseUrl ?? undefined
-    ).then((data) => {
-      services.value = data.services;
-      relations.value = data.relations;
-      offices.value = data.offices;
-
-      const foundService = services.value.find(
-        (service) => service.id == props.serviceId
-      );
-      const foundOffice = offices.value.find(
-        (office) => office.id == props.locationId
-      );
-
-      if (foundService && foundOffice) {
-        selectedService.value = foundService as ServiceImpl;
-        selectedService.value.count = 1;
-        selectedService.value.providers = getProviders(
-          selectedService.value.id,
-          null
-        );
-        preselectedLocationId.value = props.locationId;
-        selectedProvider.value = foundOffice as OfficeImpl;
-        selectedServiceMap.value = new Map<string, number>();
-        selectedServiceMap.value.set(foundService.id, 1);
-        currentView.value = 1;
-      }
     });
   }
 });
