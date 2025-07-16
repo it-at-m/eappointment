59f3af29e31a6d4bb6f73a5c32683bd4184b49d0
fabian.weiss <fabian.weiss@muenchen.de>
2025-07-16 16:04:56 +0200

    feat(ZMSKVR-218): avoid generic fallback token in zmsmessaging

--- zmsmessaging/src/Zmsmessaging/Application.php
+++ zmsmessaging/src/Zmsmessaging/Application.php
@@ -8,10 +8,11 @@
 
 namespace BO\Zmsmessaging;
 
-define(
-    'ZMS_CONFIG_SECURE_TOKEN',
-    getenv('ZMS_CONFIG_SECURE_TOKEN') ? getenv('ZMS_CONFIG_SECURE_TOKEN') : 'secure-token'
-);
+if (!getenv('ZMS_CONFIG_SECURE_TOKEN')) {
+    throw new \RuntimeException('ZMS_CONFIG_SECURE_TOKEN environment variable must be set');
+}
+
+define('ZMS_CONFIG_SECURE_TOKEN', getenv('ZMS_CONFIG_SECURE_TOKEN'));
 
 /**
  * @SuppressWarnings("TooManyFields")
