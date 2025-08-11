6280aa810c8e3e31c1813be1ac857db57306c2cf
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-08-11 10:30:52 +0200

    feat(zmskvr-494): migration to create the request_variant table and publish it

---
+++ zmsdb/migrations/91754036943-create-request-variant -table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `request_variant`;
+
+CREATE TABLE `request_variant`
+(
+    `id`   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+    DEFAULT CHARSET = utf8
+    COLLATE = utf8_unicode_ci;
+
+INSERT INTO `request_variant` (`name`) VALUES ('Präsenz');
+INSERT INTO `request_variant` (`name`) VALUES ('Telefon');
+INSERT INTO `request_variant` (`name`) VALUES ('Videoberatung');
+INSERT INTO `request_variant` (`name`) VALUES ('Großkunde');
+INSERT INTO `request_variant` (`name`) VALUES ('Kleinkunde');
+INSERT INTO `request_variant` (`name`) VALUES ('Familie');
+INSERT INTO `request_variant` (`name`) VALUES ('Einzelperson');
