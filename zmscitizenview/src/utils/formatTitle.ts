12457f4e175b3e6ed6c450748369b58f0cf34cb1
lehju <julia.grasmeier@muenchen.de>
2025-08-13 15:29:36 +0200

    feat(MUXDBS-75): fix format

--- zmscitizenview/src/utils/formatTitle.ts
+++ zmscitizenview/src/utils/formatTitle.ts
@@ -1,11 +1,11 @@
-import {AppointmentDTO} from "@/api/models/AppointmentDTO";
+import { AppointmentDTO } from "@/api/models/AppointmentDTO";
 
 /**
  * Creates a formatted multiline string.
  * @param appointment
  * @returns Formatted multiline string
  */
-export function formatTitle(appointment: AppointmentDTO): string  {
+export function formatTitle(appointment: AppointmentDTO): string {
   const serviceTitle =
     appointment.serviceCount + "x " + appointment.serviceName;
   const subserviceTitle = appointment.subRequestCounts
