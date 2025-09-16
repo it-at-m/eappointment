0208b3a0e5d75ac164039ff08dba7c7a85575e8f
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-09-16 13:39:37 +0200

    (ZMSKVR-495): adjusting the phpunit.xml and revert the usage of ENV while logging

--- zmscitizenapi/src/Zmscitizenapi/Services/Core/ZmsApiClientService.php
+++ zmscitizenapi/src/Zmscitizenapi/Services/Core/ZmsApiClientService.php
@@ -376,13 +376,11 @@ class ZmsApiClientService
 
         if (\App::$cache) {
             \App::$cache->set($cacheKey, $entity, \App::$SOURCE_CACHE_TTL);
-            if (getenv('APP_ENV') !== 'testing') {
-                LoggerService::logInfo('Cache set', [
-                    'key' => $cacheKey,
-                    'ttl' => \App::$SOURCE_CACHE_TTL,
-                    'entity_type' => get_class($entity)
-                ]);
-            }
+            LoggerService::logInfo('Cache set', [
+                'key' => $cacheKey,
+                'ttl' => \App::$SOURCE_CACHE_TTL,
+                'entity_type' => get_class($entity)
+            ]);
         }
 
         return $entity;
