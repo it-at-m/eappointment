ac5a0c459365ceb886b92459a8eaa81cd7776c42
Igor Manjencic <manjak.igor@gmail.com>
2025-09-22 16:53:48 +0200

    feat(ZMSKVR-311): fix migration

--- zmsdb/migrations/91758552677-update-mails-use-displayNumber-field.sql
+++ zmsdb/migrations/91758552677-update-mails-use-displayNumber-field.sql
@@ -1,6 +1,6 @@
 UPDATE mailtemplate
 SET value = REPLACE(
-        REPLACE(value, ''queue.number'', ''displayNumber''),
-        ''process.id'', ''process.displayNumber''
+        REPLACE(value, 'queue.number', 'displayNumber'),
+        'process.id', 'process.displayNumber'
     )
-WHERE value LIKE ''%queue.number%'' OR value LIKE ''%process.id%'';
+WHERE value LIKE '%queue.number%' OR value LIKE '%process.id%';
