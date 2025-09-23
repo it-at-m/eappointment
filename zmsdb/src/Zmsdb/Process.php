68d682ab352d407311f309230ef9dace994695f5
Igor Manjencic <manjak.igor@gmail.com>
2025-09-23 12:27:52 +0200

    feat(ZMSKVR-311): test

--- zmsdb/src/Zmsdb/Process.php
+++ zmsdb/src/Zmsdb/Process.php
@@ -598,7 +598,7 @@ class Process extends Base implements Interfaces\ResolveReferences
 
     public function shouldUpdateDisplayNumber(Entity $process, $status): bool
     {
-        if ($status !== Entity::STATUS_CONFIRMED || $status !== Entity::STATUS_PRECONFIRMED) {
+        if ($status !== Entity::STATUS_CONFIRMED && $status !== Entity::STATUS_PRECONFIRMED) {
             return false;
         }
 
