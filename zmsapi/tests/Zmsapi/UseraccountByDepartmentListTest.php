0d553e4274b972a81539f2cd52375c21ed07751e
Igor Manjencic <manjak.igor@gmail.com>
2025-09-26 17:41:44 +0200

    fix(ZMSKVR-748): fix tests

--- zmsapi/tests/Zmsapi/UseraccountByDepartmentListTest.php
+++ zmsapi/tests/Zmsapi/UseraccountByDepartmentListTest.php
@@ -10,7 +10,7 @@ class UseraccountByDepartmentListTest extends Base
     {
         $this->setWorkstation()->getUseraccount()->setRights('useraccount');
         $this->setDepartment(74);
-        $response = $this->render(['id' => 74], [], []);
+        $response = $this->render(['ids' => 74], [], []);
         $this->assertStringContainsString('testuser', (string)$response->getBody());
         $this->assertTrue(200 == $response->getStatusCode());
     }
@@ -21,7 +21,7 @@ class UseraccountByDepartmentListTest extends Base
         $this->setDepartment(74);
         $this->expectException('BO\Zmsentities\Exception\UserAccountMissingDepartment');
         $this->expectExceptionCode(403);
-        $this->render(['id' => 72], [], []);
+        $this->render(['ids' => 72], [], []);
     }
 
     public function testDepartmentNotFound()
@@ -29,6 +29,6 @@ class UseraccountByDepartmentListTest extends Base
         $this->setWorkstation()->getUseraccount()->setRights('superuser', 'useraccount');
         $this->expectException('BO\Zmsentities\Exception\UserAccountMissingDepartment');
         $this->expectExceptionCode(403);
-        $this->render(['id' => 99999], [], []);
+        $this->render(['ids' => 99999], [], []);
     }
 }
