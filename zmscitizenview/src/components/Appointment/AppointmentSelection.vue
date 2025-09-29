40293f8c73dfd5cbf433c1246aa11cd249b8bdc2
ThomasAFink <thomas.fink@muenchen.de>
2025-09-29 12:00:09 +0200

    fix(ZMSKVR): Ensure getAppointmentsOfDay returns a Promise to avoid undefined .then TypeError in tests

--- zmscitizenview/src/components/Appointment/AppointmentSelection.vue
+++ zmscitizenview/src/components/Appointment/AppointmentSelection.vue
@@ -779,7 +779,7 @@ const refetchAvailableDaysForSelection = async (): Promise<void> => {
   }
 };
 
-const getAppointmentsOfDay = (date: string) => {
+const getAppointmentsOfDay = async (date: string): Promise<void> => {
   isLoadingAppointments.value = true;
   appointmentTimestamps.value = [];
   appointmentTimestampsByOffice.value = [];
@@ -791,12 +791,12 @@ const getAppointmentsOfDay = (date: string) => {
   if (selectedProviderIds.length === 0) {
     // No providers selected, clear appointments
     isLoadingAppointments.value = false;
-    return;
+    return Promise.resolve();
   }
 
   const providerIds = selectedProviderIds.map(Number);
 
-  fetchAvailableTimeSlots(
+  return fetchAvailableTimeSlots(
     date,
     providerIds.map(Number),
     Array.from(props.selectedServiceMap.keys()),
