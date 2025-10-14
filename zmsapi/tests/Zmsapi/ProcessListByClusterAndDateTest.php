0e11f3d7eb88f5e55f567549d4af0453d3d6f6d2
Igor Manjencic <manjak.igor@gmail.com>
2025-10-14 13:47:11 +0200

    feat(ZMSKVR): fix some tests

--- zmsapi/tests/Zmsapi/ProcessListByClusterAndDateTest.php
+++ zmsapi/tests/Zmsapi/ProcessListByClusterAndDateTest.php
@@ -10,7 +10,7 @@ class ProcessListByClusterAndDateTest extends Base
 {
     protected $classname = "ProcessListByClusterAndDate";
 
-    protected function setUp(): void
+    public function setUp(): void
     {
         parent::setUp();
 
