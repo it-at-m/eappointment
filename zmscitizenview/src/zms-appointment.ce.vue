bb63bd42494d4e4ffc874b712c8392a19bce3e5e
Tobias Holler <tobias.holler@muenchen.de>
2025-10-22 16:53:41 +0200

    fix(ZMSKVR-990): refactored external url parsing

--- zmscitizenview/src/zms-appointment.ce.vue
+++ zmscitizenview/src/zms-appointment.ce.vue
@@ -25,95 +25,65 @@
 <script lang="ts" setup>
 import customIconsSprit from "@muenchen/muc-patternlab-vue/assets/icons/custom-icons.svg?raw";
 import mucIconsSprite from "@muenchen/muc-patternlab-vue/assets/icons/muc-icons.svg?raw";
-import { computed, onMounted } from "vue";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 import AppointmentView from "@/components/Appointment/AppointmentView.vue";
 import { sanitizeHtml } from "@/utils/sanitizeHtml";
 import { useGlobalState } from "./utils/useGlobalState";
 
-// URL Parsing
-const hash = window.location.hash || "";
-const path = window.location.pathname || "";
-
-const confirmHashMatch =
-  hash.match(/#\/appointment\/confirm\/(.+)/) ||
-  path.match(/\/appointment\/confirm\/(.+)/);
-const appointmentHashMatch =
-  hash.match(/#\/appointment\/([^/]+)$/) ||
-  path.match(/\/appointment\/([^/]+)$/);
-const hashMatch = hash.match(/services\/([^/]+)(?:\/locations\/([^/]+))?/);
-const pathMatch = path.match(/services\/([^/]+)(?:\/locations\/([^/]+))?/);
-
-const fallbackConfirmAppointmentHash = confirmHashMatch?.[1];
-const fallbackAppointmentHash = appointmentHashMatch?.[1];
-const fallbackServiceId = hashMatch?.[1] || pathMatch?.[1];
-const fallbackLocationId = hashMatch?.[2] || pathMatch?.[2];
-
 // Props
 const props = withDefaults(
   defineProps<{
     baseUrl?: string;
-    serviceId?: string;
-    locationId?: string;
-    exclusiveLocation?: string;
-    appointmentHash?: string;
-    confirmAppointmentHash?: string;
     appointmentDetailUrl?: string;
     showLoginOption?: string;
   }>(),
   {
     baseUrl: undefined,
-    serviceId: undefined,
-    locationId: undefined,
-    exclusiveLocation: undefined,
-    appointmentHash: undefined,
-    confirmAppointmentHash: undefined,
     appointmentDetailUrl: "appointment-detail.html",
     showLoginOption: "false",
   }
 );
 
-const serviceId = computed(() => props.serviceId ?? fallbackServiceId);
-const locationId = computed(() => props.locationId ?? fallbackLocationId);
-const appointmentHash = computed(
-  () => props.appointmentHash ?? fallbackAppointmentHash
-);
-const confirmAppointmentHash = computed(
-  () => props.confirmAppointmentHash ?? fallbackConfirmAppointmentHash
-);
+// START Routing
+const urlElements = window.location.hash.split("/");
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+
+const serviceId = ref<string | undefined>(undefined);
+if (urlElements.length >= 3 && urlElements[1] === "services") {
+  serviceId.value = urlElements[2];
+}
+
+const locationId = ref<string | undefined>(undefined);
+if (urlElements.length >= 5 && urlElements[3] === "locations") {
+  locationId.value = urlElements[4];
+}
+
+const confirmAppointmentHash = ref<string | undefined>(undefined);
+if (
+  urlElements.length === 4 &&
+  urlElements[1] === "appointment" &&
+  urlElements[2] === "confirm"
+) {
+  confirmAppointmentHash.value = urlElements[3];
+}
+
+const appointmentHash = ref<string | undefined>(undefined);
+if (urlElements.length === 3 && urlElements[1] === "appointment") {
+  appointmentHash.value = urlElements[2];
+}
+
+const exclusiveLocation = ref<string | undefined>(undefined);
+if (params.get("exclusiveLocation")) {
+  exclusiveLocation.value = "1";
+}
+// END Routing
 
 // i18n & Global State
 const { t } = useI18n();
 const globalState = useGlobalState(props);
-
-// Web Component Attributes
-onMounted(() => {
-  const element = document.querySelector("zms-appointment-wrapped");
-  if (!element) return;
-
-  const urlElements = window.location.hash.split("/");
-  const params = new URLSearchParams(window.location.search);
-
-  if (urlElements[1] === "services") {
-    element.setAttribute("service-id", urlElements[2] || "");
-    if (urlElements[3] === "locations") {
-      element.setAttribute("location-id", urlElements[4] || "");
-    }
-  }
-
-  if (urlElements[1] === "appointment") {
-    if (urlElements[2] === "confirm") {
-      element.setAttribute("confirm-appointment-hash", urlElements[3] || "");
-    } else {
-      element.setAttribute("appointment-hash", urlElements[2] || "");
-    }
-  }
-
-  if (params.get("exclusiveLocation")) {
-    element.setAttribute("exclusive-location", "1");
-  }
-});
 </script>
 
 <style>
