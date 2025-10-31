c4fbc7ec85b3bbb1725bf2ff259493c526327d27
msfb3 <fabian.breitling@icloud.com>
2025-10-31 09:10:53 +0100

    featZMSKVR-862): removed CSV download and tests

--- zmsstatistic/tests/Zmsstatistic/ReportWaitingOrganisationTest.php
+++ zmsstatistic/tests/Zmsstatistic/ReportWaitingOrganisationTest.php
@@ -158,52 +158,6 @@ class ReportWaitingOrganisationTest extends Base
         ob_end_clean();
     }
 
-    public function testWithDownloadCSV()
-    {
-        $this->setApiCalls(
-            [
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/workstation/',
-                    'parameters' => ['resolveReferences' => 2],
-                    'response' => $this->readFixture("GET_Workstation_Resolved2.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/scope/141/department/',
-                    'response' => $this->readFixture("GET_department_74.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/department/74/organisation/',
-                    'response' => $this->readFixture("GET_organisation_71_resolved3.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/organisation/71/owner/',
-                    'response' => $this->readFixture("GET_owner_23.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/warehouse/waitingorganisation/71/',
-                    'response' => $this->readFixture("GET_waitingorganisation_71.json")
-                ],
-                [
-                    'function' => 'readGetResult',
-                    'url' => '/warehouse/waitingorganisation/71/2016-03/',
-                    'response' => $this->readFixture("GET_waitingorganisation_71_032016.json")
-                ]
-            ]
-        );
-
-        $response = $this->render(['period' => '2016-03'], ['type' => 'csv'], []);
-
-        $this->assertStringContainsString('csv', $response->getHeaderLine('Content-Disposition'));
-        $this->assertStringContainsString(
-            '"Stunden-Max (Spaltenmaximum) der Wartezeit in Min.";"532:00";"414:00";"280:00";"160:00";"256:00";"437:00";"455:00";"202:00";"532:00";"359:00";"384:00";"417:00";"148:00";"375:00";"343:00";',
-            (string) $response->getBody()
-        );
-    }
 
     public function testWithoutAccess()
     {
