8e6c751dc85fb7aa67455621abf412de41a68b71
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2025-11-06 13:47:50 +0100

    (ZMSKVR-784): Variante einpfelegen bei ProviderSelection.vue

--- zmscitizenview/src/components/Appointment/AppointmentSelection/ProviderSelection.vue
+++ zmscitizenview/src/components/Appointment/AppointmentSelection/ProviderSelection.vue
@@ -62,9 +62,13 @@
                     aria-hidden="true"
                     class="icon icon--before"
                   >
-                    <use xlink:href="#icon-map-pin"></use>
+                    <use :xlink:href="`#${detailIcon}`"></use>
                   </svg>
-                  <span>
+                  <span v-if="variantId === 2 || variantId === 3">
+                    {{ t(`appointmentTypes.${variantId}`) }}
+                  </span>
+
+                  <span v-else>
                     {{ selectedProvider.address.street }}
                     {{ selectedProvider.address.house_number }}
                   </span>
@@ -82,7 +86,9 @@
 import type { OfficeImpl } from "@/types/OfficeImpl";
 
 import { MucCheckbox, MucCheckboxGroup } from "@muenchen/muc-patternlab-vue";
-import { computed } from "vue";
+import { computed, inject } from "vue";
+
+import { SelectedServiceProvider } from "@/types/ProvideInjectTypes";
 
 const props = defineProps<{
   t: (key: string) => string;
@@ -106,4 +112,19 @@ const onToggle = (id: string | number, val: boolean) => {
   next[idStr] = val;
   emit("update:selectedProviders", next);
 };
+
+const { selectedService } = inject<SelectedServiceProvider>(
+  "selectedServiceProvider"
+)!;
+
+const variantId = computed<number | null>(() => {
+  const id = (selectedService.value as any)?.variantId;
+  return Number.isFinite(id) ? (id as number) : null;
+});
+
+const detailIcon = computed<string>(() => {
+  if (variantId.value === 2) return "icon-telephone";
+  if (variantId.value === 3) return "icon-video-camera";
+  return "icon-map-pin";
+});
 </script>
