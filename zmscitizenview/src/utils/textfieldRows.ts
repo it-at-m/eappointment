6c60cc2514b5cf48db517afe6d78478c02a16583
Christian Vierthaler <christian.vierthaler@muenchen.de>
2025-12-03 16:14:53 +0100

    fix(ZMSKVR-719): add SSR safety check

--- zmscitizenview/src/utils/textfieldRows.ts
+++ zmscitizenview/src/utils/textfieldRows.ts
@@ -3,7 +3,9 @@ import { computed, ref } from "vue";
 const windowWidth = ref(typeof window !== "undefined" ? window.innerWidth : 0);
 
 export const updateWindowWidth = () => {
-  windowWidth.value = window.innerWidth;
+  if(typeof window !== "undefined") {
+    windowWidth.value = window.innerWidth;
+  }
 };
 
 export const textfieldRows = computed(() => {
