328a419f3aaaedcf9e9fb48da998d6aac2069997
Fabian Weiß <98973281+Fabinatix97@users.noreply.github.com>
2026-01-28 15:36:54 +0100

    hotfix(ZMSKVR-1225): fix typ

--- zmsdb/migrations/91769610028-set-sender-to-noreply.sql
+++ zmsdb/migrations/91769610028-set-sender-to-noreply.sql
@@ -2,5 +2,5 @@ UPDATE `email`
 SET `absenderadresse` = 'noreply-terminvereinbarung@muenchen.de';
 
 UPDATE `preferences`
-SET `value` = 'noreply-terminvereinbarung@muenchen.de';
+SET `value` = 'noreply-terminvereinbarung@muenchen.de'
 WHERE `name` = 'emailFrom';
