127fcdf880cfcc1a99e6972228dad1f727578706
thomasafink <thomas.fink@muenchen.de>
2026-02-05 12:32:57 +0100

    chore(ZMSKVR-1124): Remove zmsapiautomation module
    The zmsapiautomation module has been replaced by zmsautomation which uses
    Cucumber and ATAF. The old module is no longer needed.

--- zmsapiautomation/flyway/V2__update_kundenname_to_muenchen.sql
+++
@@ -1,4 +0,0 @@
--- Flyway migration: Update Kundenname to München
--- Change the customer name from 'Teststadt' to 'München' for Munich test data
-
-UPDATE `kunde` SET `Kundenname` = 'München', `Anschrift` = 'München' WHERE `KundenID` = 1;
