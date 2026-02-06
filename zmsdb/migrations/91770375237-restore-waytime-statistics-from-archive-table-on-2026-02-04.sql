5f68f7955dd234d9028f6a445ed670cf35327a9d
thomasafink <thomas.fink@muenchen.de>
2026-02-06 12:21:01 +0100

    fix(ZMSKVR): waytime division

--- zmsdb/migrations/91770375237-restore-waytime-statistics-from-archive-table-on-2026-02-04.sql
+++ zmsdb/migrations/91770375237-restore-waytime-statistics-from-archive-table-on-2026-02-04.sql
@@ -28,7 +28,7 @@ SELECT
   HOUR(STR_TO_DATE(`Timestamp`, '%H:%i:%s')) AS bucket_hour,
   CASE WHEN mitTermin = 1 THEN 'termin' ELSE 'spontan' END AS type,
   CASE WHEN wartezeit = 0 THEN NULL ELSE ROUND(wartezeit, 2) END AS waited_minutes,
-  ROUND(COALESCE(wegezeit, 0) / 60.0, 2) AS way_minutes
+  COALESCE(wegezeit, 0) AS way_minutes
 FROM buergerarchiv
 WHERE Datum = @start_date
   AND (nicht_erschienen IS NULL OR nicht_erschienen = 0)
