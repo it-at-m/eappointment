5760fb73a48d9f04b54d3055153bfda2258b90f5
thomasafink <thomas.fink@muenchen.de>
2026-02-24 18:45:36 +0100

    fix(ZMSKVR-1124): Fix test data

--- zmsautomation/src/main/resources/db/migration/V9__enable_keycloak_login.sql
+++
@@ -1,5 +0,0 @@
--- Flyway migration: Enable Keycloak login button
--- Set oidc__provider to 'keycloak' to enable the Keycloak SSO login button
--- in zmsadmin and zmsstatistic applications
-
-UPDATE `config` SET `value` = 'keycloak' WHERE `name` = 'oidc__provider';
