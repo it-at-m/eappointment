b23a3a5e2e532e8f4c48e758b6edd919929971b5
thomasafink <thomas.fink@muenchen.de>
2026-02-27 21:39:04 +0100

    fix(ZMSKVR-1124): more selector fixes

--- zmsautomation/src/test/java/zms/ataf/ui/pages/admin/workview/counterprocessingstation/CounterProcessingStationPage.java
+++ zmsautomation/src/test/java/zms/ataf/ui/pages/admin/workview/counterprocessingstation/CounterProcessingStationPage.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
