d26f745c93fbbf93fef18eff683750d14788199d
thomasafink <thomas.fink@muenchen.de>
2026-03-02 21:00:56 +0100

    fix(ZMSKVR-1124): try fixing two more tests with test data changes

---
+++ zmsautomation/src/main/resources/db/migration/V17__ZMS-3171_set_email_activated_for_scope_1_to_false.sql
@@ -0,0 +1,5 @@
+-- Flyway migration: Set email confirmation activated to false for scope 1 in ZMS-3171.feature Vorbelegung von "Mit E-Mail Bestätigung" ist konfigurierbar
+
+UPDATE `scope` SET `email_confirmation_activated` = 0 WHERE `StandortID` = 1;
+
+UPDATE `preferences` SET `value` = 0 WHERE `id` = 1 AND `entity` = 'scope' AND `groupName` = 'client' AND `name` = 'emailConfirmationActivated';
--- zmsautomation/src/main/resources/db/migration/V17__set_email_activated_for_scope_1_to_false.sql
+++
@@ -1,3 +0,0 @@
--- Flyway migration: Set email confirmation activated to false for scope 1 in ZMS-3171.feature Vorbelegung von "Mit E-Mail Bestätigung" ist konfigurierbar
-
-UPDATE `scope` SET `email_confirmation_activated` = 0 WHERE `StandortID` = 1;
