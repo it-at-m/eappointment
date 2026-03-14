b7ab988c1f9bc788c6af7f0c528bafb613a39e43
thomasafink <thomas@thomasafink.com>
2026-03-15 00:54:15 +0100

    zmsapi: debug log ProcessGet (entry, testProcessData, readEntity)
    Trace 404 on GET /process/{id}/{authKey}/ via error_log

--- zmsapi/src/Zmsapi/ProcessGet.php
+++ zmsapi/src/Zmsapi/ProcessGet.php
@@ -22,11 +22,18 @@ class ProcessGet extends BaseController
         \Psr\Http\Message\ResponseInterface $response,
         array $args
     ) {
+        $processId = $args['id'] ?? null;
+        $authKey = $args['authKey'] ?? '';
+        error_log('[ProcessGet] entry processId=' . $processId . ' authKeyLength=' . strlen((string) $authKey));
+
         $resolveReferences = Validator::param('resolveReferences')->isNumber()->setDefault(2)->getValue();
-        $this->testProcessData($args['id'], $args['authKey']);
+        $this->testProcessData($processId, $authKey);
+        error_log('[ProcessGet] testProcessData passed');
 
         $message = Response\Message::create($request);
-        $message->data = (new Process())->readEntity($args['id'], $args['authKey'], $resolveReferences);
+        error_log('[ProcessGet] about to readEntity');
+        $message->data = (new Process())->readEntity($processId, $authKey, $resolveReferences);
+        error_log('[ProcessGet] readEntity done dataPresent=' . ($message->data !== null ? '1' : '0'));
 
         $response = Render::withLastModified($response, time(), '0');
         $response = Render::withJson($response, $message->setUpdatedMetaData(), $message->getStatuscode());
@@ -37,8 +44,10 @@ class ProcessGet extends BaseController
     {
         $authCheck = (new Process())->readAuthKeyByProcessId($processId);
         if (! $authCheck) {
+            error_log('[ProcessGet] testProcessData: readAuthKeyByProcessId returned false/empty');
             throw new Exception\Process\ProcessNotFound();
         } elseif ($authCheck['authKey'] != $authKey && $authCheck['authName'] != $authKey) {
+            error_log('[ProcessGet] testProcessData: authKey/authName mismatch');
             throw new Exception\Process\AuthKeyMatchFailed();
         }
     }
