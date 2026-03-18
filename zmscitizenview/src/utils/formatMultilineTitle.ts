3c2e3d433f2028e01311af0f8999c2ec78156d0e
msfb3 <fabian.breitling@icloud.com>
2026-03-18 09:01:33 +0100

    feat(ZMSKVR-1088): updated formatTitle function to correct display order for services

--- zmscitizenview/src/utils/formatMultilineTitle.ts
+++ zmscitizenview/src/utils/formatMultilineTitle.ts
@@ -9,7 +9,8 @@ export function formatMultilineTitle(appointment: AppointmentDTO): string {
   const serviceTitle =
     appointment.serviceCount + "x " + appointment.serviceName;
   const subserviceTitle = appointment.subRequestCounts
+    .sort((a, b) => a.name.localeCompare(b.name, "de"))
     .map((subCount) => subCount.count + "x " + subCount.name)
     .join("\n");
-  return subserviceTitle ? serviceTitle + "\n" + subserviceTitle : serviceTitle;
+  return [serviceTitle, subserviceTitle].filter(Boolean) .join("\n");
 }
