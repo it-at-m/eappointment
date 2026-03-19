9569e392e3dbf952048016e535b6c6f032d902f6
thomasafink <thomas@thomasafink.com>
2026-03-19 19:21:48 +0100

    clean(ZMSKVR-1124): remove unused zmsapi placeholder steps

--- zmsautomation/src/test/java/zms/ataf/rest/steps/ZmsApiSteps.java
+++ zmsautomation/src/test/java/zms/ataf/rest/steps/ZmsApiSteps.java
@@ -46,16 +46,6 @@ public class ZmsApiSteps {
         CommonApiSteps.setResponse(response);
     }
     
-    @When("I request available appointments for scope {int}")
-    public void iRequestAvailableAppointments(int scopeId) {
-        response = given()
-            .baseUri(baseUri != null ? baseUri : TestConfig.getBaseUri())
-            .pathParam("scopeId", scopeId)
-        .when()
-            .get("/scope/{scopeId}/availability/");
-        CommonApiSteps.setResponse(response);
-    }
-    
     
     @Then("the response should contain status information")
     public void theResponseShouldContainStatusInformation() {
@@ -69,116 +59,6 @@ public class ZmsApiSteps {
         StatusResponse statusData = apiResponse.getData();
         org.assertj.core.api.Assertions.assertThat(statusData).isNotNull();
         org.assertj.core.api.Assertions.assertThat(statusData.getVersion()).isNotNull();
-    }
-    
-    @Then("the response should contain available slots")
-    public void theResponseShouldContainSlots() {
-        response.then()
-            .body("data", not(empty()));
-    }
-    
-    @When("I request scope information for scope {int}")
-    public void iRequestScopeInformation(int scopeId) {
-        response = given()
-            .baseUri(baseUri != null ? baseUri : TestConfig.getBaseUri())
-            .pathParam("scopeId", scopeId)
-        .when()
-            .get("/scope/{scopeId}/");
-        CommonApiSteps.setResponse(response);
-    }
-    
-    @Then("the response should contain scope details")
-    public void theResponseShouldContainScopeDetails() {
-        response.then()
-            .body("data", notNullValue());
-    }
-    
-    @When("I request an invalid endpoint {string}")
-    public void iRequestAnInvalidEndpoint(String endpoint) {
-        response = given()
-            .baseUri(baseUri != null ? baseUri : TestConfig.getBaseUri())
-        .when()
-            .get(endpoint);
-        CommonApiSteps.setResponse(response);
-    }
-    
-    @When("I send a {string} request to {string}")
-    public void iSendARequest(String method, String endpoint) {
-        baseUri = baseUri != null ? baseUri : TestConfig.getBaseUri();
-        switch (method.toUpperCase()) {
-            case "DELETE":
-                response = given()
-                    .baseUri(baseUri)
-                .when()
-                    .delete(endpoint);
-                break;
-            case "PUT":
-                response = given()
-                    .baseUri(baseUri)
-                .when()
-                    .put(endpoint);
-                break;
-            case "PATCH":
-                response = given()
-                    .baseUri(baseUri)
-                .when()
-                    .patch(endpoint);
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported HTTP method: " + method);
-        }
-        CommonApiSteps.setResponse(response);
-    }
-    
-    @When("I request availability without scope ID")
-    public void iRequestAvailabilityWithoutScopeId() {
-        response = given()
-            .baseUri(baseUri != null ? baseUri : TestConfig.getBaseUri())
-        .when()
-            .get("/scope//availability/");
-        CommonApiSteps.setResponse(response);
-    }
-    
-    @When("I submit an invalid request body to {string}")
-    public void iSubmitAnInvalidRequestBody(String endpoint) {
-        response = given()
-            .baseUri(baseUri != null ? baseUri : TestConfig.getBaseUri())
-            .contentType("application/json")
-            .body("{\"invalid\": \"data\"}")
-        .when()
-            .post(endpoint);
-        CommonApiSteps.setResponse(response);
-    }
-    
-    @Then("the response should contain an error message")
-    public void theResponseShouldContainAnErrorMessage() {
-        response.then()
-            .body(not(empty()));
-    }
-    
-    @Then("the response should contain validation errors")
-    public void theResponseShouldContainValidationErrors() {
-        response.then()
-            .body(not(empty()));
-    }
-    
-    @Then("the response should contain {string}")
-    public void theResponseShouldContain(String expectedContent) {
-        switch (expectedContent.toLowerCase()) {
-            case "available slots":
-                theResponseShouldContainSlots();
-                break;
-            case "error message":
-                theResponseShouldContainAnErrorMessage();
-                break;
-            case "empty array":
-                response.then()
-                    .body("data", empty());
-                break;
-            default:
-                response.then()
-                    .body(not(empty()));
-        }
     }
     
     private <T> T as(Response response, TypeReference<T> typeReference) {
