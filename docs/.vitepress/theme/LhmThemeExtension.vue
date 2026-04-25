c1079899063cd51e0d7186f74c54eba9abcb9c2b
ThomasAFink <thomas.fink@muenchen.de>
2026-04-25 23:42:28 +0200

    docs(GH-2269): align Munich logo slot with refarch theme

--- docs/.vitepress/theme/LhmThemeExtension.vue
+++ docs/.vitepress/theme/LhmThemeExtension.vue
@@ -1,18 +1,14 @@
 <template>
   <Layout>
     <template #nav-bar-content-after>
-      <a
-        class="logo"
-        href="https://www.muenchen.de"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Landeshauptstadt Muenchen"
-      >
-        <img
-          src="https://assets.muenchen.de/logos/lhm/logo-lhm-muenchen.svg"
-          alt="Landeshauptstadt Muenchen"
-        />
-      </a>
+      <div class="logo">
+        <a href="https://www.muenchen.de/" target="_blank">
+          <img
+            src="https://assets.muenchen.de/logos/lhm/logo-lhm-muenchen.svg"
+            alt="Logo Landeshauptstadt Muenchen"
+          />
+        </a>
+      </div>
     </template>
   </Layout>
 </template>
@@ -22,3 +18,25 @@ import DefaultTheme from "vitepress/theme";
 
 const { Layout } = DefaultTheme;
 </script>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+}
+@media (min-width: 768px) {
+  .extra + .logo:before {
+    margin-right: 16px;
+    margin-left: 16px;
+    width: 1px;
+    height: 24px;
+    background-color: var(--vp-c-divider);
+    content: "";
+  }
+}
+
+.logo img {
+  filter: var(--muc-logo-filter);
+  height: 28px;
+}
+</style>
