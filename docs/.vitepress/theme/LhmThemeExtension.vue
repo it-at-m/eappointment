39bfb8ef3ea1476142ba6fc46b328b4740e0b8b9
ThomasAFink <thomas.fink@muenchen.de>
2026-04-25 23:33:04 +0200

    docs(GH-2269): add Munich logo in VitePress top navbar

---
+++ docs/.vitepress/theme/LhmThemeExtension.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <template #nav-bar-content-after>
+      <a
+        class="logo"
+        href="https://www.muenchen.de"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Landeshauptstadt Muenchen"
+      >
+        <img
+          src="https://assets.muenchen.de/logos/lhm/logo-lhm-muenchen.svg"
+          alt="Landeshauptstadt Muenchen"
+        />
+      </a>
+    </template>
+  </Layout>
+</template>
+
+<script setup>
+import DefaultTheme from "vitepress/theme";
+
+const { Layout } = DefaultTheme;
+</script>
