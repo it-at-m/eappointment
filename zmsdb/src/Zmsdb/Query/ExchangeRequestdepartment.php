708131c1591a3daa4588c97c3e1bb88e356f6111
ThomasAFink <thomas.fink@muenchen.de>
2026-04-27 13:40:47 +0200

    clean(ZMSKVR-1314): use shared request status constants in SQL queries
    Import Exchange constants in request query classes and reuse them in SQL CASE labels to avoid duplicated literals while keeping SQL-safe quoted values and consistent formatting.

--- zmsdb/src/Zmsdb/Query/ExchangeRequestdepartment.php
+++ zmsdb/src/Zmsdb/Query/ExchangeRequestdepartment.php
@@ -2,6 +2,8 @@
 
 namespace BO\Zmsdb\Query;
 
+use BO\Zmsentities\Exchange;
+
 class ExchangeRequestdepartment extends Base
 {
     /**
@@ -18,8 +20,8 @@ class ExchangeRequestdepartment extends Base
         DATE_FORMAT(statistikJoin.`datum`, :groupby) as date,
         (
             CASE
-              WHEN statistikJoin.anliegenid = -1 THEN "uncategorized"
-              WHEN statistikJoin.anliegenid = 0 THEN "nonexistent"
+              WHEN statistikJoin.anliegenid = -1 THEN \'' . Exchange::REQUEST_STAT_NAME_UNCATEGORIZED . '\'
+              WHEN statistikJoin.anliegenid = 0 THEN \'' . Exchange::REQUEST_STAT_NAME_NOT_PROVIDED . '\'
               ELSE r.name
             END
         ) as name,
