21efb3b8431b43c23882342be339bb7093af4230
Fabinatix97 <fabian@fabi-online.de>
2026-04-28 16:59:47 +0200

    feat(ZMSKVR-1172): fix db useraccount unit test

--- zmsdb/tests/Zmsdb/UserAccountTest.php
+++ zmsdb/tests/Zmsdb/UserAccountTest.php
@@ -215,6 +215,7 @@ class UserAccountTest extends Base
         foreach (array_keys($scopeUser->rights) as $rightName) {
             $scopeUser->rights[$rightName] = false;
         }
+        $scopeUser->roles = [];
         $scopeUser->rights['scope'] = true;
         $createdScopeUser = $query->writeEntity($scopeUser);
         $this->assertIsArray($createdScopeUser->roles);
@@ -227,6 +228,7 @@ class UserAccountTest extends Base
         foreach (array_keys($basicUser->rights) as $rightName) {
             $basicUser->rights[$rightName] = false;
         }
+        $basicUser->roles = [];
         $basicUser->rights['basic'] = true;
         $createdBasicUser = $query->writeEntity($basicUser);
         $this->assertIsArray($createdBasicUser->roles);
