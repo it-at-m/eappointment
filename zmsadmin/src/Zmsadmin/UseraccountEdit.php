e576f276de9b44367be1b80ea0ae31c766f0782c
Fabinatix97 <fabian@fabi-online.de>
2026-05-05 08:20:20 +0200

    feat(ZMSKVR-1398): make role loading more robust

--- zmsadmin/src/Zmsadmin/UseraccountEdit.php
+++ zmsadmin/src/Zmsadmin/UseraccountEdit.php
@@ -50,9 +50,12 @@ class UseraccountEdit extends BaseController
         $roleList = new RoleList();
         if ($workstation->getUseraccount()->isSuperUser()) {
             try {
-                $loaded = \App::$http->readGetResult('/roles/', [])->getCollection();
-                if ($loaded !== null) {
-                    $roleList = $loaded;
+                $roleResult = \App::$http->readGetResult('/roles/', []);
+                if ($roleResult) {
+                    $loaded = $roleResult->getCollection();
+                    if ($loaded !== null) {
+                        $roleList = $loaded;
+                    }
                 }
             } catch (\BO\Zmsclient\Exception $e) {
                 // keep empty list; form still usable without role assignment
