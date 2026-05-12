91456ea216d79409c16daf60a62e89ee2324bbae
Fabinatix97 <fabian@fabi-online.de>
2026-05-12 09:36:56 +0200

    feat(ZMSKVR-1398): remove js from twig and use pop-up for delete confirmation

---
+++ zmsadmin/js/block/roles-list.js
@@ -0,0 +1,41 @@
+import $ from "jquery";
+import BaseView from "../lib/baseview";
+import { buildConfirmDialogHtml } from "../lib/confirmDialog";
+
+class View extends BaseView {
+
+    constructor(element, options) {
+        super(element, options);
+        this.$.on('click', '.role-delete', this.onDeleteClick.bind(this));
+    }
+
+    onDeleteClick(ev) {
+        ev.preventDefault();
+
+        const trigger = ev.currentTarget;
+        const dialogHtml = buildConfirmDialogHtml(
+            'Rolle löschen',
+            'Soll diese Rolle wirklich gelöscht werden?',
+            'Löschen'
+        );
+
+        BaseView.loadDialogStatic(
+            dialogHtml,
+            () => {
+                $.ajax(trigger.href, {
+                    method: 'DELETE'
+                }).done((response, textStatus, xhr) => {
+                    window.location.assign(xhr.responseURL || window.location.href);
+                }).fail(() => {
+                    window.location.assign(window.location.href);
+                });
+            },
+            () => {},
+            { $main: $('body') },
+            false,
+            trigger
+        );
+    }
+}
+
+export default View;
