f765e731bf279b1ab568441c0081cec7a2dbd40b
Fabinatix97 <fabian@fabi-online.de>
2026-05-13 08:05:01 +0200

    feat(ZMSKVR-1398): remove RoleInputHelper and use existing form validation functionality instead

--- zmsadmin/src/Zmsadmin/RoleEdit.php
+++ zmsadmin/src/Zmsadmin/RoleEdit.php
@@ -2,7 +2,6 @@
 
 namespace BO\Zmsadmin;
 
-use BO\Zmsadmin\Helper\RoleInputHelper;
 use BO\Mellon\Validator;
 use BO\Zmsentities\Exception\UserAccountMissingRights;
 use BO\Zmsentities\Role as RoleEntity;
