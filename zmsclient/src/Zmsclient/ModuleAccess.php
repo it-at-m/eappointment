065085fbb4873636cafed6037374661ac3fafb14
Fabinatix97 <fabian@fabi-online.de>
2026-05-26 08:50:07 +0200

    feat(ZMSKVR-1172): clean up

--- zmsclient/src/Zmsclient/ModuleAccess.php
+++ zmsclient/src/Zmsclient/ModuleAccess.php
@@ -25,33 +25,26 @@ class ModuleAccess
         $rejected = ($application === self::MODULE_STATISTIC && !$useraccount->hasPermissions(['statistic']))
             || ($application === self::MODULE_ADMIN && $useraccount->hasExclusivePermission('statistic'));
 
-        if (!$rejected) {
+        if ($rejected) {
+            self::endSession($workstation);
+            $template = $application === self::MODULE_ADMIN
+                ? 'exception/bo/slim/exception/wrongmodulestatistic.twig'
+                : 'exception/bo/slim/exception/wrongmoduleadmin.twig';
+            return \BO\Slim\Render::withHtml($response, $template, [], 403);
+        } else {
             return null;
         }
-
-        self::endSession($workstation);
-
-        $template = $application === self::MODULE_ADMIN
-            ? 'exception/bo/slim/exception/wrongmodulestatistic.twig'
-            : 'exception/bo/slim/exception/wrongmoduleadmin.twig';
-
-        return \BO\Slim\Render::withHtml($response, $template, [], 403);
     }
 
     private static function endSession(Workstation $workstation): void
     {
-        if (!Auth::getKey() && !empty($workstation->authkey)) {
-            Auth::setKey($workstation->authkey, time() + \App::SESSION_DURATION);
-        }
-        if (!Auth::getKey()) {
-            return;
-        }
         try {
-            if ($workstation->hasId()) {
+            if (Auth::getKey()) {
                 \App::$http->readDeleteResult('/workstation/login/' . $workstation->getUseraccount()->id . '/');
             }
         } catch (\Exception $exception) {
         }
+
         Auth::removeKey();
     }
 }
