9017c74dfa6afe634e6a018b1a062a62b0f746bb
fabian.weiss <fabian.weiss@muenchen.de>
2026-05-28 11:25:43 +0200

    feat(ZMSKVR-1172): reset role agent_basic

---
+++ zmsdb/migrations/91779959485-alter-role-agent-basic.sql
@@ -0,0 +1,8 @@
+DELETE FROM role_permission WHERE role_id = 1;
+
+INSERT INTO `role_permission` SET `role_id` = 1, `permission_id` = 1;
+INSERT INTO `role_permission` SET `role_id` = 1, `permission_id` = 8;
+INSERT INTO `role_permission` SET `role_id` = 1, `permission_id` = 11;
+INSERT INTO `role_permission` SET `role_id` = 1, `permission_id` = 12;
+INSERT INTO `role_permission` SET `role_id` = 1, `permission_id` = 16;
+INSERT INTO `role_permission` SET `role_id` = 1, `permission_id` = 20;
