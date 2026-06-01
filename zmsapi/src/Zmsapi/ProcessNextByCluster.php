ca7e11cfc1eb2b7ec915a92e754af38edb538908
thomasafink <thomas.fink@muenchen.de>
2026-06-01 07:51:45 +0200

    fix(ZMS): load user departments for cluster queue next access check
    ProcessNextByCluster used resolveReferences=1, so assigned departments were
    not loaded and EntityAccess on the cluster failed with missing rights when
    calling the next customer with all cluster locations enabled.

--- zmsapi/src/Zmsapi/ProcessNextByCluster.php
+++ zmsapi/src/Zmsapi/ProcessNextByCluster.php
@@ -23,7 +23,7 @@ class ProcessNextByCluster extends BaseController
         \Psr\Http\Message\ResponseInterface $response,
         array $args
     ) {
-        $workstation = (new Helper\User($request, 1))->checkPermissions('appointment');
+        $workstation = (new Helper\User($request, 2))->checkPermissions('appointment');
         $query = new Query();
         $selectedDate = Validator::param('date')->isString()->getValue();
         $exclude = Validator::param('exclude')->isString()->getValue();
@@ -34,7 +34,7 @@ class ProcessNextByCluster extends BaseController
             throw new Exception\Cluster\ClusterNotFound();
         }
 
-        (new Helper\User($request, 1))->checkPermissions(
+        (new Helper\User($request, 2))->checkPermissions(
             new \BO\Zmsentities\Useraccount\EntityAccess($cluster)
         );
 
