957470b79e99946d2ce90ec1f261c8a43961ef74
thomasafink <thomas.fink@muenchen.de>
2026-06-05 00:39:54 +0200

    clean(ZMS): fix unused $process vars in WorkstationProcessRedirect
    Read workstation process only in the POST branch and drop the unused
    redirect POST result before returning.

--- zmsadmin/src/Zmsadmin/WorkstationProcessRedirect.php
+++ zmsadmin/src/Zmsadmin/WorkstationProcessRedirect.php
@@ -29,7 +29,6 @@ class WorkstationProcessRedirect extends BaseController
                 ['resolveReferences' => 2]
             )->getEntity();
         $input = $request->getParsedBody();
-        $process = $workstation->process;
 
         if ($request->getMethod() === 'POST') {
             $validator = $request->getAttribute('validator');
@@ -53,12 +52,13 @@ class WorkstationProcessRedirect extends BaseController
                     ['resolveReferences' => 2]
                 )->getEntity();
 
+            $process = $workstation->process;
             $newProcess = clone $process;
             $newProcess->scope = $scope;
             $newProcess->appointments[0]->scope = $scope;
             $newProcess->amendment = ProcessPlainText::normalize($input['amendment'] ?? '');
 
-            $process = \App::$http->readPostResult('/process/status/redirect/', $newProcess)->getEntity();
+            \App::$http->readPostResult('/process/status/redirect/', $newProcess)->getEntity();
 
             return \BO\Slim\Render::redirect(
                 $workstation->getVariantName(),
