4e0a88d91ed392938b2127de5bf2ba3e19cead21
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2026-06-15 12:16:27 +0200

    Strengthen useraccount role list test

--- zmsapi/tests/Zmsapi/UseraccountListByRoleTest.php
+++ zmsapi/tests/Zmsapi/UseraccountListByRoleTest.php
@@ -14,6 +14,7 @@ class UseraccountListByRoleTest extends Base
         $body = (string) $response->getBody();
 
         $this->assertStringContainsString('agent_queue', $body);
+        $this->assertStringContainsString('testuser', $body);
         $this->assertEquals(200, $response->getStatusCode());
     }
 
