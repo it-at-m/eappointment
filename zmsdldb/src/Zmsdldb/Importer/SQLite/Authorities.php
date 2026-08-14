022e5ffe38f2fb365a01d14b4e7ff824233c60ed
ThomasAFink <thomas.fink@muenchen.de>
2026-08-15 00:30:28 +0200

    fix(ZMS): clear Psalm findings in the SQLite importer
    Fix the Location namespace casing, match entityClass docblocks to the MySQL base, and mark dynamically constructed importer classes as used.

--- zmsdldb/src/Zmsdldb/Importer/SQLite/Authorities.php
+++ zmsdldb/src/Zmsdldb/Importer/SQLite/Authorities.php
@@ -4,7 +4,9 @@ namespace BO\Zmsdldb\Importer\SQLite;
 
 use BO\Zmsdldb\Importer\MySQL\Authorities as AuthoritiesBase;
 
+/** @psalm-api */
 class Authorities extends AuthoritiesBase
 {
+    /** @var class-string<\BO\Zmsdldb\Importer\MySQL\Entity\Base>|null */
     protected ?string $entityClass = \BO\Zmsdldb\Importer\SQLite\Entity\Authority::class;
 }
--- zmsdldb/src/Zmsdldb/Importer/SQLite/Services.php
+++ zmsdldb/src/Zmsdldb/Importer/SQLite/Services.php
@@ -4,7 +4,9 @@ namespace BO\Zmsdldb\Importer\SQLite;
 
 use BO\Zmsdldb\Importer\MySQL\Services as ServicesBase;
 
+/** @psalm-api */
 class Services extends ServicesBase
 {
+    /** @var class-string<\BO\Zmsdldb\Importer\MySQL\Entity\Base>|null */
     protected ?string $entityClass = \BO\Zmsdldb\Importer\SQLite\Entity\Service::class;
 }
