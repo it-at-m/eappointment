35ad405b1730f7bf52cfdba27d01f455d0fba4a2
ThomasAFink <thomas.fink@muenchen.de>
2026-08-16 20:08:29 +0200

    fix(ZMS): keep API error status an int and write metaresult via array access

--- zmsbackend/src/Zmsbackend/Helper/ErrorHandler.php
+++ zmsbackend/src/Zmsbackend/Helper/ErrorHandler.php
@@ -17,12 +17,6 @@ class ErrorHandler implements ErrorHandlerInterface
     /**
      * @SuppressWarnings("PMD.UnusedFormalParameter")
      * @SuppressWarnings(Complexity)
-     * @param ServerRequestInterface $request
-     * @param \Throwable $exception
-     * @param bool $displayErrorDetails
-     * @param bool $logErrors
-     * @param bool $logErrorDetails
-     * @return ResponseInterface
      */
     #[\Override]
     public function __invoke(
@@ -35,42 +29,48 @@ class ErrorHandler implements ErrorHandlerInterface
         $response = \App::$slim->getResponseFactory()->createResponse();
         if ($exception instanceof \Slim\Exception\HttpNotFoundException) {
             $message = \BO\Zmsbackend\Api\Response\Message::create($request);
-            $message->meta->error = true;
-            $message->meta->message = "Could not find a resource with the given URL " . $request->getUri()->getPath();
+            $message->meta['error'] = true;
+            $message->meta['message'] = "Could not find a resource with the given URL " . $request->getUri()->getPath();
             $response = \BO\Slim\Render::withLastModified($response, time(), '0');
             return Render::withJson($response, $message, 404);
         }
 
         $message = \BO\Zmsbackend\Api\Response\Message::create($request);
-        $message->meta->error = true;
-        $message->meta->message = $exception->getMessage();
-        $message->meta->exception = get_class($exception);
-        $message->meta->trace = '';
+        $message->meta['error'] = true;
+        $message->meta['message'] = $exception->getMessage();
+        $message->meta['exception'] = get_class($exception);
+        $trace = '';
         foreach (array_slice($exception->getTrace(), 0, 10) as $call) {
-            $message->meta->trace .= "\\";
-            $message->meta->trace .= isset($call['class']) ? $call['class'] : '';
-            $message->meta->trace .= "::";
-            $message->meta->trace .= isset($call['function']) ? $call['function'] : '';
-            $message->meta->trace .= " +";
-            $message->meta->trace .= isset($call['line']) ? $call['line'] : '';
-            $message->meta->trace .= "\n";
+            $trace .= "\\";
+            $trace .= $call['class'] ?? '';
+            $trace .= "::";
+            $trace .= $call['function'] ?? '';
+            $trace .= " +";
+            $trace .= isset($call['line']) ? (string) $call['line'] : '';
+            $trace .= "\n";
         }
-        $message->meta->trace = Sanitizer::sanitizeStackTrace($message->meta->trace);
+        $trace = Sanitizer::sanitizeStackTrace($trace);
+        $message->meta['trace'] = $trace;
 
-        if (isset($exception->data)) {
-            $message->data = $exception->data;
+        $exceptionVars = get_object_vars($exception);
+        if (array_key_exists('data', $exceptionVars)) {
+            $message->data = $exceptionVars['data'];
         }
         $response = \BO\Slim\Render::withLastModified($response, time(), '0');
+        $code = (int) $exception->getCode();
         $status = 500;
-        if ($exception->getCode() >= 200 && $exception->getCode() <= 599) {
-            $status = $exception->getcode();
+        if ($code >= 200 && $code <= 599) {
+            $status = $code;
         }
-        if ($exception->getCode() >= 500 || !$exception->getCode()) {
+        if ($code >= 500 || $code === 0) {
+            $collapsedTrace = preg_replace("#(\s)+#", ' ', str_replace('\\', ':', $trace));
             \App::$log->critical(
                 "[API] Fatal Exception: "
                 . " in " . $exception->getFile() . " +" . $exception->getLine()
                 . " -> " . $exception->getMessage()
-                . " | Trace: " . Sanitizer::sanitizeStackTrace(preg_replace("#(\s)+#", ' ', str_replace('\\', ':', $message->meta->trace)) ?? '')
+                . " | Trace: " . Sanitizer::sanitizeStackTrace(
+                    is_string($collapsedTrace) ? $collapsedTrace : $trace
+                )
             );
         }
         return Render::withJson($response, $message, $status);
