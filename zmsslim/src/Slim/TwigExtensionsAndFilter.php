504862939d48bc6ee048aee8191334a54b387daf
ThomasAFink <thomas.fink@muenchen.de>
2026-08-16 20:05:16 +0200

    fix(ZMS): type Twig filter helpers used from templates

--- zmsslim/src/Slim/TwigExtensionsAndFilter.php
+++ zmsslim/src/Slim/TwigExtensionsAndFilter.php
@@ -14,7 +14,7 @@ namespace BO\Slim;
 class TwigExtensionsAndFilter extends TwigExtension
 {
     #[\Override]
-    public function getFilters()
+    public function getFilters(): array
     {
         return array(
             new \Twig\TwigFilter('msort', array($this, 'msort')),
@@ -24,15 +24,15 @@ class TwigExtensionsAndFilter extends TwigExtension
         );
     }
 
-    public function decodeEntities($string): string
+    public function decodeEntities(mixed $string): string
     {
-        return $string === null ? '' : trim(nl2br(html_entity_decode($string)));
+        return $string === null ? '' : trim(nl2br(html_entity_decode((string) $string)));
     }
 
-    public function msort($array, $key, $sort_flags = SORT_REGULAR)
+    public function msort(mixed $array, mixed $key, int $sort_flags = SORT_REGULAR): mixed
     {
         if (is_array($array) && count($array) > 0) {
-            if (!empty($key)) {
+            if ($key !== null && $key !== '' && $key !== []) {
                 $mapping = array();
                 foreach ($array as $k => $v) {
                     $sort_key = '';
@@ -49,7 +49,7 @@ class TwigExtensionsAndFilter extends TwigExtension
                 }
                 asort($mapping, $sort_flags);
                 $sorted = array();
-                foreach ($mapping as $k => $v) {
+                foreach (array_keys($mapping) as $k) {
                     $sorted[] = $array[$k];
                 }
                 return $sorted;
@@ -58,18 +58,21 @@ class TwigExtensionsAndFilter extends TwigExtension
         return $array;
     }
 
-    public function getObjectName($object): string
+    public function getObjectName(mixed $object): string
     {
+        if (!is_object($object) && !is_string($object)) {
+            return '';
+        }
         return (new \ReflectionClass($object))->getShortName();
     }
 
-    public function sanitizeHtml($html)
+    public function sanitizeHtml(mixed $html): string
     {
         static $purifier = null;
         if ($purifier === null) {
             $config = \HTMLPurifier_Config::createDefault();
             $purifier = new \HTMLPurifier($config);
         }
-        return $purifier->purify($html);
+        return $purifier->purify((string) $html);
     }
 }
