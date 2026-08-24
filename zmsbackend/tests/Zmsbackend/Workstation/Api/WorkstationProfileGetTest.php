9229d278aff2d299ebd8b259448ff50e7410aa77
matthilo96 <matthias.lorenz@muenchen.de>
2026-08-24 17:01:34 +0200

    fix(ZMSKVR-1575): fix unittest

--- zmsbackend/tests/Zmsbackend/Workstation/Api/WorkstationProfileGetTest.php
+++ zmsbackend/tests/Zmsbackend/Workstation/Api/WorkstationProfileGetTest.php
@@ -2,10 +2,33 @@
 
 namespace BO\Zmsbackend\Tests\Workstation\Api;
 
+use Psr\Http\Message\ResponseInterface;
+
 class WorkstationProfileGetTest extends
     \BO\Zmsbackend\Tests\Api\Base
 {
     protected $classname = 'WorkstationProfileGet';
+
+    public function testRendering(): ResponseInterface
+    {
+        $workstation = $this->setWorkstation();
+        $workstation->getUseraccount()->roles = ['agent_queue'];
+
+        $response = $this->render([], [], []);
+        $body = (string) $response->getBody();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertStringContainsString(
+            'useraccountprofile.json',
+            $body
+        );
+        $this->assertStringContainsString(
+            'Sachbearbeitung (Standard)',
+            $body
+        );
+
+        return $response;
+    }
 
     public function testAccessibleForEveryRole(): void
     {
