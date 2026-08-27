c85ed68d9deeef0e58deb0dd231e367fcd386508
ThomasAFink <thomas.fink@muenchen.de>
2026-08-27 21:31:58 +0200

    fix(ZMS): show login page instead of API error on statistic status
    Logged-out /statistic/status/ called the protected status API and rendered StatusAuthenticationFailed. Check workstation first and throw UserAccountMissingLogin so the same not-logged-in page as zmsadmin is shown.

--- zmsstatistic/tests/Zmsstatistic/StatusTest.php
+++ zmsstatistic/tests/Zmsstatistic/StatusTest.php
@@ -12,6 +12,11 @@ class StatusTest extends Base
         return [
             [
                 'function' => 'readGetResult',
+                'url' => '/workstation/',
+                'response' => $this->readFixture("GET_Workstation_Resolved2.json")
+            ],
+            [
+                'function' => 'readGetResult',
                 'url' => '/status/',
                 'response' => $this->readFixture("GET_status.json")
             ]
@@ -24,5 +29,22 @@ class StatusTest extends Base
         $this->assertStringContainsString('API Version', (string)$response->getBody());
         //check processes.confirmed:
         $this->assertStringContainsString('86861', (string)$response->getBody());
+    }
+
+    public function testWithoutWorkstation()
+    {
+        $this->expectException('\BO\Zmsentities\Exception\UserAccountMissingLogin');
+        $exception = new \BO\Zmsclient\Exception();
+        $exception->template = 'BO\Zmsentities\Exception\UserAccountMissingLogin';
+        $this->setApiCalls(
+            [
+                [
+                    'function' => 'readGetResult',
+                    'url' => '/workstation/',
+                    'exception' => $exception
+                ]
+            ]
+        );
+        parent::testRendering();
     }
 }
