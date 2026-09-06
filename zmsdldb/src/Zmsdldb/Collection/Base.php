4027b0347f22e35b677049f518ab55757751bdc0
Tom Fink <53316058+ThomasAFink@users.noreply.github.com>
2026-09-06 18:01:19 +0200

    fix(ZMS): correct zmsdldb InvalidTemplateParam notes

--- zmsdldb/src/Zmsdldb/Collection/Base.php
+++ zmsdldb/src/Zmsdldb/Collection/Base.php
@@ -11,7 +11,7 @@ use BO\Zmsdldb\Helper\Sorter;
 
 /**
  * @template T of \BO\Zmsdldb\Entity\Base
- * @extends \ArrayObject<int|string|null, T>
+ * @extends \ArrayObject<int|string, T>
  */
 class Base extends \ArrayObject
 {
