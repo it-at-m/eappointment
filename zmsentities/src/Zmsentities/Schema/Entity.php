1e31d63de227a7df3fe102b1edb5633f50a0a663
Tom Fink <53316058+ThomasAFink@users.noreply.github.com>
2026-09-06 23:52:00 +0200

    fix(ZMS): clear remaining PossiblyNullOperand notes in zmsentities

--- zmsentities/src/Zmsentities/Schema/Entity.php
+++ zmsentities/src/Zmsentities/Schema/Entity.php
@@ -269,11 +269,11 @@ class Entity extends \ArrayObject implements \JsonSerializable
         foreach ($mergeData as $key => $item) {
             if (isset($this[$key])) {
                 if ($this[$key] instanceof Entity) {
-                    $this[$key]->setResolveLevel($this->getResolveLevel() - 1);
+                    $this[$key]->setResolveLevel(($this->getResolveLevel() ?? 0) - 1);
                     $this[$key]->addData($item);
                 } elseif ($this[$key] instanceof \BO\Zmsentities\Collection\Base) {
                     $this[$key]->exchangeArray([]);
-                    $this[$key]->setResolveLevel($this->getResolveLevel() - 1);
+                    $this[$key]->setResolveLevel(($this->getResolveLevel() ?? 0) - 1);
                     $this[$key]->addData($item);
                 } elseif (is_array($this[$key])) {
                     $this[$key] = array_replace_recursive($this[$key], $item);
