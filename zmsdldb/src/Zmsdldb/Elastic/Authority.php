00c12c91a889d07cdcc9853112788fb30225116a
Tom Fink <53316058+ThomasAFink@users.noreply.github.com>
2026-09-06 18:19:49 +0200

    fix(ZMS): drop redundant array cast for zmsdldb RedundantCast notes

--- zmsdldb/src/Zmsdldb/Elastic/Authority.php
+++ zmsdldb/src/Zmsdldb/Elastic/Authority.php
@@ -30,7 +30,7 @@ class Authority extends Base
 
         /** @psalm-suppress RiskyTruthyFalsyComparison */
         if (is_array($servicelist) && $servicelist !== []) {
-            $filter = new \Elastica\Filter\Terms('services.service', array_values((array)$servicelist));
+            $filter = new \Elastica\Filter\Terms('services.service', array_values($servicelist));
             $filter->setExecution('and');
             $query->setPostFilter($filter);
         }
