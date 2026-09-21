4c29c08bd3f0b4ef7df7830ffd286ed74e5beee6
ThomasAFink <thomas.fink@muenchen.de>
2026-09-21 16:07:32 +0200

    fix(GH-3380): count JSX tags as used without eslint-plugin-react
    Core no-unused-vars does not see <FormContent /> as a reference after the React plugin was dropped.

--- zmsadmin/eslint.config.js
+++ zmsadmin/eslint.config.js
@@ -2,9 +2,40 @@ const { defineConfig } = require("eslint/config");
 const js = require("@eslint/js");
 const globals = require("globals");
 
+const jsxUsesVars = {
+    meta: {
+        name: "jsx-uses-vars",
+        version: "1.0.0"
+    },
+    rules: {
+        "jsx-uses-vars": {
+            create(context) {
+                const sourceCode = context.sourceCode;
+
+                function mark(node) {
+                    if (node.type === "JSXIdentifier") {
+                        sourceCode.markVariableAsUsed(node.name);
+                    } else if (node.type === "JSXMemberExpression") {
+                        mark(node.object);
+                    }
+                }
+
+                return {
+                    JSXOpeningElement(node) {
+                        mark(node.name);
+                    }
+                };
+            }
+        }
+    }
+};
+
 module.exports = defineConfig([
     js.configs.recommended,
     {
+        plugins: {
+            "jsx-uses-vars": jsxUsesVars
+        },
         languageOptions: {
             ecmaVersion: "latest",
             sourceType: "module",
@@ -26,6 +57,7 @@ module.exports = defineConfig([
             "no-console": [
                 "off"
             ],
+            "jsx-uses-vars/jsx-uses-vars": "error",
             "no-unused-vars": [
                 "error",
                 {
