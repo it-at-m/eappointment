26ea1d0984c568fdf25daaed38e0dde1e2e8be61
ThomasAFink <thomas.fink@muenchen.de>
2026-09-25 19:40:31 +0200

    fix(GH-3421): give each statistic worker its own Twig cache

--- zmsstatistic/tests/Zmsstatistic/bootstrap.php
+++ zmsstatistic/tests/Zmsstatistic/bootstrap.php
@@ -7,6 +7,15 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline) {
 
 putenv('ZMS_CONFIG_SECURE_TOKEN=secure-token');
 
+$testToken = getenv('TEST_TOKEN');
+if (is_string($testToken) && $testToken !== '') {
+    $cacheDir = dirname(__DIR__, 2) . '/cache/paratest-' . $testToken;
+    if (!is_dir($cacheDir)) {
+        mkdir($cacheDir, 0777, true);
+    }
+    putenv('ZMS_STATISTIC_TWIG_CACHE=/cache/paratest-' . $testToken . '/');
+}
+
 require(dirname(dirname(__DIR__)) . '/bootstrap.php');
 
 App::$now = new DateTimeImmutable('2016-04-01 11:55:00', new DateTimeZone('Europe/Berlin'));
